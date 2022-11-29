55abdc7a92ea311b09ba244f39888ff5b2451ca6
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2022-11-29 11:43:25 +0200

    Update 02. Half Sum Element!!!!!.py
    This is a fundamental tutorial for using variable to store sum of inputs in a loop and finding the biggest number out of many unputs in a loop.

--- Python Basics/04.PB-Python-For-Loop/For Loop - Exercise/02. Half Sum Element!!!!!.py
+++ Python Basics/04.PB-Python-For-Loop/For Loop - Exercise/02. Half Sum Element!!!!!.py
@@ -1,21 +1,21 @@
 n = int(input())
 import sys
-# 1.Създайте две помощни променливи:"max_num" с много ниска първоначална стойност, в която да пазите най-голямото от прочетените числа;"sum" с първоначална стойност 0, в която да пазите сумата от прочетените числа:
+# 1.Create two variables:"max_num" with very low value, in which we keep the biggest number and "sum_numbers"=0, in which we keep the sum of inputs:
 sum_numbers = 0
 max_num = -sys.maxsize
-# 2.Прочетете броя числа, които ще се въведат на конзолата – n, и направете for цикъл от 0 до n, като на всяко завъртане четете число num:
+# 2.After we get the number of inputs – n, we loop from 0 to n, as we input "num" on each iteration:
 for i in range (n) :
     num = int(input())
-#3.Вътре в цикъла направете проверка дали прочетеното число е по-голямо от "max_num". Ако е по-голямо, приравнете стойността на "max_num" към неговата. След което добавете стойността на прочетеното число към "sum_numbers":
+#3.Inside the loop we are checking if "num" is greater than "max_num". If greater, then "max_num" ill be equal to "num". 
     if num > max_num :
         max_num = num
-    #извън if сумирайте:
+    #After this we add each "num" into "sum_numbers" (out of if, because we do this unconditionally for all "num"):
     sum_numbers += num
-#4.След цикъла проверете дали "max_num" е равно на сумата от всички числа, от която е извадено "max_num":
+#4.After the loop we finally have the final "sum_numbers" and the final "max_num" and we can finally check if... :
 if max_num == sum_numbers - max_num :
     print ("Yes")
     print (f"Sum = {max_num}")
-# 5.Разликата между най-голямото число и сумата от останалите:
+# 5.If they are not equal, we print the difference (positive number, using abs() function):
 else :
     print ("No")
     sum_others = sum_numbers - max_num
