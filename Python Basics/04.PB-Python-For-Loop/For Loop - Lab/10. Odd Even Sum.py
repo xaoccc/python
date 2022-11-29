b518b8dda6c1fa3ff89d34c9621231c157922b05
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2022-11-29 11:50:52 +0200

    Update 10. Odd Even Sum.py

--- Python Basics/04.PB-Python-For-Loop/For Loop - Lab/10. Odd Even Sum.py
+++ Python Basics/04.PB-Python-For-Loop/For Loop - Lab/10. Odd Even Sum.py
@@ -1,28 +1,19 @@
 #input
 n = int(input())
-sum1 = 0
-sum2 = 0
+sum_even = 0
+sum_odd = 0
 
 #logic
 for i in range (0, n):
     current_number = int(input())
-#sum of the even numbers
     if i % 2 == 0 :
-        sum1 = current_number + sum1
-#sum of the odd numbers
+        sum_even = current_number + sum_even
     else :
-        sum2 = current_number + sum2
+        sum_odd = current_number + sum_odd
 #comparing both sums and output
-if sum1 == sum2 :
+if sum_even == sum_odd :
     print("Yes")
-    print(f"Sum = {sum1}")
+    print(f"Sum = {sum_even}")
 else :
     print("No")
-    if sum1 > sum2 :
-        print(f"Diff = {sum1-sum2}")
-    elif  sum1 < sum2 :
-        print(f"Diff = {sum2-sum1}")
-
-
-
-
+    print(f"Diff = {abs(sum_even - sum_odd)}")
