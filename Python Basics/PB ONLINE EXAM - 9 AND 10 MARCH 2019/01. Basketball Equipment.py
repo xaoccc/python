7258131eb48a2c77641dd691740091dd5a585017
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2022-11-30 16:05:53 +0200

    Create  01. Basketball Equipment.py

---
+++ Python Basics/PB ONLINE EXAM - 9 AND 10 MARCH 2019/01. Basketball Equipment.py
@@ -0,0 +1,7 @@
+yr_fee = int(input())
+snickers = yr_fee * 0.6
+clothes = snickers * 0.8
+ball = clothes / 4
+access = ball / 5
+total = yr_fee + snickers + clothes + ball + access
+print(f"{total:.2f}")
