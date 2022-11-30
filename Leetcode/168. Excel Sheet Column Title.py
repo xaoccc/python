6a013c74ec17334c8093f831d95ff6077c5de758
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2022-11-30 16:19:49 +0200

    Create 168. Excel Sheet Column Title.py
    Source: https://leetcode.com/problems/excel-sheet-column-title/

---
+++ Leetcode/168. Excel Sheet Column Title.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        """
+        :type columnNumber: int
+        :rtype: str
+        """
+        import string
+        column_name = ""
+        #We write -1, due to the 1-based excel numbering (A = 1, not A = 0)
+        remaining_letters = columnNumber - 1
+        alphabet = list(string.ascii_uppercase)
+ 
+        while remaining_letters >= 0:
+            #Here we define the last letter of the column name
+            column_name = alphabet[remaining_letters % 26] + column_name
+            #Here we define every next letter of the name backwards
+            remaining_letters = (remaining_letters // 26) - 1
+        return column_name
