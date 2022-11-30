e8598462a7285a3e8fa17d212748497db0d3f5a8
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2022-11-30 16:25:18 +0200

    Create 119. Pascal's Triangle II.py

---
+++ Leetcode/119. Pascal's Triangle II.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """     
+        outer_list = []
+        inner_list = []
+        for i in range(0, rowIndex+1):
+            if i == 0:
+                inner_list = [1]
+                outer_list.append(inner_list)
+                continue
+            elif i == 1:
+                inner_list = [1,1]
+                outer_list.append(inner_list)
+                continue
+            else:
+                inner_list = []
+                for j in range(0, i + 1):
+                    if j == 0 or j == i:
+                        current_num = 1
+                    else:
+                        current_num = outer_list[i-1][j-1] + outer_list[i-1][j]
+                    inner_list.append(current_num)
+                outer_list.append(inner_list)    
+        return outer_list[rowIndex]
