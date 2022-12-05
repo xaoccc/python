ac0970ae8de989253fb185927940fd1b3d55ad91
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2022-12-05 10:45:07 +0200

    Update 7. Patterns.py

--- Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/7. Patterns.py
+++ Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/7. Patterns.py
@@ -1,3 +1,5 @@
+#We use two loops instead of one nestd loop, because hre we have Linear Time — O(n), in nested loops we have Quadratic Time — O(n²)
+#Thus we optimize time complexity
 num = int(input())
 for i in range(num+1):
   print("*" * i)
