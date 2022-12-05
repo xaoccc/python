9347c391a5425910b9920bbad94941ccd5ab295f
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2022-12-05 11:04:00 +0200

    Update 7. Patterns.py

--- Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/7. Patterns.py
+++ Python-Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/7. Patterns.py
@@ -1,5 +1,6 @@
-#We use two loops instead of one nestd loop, because hre we have Linear Time — O(n), in nested loops we have Quadratic Time — O(n²)
-#Thus we optimize time complexity
+#We use normal loops instead of nested loops, because hre we have 2 x Linear Time — O(2*n), in nested loops we have 2 x Quadratic Time — O(2*n²) , 
+#because in furst case we have 2 loops, in second one we have 2 nested loops, so time complexity = O(2*n)
+#Thus we optimize time complexity, also we use 5 lines instead of 9 lines -> space complexity = O(2*n) as well
 num = int(input())
 for i in range(num+1):
   print("*" * i)
