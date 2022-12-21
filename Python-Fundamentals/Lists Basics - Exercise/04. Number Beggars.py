d3f5612cbae91adc29492f4f9fd39dd98aedc4e9
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2022-12-21 16:57:48 +0200

    Update 04. Number Beggars.py

--- Python-Fundamentals/Lists Basics - Exercise/04. Number Beggars.py
+++ Python-Fundamentals/Lists Basics - Exercise/04. Number Beggars.py
@@ -1,9 +1,14 @@
+#input
 int_string = input()
 beggars_num = int(input())
+#Converting the input into a list
 int_string = int_string.split(", ")
+#Creating a list to store the final data
 final_list = []
 new_list_element = 0
 
+#logic
+#We need two nested loops - one to iterate and sum each beggar profit and anouther one - to append each sum into the final list
 for i in range(0, beggars_num + 1):
   final_list.append(new_list_element)
   new_list_element = 0
@@ -12,7 +17,7 @@ for i in range(0, beggars_num + 1):
       new_list_element += int(int_string[i + beggars_num * j]) 
     else:
         break
-    
+#because we added the first element in the beginning, now we need to delete it    
 del final_list[0]  
   
 print(final_list)
