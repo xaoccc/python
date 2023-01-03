1b166cb1c8a99c57ddde2da028c3af71f2a0be79
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-03 16:40:47 +0200

    Update 03. Car Race 90-100.py

--- Python-Fundamentals/Lists Basics - More Exercises/03. Car Race 90-100.py
+++ Python-Fundamentals/Lists Basics - More Exercises/03. Car Race 90-100.py
@@ -7,13 +7,16 @@ car_two_sum_time = 0
 
 for i in range(len(car_one)):
     car_one[i] = int(car_one[i])
-    car_two[i] = int(car_two[i])
     car_one_sum_time += car_one[i]
-    car_two_sum_time += car_two[i]
     if car_one[i] == 0:
         car_one_sum_time *= 0.8
-    if car_two[i] == 0:
-        car_two_sum_time *= 0.8
+
+for j in range(len(car_two) - 1, -1, -1):       
+    car_two[j] = int(car_two[j])    
+    car_two_sum_time += car_two[j]
+    if car_two[j] == 0:
+        car_two_sum_time *= 0.8    
+    
 if car_one_sum_time < car_two_sum_time:
     print(f"The winner is left with total time: {car_one_sum_time:.1f}")
 else:
