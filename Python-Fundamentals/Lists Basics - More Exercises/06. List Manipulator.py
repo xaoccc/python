a72e0292e924086f71850b025f25236a2ad26e61
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-05 11:04:44 +0200

    Create 06. List Manipulator.py

---
+++ Python-Fundamentals/Lists Basics - More Exercises/06. List Manipulator.py
@@ -0,0 +1,26 @@
+nums_input = input()
+nums_input = [eval(i) for i in nums_input.split(" ")]
+command_input = input()
+while command_input[0] != "end":
+    command_input = command_input.split(" ")
+    if command_input[0] == exchange:
+        
+    elif command_input[0] == min:
+        if command_input[1] == odd:
+            
+        elif command_input[1] == even:
+        
+    elif command_input[0] == max:
+        if command_input[1] == odd:
+            
+        elif command_input[1] == even:
+            
+    elif command_input[0] == first:
+        if command_input[2] == odd:
+            
+        elif command_input[2] == even:
+        
+    elif command_input[0] == last:
+        if command_input[2] == odd:
+            
+        elif command_input[2] == even:
