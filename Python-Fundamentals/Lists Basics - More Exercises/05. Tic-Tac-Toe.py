c9cfa78c1c2d887e4a7cfecb83f2c8f1a80489a8
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-05 10:25:45 +0200

    Update 05. Tic-Tac-Toe.py

--- Python-Fundamentals/Lists Basics - More Exercises/05. Tic-Tac-Toe.py
+++ Python-Fundamentals/Lists Basics - More Exercises/05. Tic-Tac-Toe.py
@@ -5,23 +5,30 @@ line_three = input()
 line_one = line_one.split(" ")
 line_two = line_two.split(" ")
 line_three = line_three.split(" ")
+first_win = False
+second_win = False
 
 game_data = line_one + line_two + line_three
 game_data = [eval(i) for i in game_data]
-
-if (game_data[0] + game_data[3] + game_data[6] == 6) \n
-    or (game_data[1] + game_data[4] + game_data[7] == 6) \n
-    or (game_data[2] + game_data[5] + game_data[8] == 6) \n
+for i in range(3):
+    if (game_data[i] + game_data[i + 3] + game_data[i + 6] == 6) \
+    or (game_data[3 * i] + game_data[(3 * i) + 1] + game_data[(3 * i) + 2] == 6):
+        second_win = True
+    elif (game_data[i] == 1 and game_data[i + 3] == 1 and game_data[i + 6] == 1) \
+    or (game_data[3 * i] == 1 and game_data[(3 * i) + 1] == 1 and game_data[(3 * i) + 2] == 1):
+        first_win = True
+if (game_data[0] + game_data[4] + game_data[8] == 6) \
+or (game_data[2] + game_data[4] + game_data[6] == 6):
+    second_win = True
+if (game_data[0] == 1 and game_data[4] == 1 and game_data[8] == 1) \
+or (game_data[2] == 1 and game_data[4] == 1 and game_data[6] == 1):
+    first_win = True
     
-    or (game_data[0] + game_data[1] + game_data[2] == 6) \n
-    or (game_data[3] + game_data[4] + game_data[5] == 6) \n
-    or (game_data[6] + game_data[7] + game_data[8] == 6) \n
-    
-    or (game_data[0] + game_data[4] + game_data[8] == 6) \n
-    or (game_data[2] + game_data[4] + game_data[6] == 6):
-
+if first_win == True and second_win == True:
+    print("Draw!")
+elif first_win == True:
+    print("First player won")
+elif second_win == True:
     print("Second player won")
-
-elif
-
-print(game_data)
+else:
+    print("Draw!")
