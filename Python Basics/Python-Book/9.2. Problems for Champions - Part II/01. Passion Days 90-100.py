6eb1869502a1f28939d45cedf4b90d3f782f318e
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-06 10:51:33 +0200

    Create 01. Passion Days 90-100.py

---
+++ Python Basics/Python-Book/9.2. Problems for Champions - Part II/01. Passion Days 90-100.py
@@ -0,0 +1,47 @@
+#src: https://python-book.softuni.bg/chapter-09-problems-for-champions-part-2.html
+
+available_money = float(input())
+command = input()
+ 
+while command != "mall.Enter":
+    command = input()
+
+number_purchases = 0
+action = ""
+ 
+while action != "mall.Exit":
+ 
+    for symbol in range(len(action)):
+ 
+        if 65 <= ord(action[symbol]) <= 90:
+            cost_of_purchase = ord(action[symbol]) * 0.50
+            if cost_of_purchase <= available_money:
+                available_money -= cost_of_purchase
+                number_purchases += 1
+ 
+        elif 97 <= ord(action[symbol]) <= 122:
+            cost_of_purchase = ord(action[symbol]) * 0.30
+            if cost_of_purchase <= available_money:
+                available_money -= cost_of_purchase
+                number_purchases += 1
+ 
+        elif action[symbol] == "%":
+            if available_money != 0:
+                available_money *= 0.50
+                number_purchases += 1
+ 
+        elif action[symbol] == "*":
+            available_money += 10
+ 
+        else:
+            cost_of_purchase = ord(action[symbol])
+            if cost_of_purchase <= available_money:
+                available_money -= cost_of_purchase
+                number_purchases += 1
+ 
+    action = input()
+ 
+if number_purchases == 0:
+    print(f"No purchases. Money left: {available_money:.2f} lv.")
+else:
+    print(f"{number_purchases} purchases. Money left: {available_money:.2f} lv.")
