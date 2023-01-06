698ef9e4e0eaeab133f3359b3d02b69afe46c523
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-06 15:15:57 +0200

    Update and rename 02. X Expression 0-100.py to 02. X Expression 48-100.py

---
+++ Python Basics/Python-Book/9.2. Problems for Champions - Part II/02. X Expression 48-100.py
@@ -0,0 +1,73 @@
+#src: https://python-book.softuni.bg/chapter-09-problems-for-champions-part-2.html
+
+problem = input()
+total = 0
+result = 0
+internal_result = 0
+internal = False
+operator = ""
+
+for i in range(0, len(problem) - 1):
+    if i == 1 and problem[0] != "(":
+        result = int(problem[0])
+    
+    if problem[i] == "+":
+        if problem[i+1] == "(":
+            continue
+        else:
+            if internal == True:
+                internal_result += int(problem[i+1])
+            else:
+                result += int(problem[i+1])
+            i += 1
+            
+    elif problem[i] == "-": 
+        if problem[i+1] == "(":
+            continue
+        else:
+            if internal == True:
+                internal_result -= int(problem[i+1])
+            else:
+                result -= int(problem[i+1])
+            i += 1
+            
+    elif problem[i] == "/":
+        if problem[i+1] == "(":
+            continue
+        else:
+            if internal == True:
+                internal_result /= int(problem[i+1])
+            else:
+                result /= int(problem[i+1])
+            i += 1
+            
+    elif problem[i] == "*":
+        if problem[i+1] == "(":
+            continue
+        else:
+            if internal == True:
+                internal_result *= int(problem[i+1])
+            else:
+                result *= int(problem[i+1])
+            i += 1
+        
+    elif problem[i] == "(":
+        if i > 0:
+            operator = problem[i-1]
+        internal = True
+        internal_result = int(problem[i+1])
+    elif problem[i] == ")":
+        internal = False
+        if operator == "+":
+            result += internal_result
+        elif operator == "-":
+            result -= internal_result
+        elif operator == "/":
+            result /= internal_result
+        elif operator == "*":
+            result *= internal_result
+        else:
+            result = internal_result
+        
+print(round(result,2))
+
