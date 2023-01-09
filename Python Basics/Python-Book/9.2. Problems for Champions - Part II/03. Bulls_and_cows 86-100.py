d7d89a648d1712897516966b143f7c4a6155a463
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-09 15:29:02 +0200

    Update and rename 03. Bulls_and_cows 33-100.py to 03. Bulls_and_cows 86-100.py

--- Python Basics/Python-Book/9.2. Problems for Champions - Part II/03. Bulls_and_cows 86-100.py
+++ Python Basics/Python-Book/9.2. Problems for Champions - Part II/03. Bulls_and_cows 86-100.py
@@ -14,62 +14,62 @@ for i in range(1111, 9999):
     numcheck = [str(i).rjust(4, '0')]
     if numcheck[0][0] == num[0]:
         bulls_found += 1
-        num.replace(num[0], "a")
+        num = num.replace(num[0], "a")
     if numcheck[0][1] == num[1]:
         bulls_found += 1
-        num.replace(num[1], "a")
+        num = num.replace(num[1], "a")
     if numcheck[0][2] == num[2]:
         bulls_found += 1
-        num.replace(num[2], "a")
+        num = num.replace(num[2], "a")
     if numcheck[0][3] == num[3]:
         bulls_found += 1
-        num.replace(num[3], "a")
+        num = num.replace(num[3], "a")
         
         
     if numcheck[0][0] == num[1]:
         cows_found += 1
-        num.replace(num[1], "a")
+        num = num.replace(num[1], "a")
     if numcheck[0][0] == num[2]:
         cows_found += 1
-        num.replace(num[2], "a")
+        num = num.replace(num[2], "a")
     if numcheck[0][0] == num[3]:
         cows_found += 1
-        num.replace(num[3], "a")
+        num = num.replace(num[3], "a")
         
     if numcheck[0][1] == num[0]:
         cows_found += 1
-        num.replace(num[0], "a")
+        num = num.replace(num[0], "a")
     if numcheck[0][1] == num[2]:
         cows_found += 1
-        num.replace(num[2], "a")
+        num = num.replace(num[2], "a")
     if numcheck[0][1] == num[3]:
         cows_found += 1
-        num.replace(num[3], "a")
+        num = num.replace(num[3], "a")
         
     if numcheck[0][2] == num[0]:
         cows_found += 1
-        num.replace(num[0], "a")
+        num = num.replace(num[0], "a")
     if numcheck[0][2] == num[1]:
         cows_found += 1
-        num.replace(num[1], "a")
+        num = num.replace(num[1], "a")
     if numcheck[0][2] == num[3]:
         cows_found += 1
-        num.replace(num[3], "a")
+        num = num.replace(num[3], "a")
         
     if numcheck[0][3] == num[0]:
         cows_found += 1
-        num.replace(num[0], "a")
+        num = num.replace(num[0], "a")
     if numcheck[0][3] == num[1]:
         cows_found += 1
-        num.replace(num[1], "a")
+        num = num.replace(num[1], "a")
     if numcheck[0][3] == num[2]:
         cows_found += 1
-        num.replace(num[2], "a")
+        num = num.replace(num[2], "a")
 
 
     if cows == cows_found and bulls == bulls_found:
         solution_found = True
-        print(numcheck[0], end="")
+        print(f"{numcheck[0]} ", end="")
         
 if not solution_found:
     print("No")
