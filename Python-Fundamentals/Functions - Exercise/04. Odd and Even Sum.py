178e8559167113f1932c60ce0ccf57b58090d4a1
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-12 15:04:04 +0200

    Create 04. Odd and Even Sum.py

---
+++ Python-Fundamentals/Functions - Exercise/04. Odd and Even Sum.py
@@ -0,0 +1,19 @@
+def odd_even_sum(num = input()):
+
+    def odd_sum_funk():
+        odd_sum = 0
+        for i in range(len(num)):
+            if int(num[i]) % 2 != 0:
+                odd_sum += int(num[i])
+        print(f"Odd sum = {odd_sum}, ", end="")
+    odd_sum_funk()
+    
+    def even_sum_funk():
+        even_sum = 0
+        for i in range(len(num)):
+            if int(num[i]) % 2 == 0:
+                even_sum += int(num[i])
+        print(f"Even sum = {even_sum}")
+    even_sum_funk()
+    
+odd_even_sum()
