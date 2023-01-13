b3fc202edd184a83afbf4af9dbde25260573b6cf
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-13 11:09:21 +0200

    Update RockPaperScissors.py

--- Projects/RockPaperScissors.py
+++ Projects/RockPaperScissors.py
@@ -1,1 +1,18 @@
-.
+import random
+
+rock = "Rock"
+paper = "Paper"
+scissors = "Scissors"
+
+player_move = input("Choose[r]ock, [p]aper or [s]cissors :")
+
+if player_move = "r":
+    player_move = rock
+elif player_move = "p":
+    player_move = paper
+elif player_move = "s":
+    player_move = scissors
+else:
+    raise SystemExit("Invalid input. Please try again...")
+    
+computer_move = random.randint(1, 3)
