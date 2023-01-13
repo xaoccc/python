4c502f7fd2f4c2aac0d3d83f96a8617b465e9e24
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-13 12:00:40 +0200

    Update RockPaperScissors.py

--- Projects/RockPaperScissors.py
+++ Projects/RockPaperScissors.py
@@ -1,5 +1,8 @@
 import random
-from colorama import Fore, Back, Style
+def prRed(skk): print("\033[31m {}\033[00m" .format(skk))
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def prGreen(skk): print("\033[32m {}\033[00m" .format(skk))
+def prBlue(skk): print("\033[34m {}\033[00m" .format(skk))
 
 rock = "Rock"
 paper = "Paper"
@@ -27,31 +30,31 @@ while True:
     elif computer_random_number == 3:
         computer_move = scissors
     
-    print(Fore.BLUE + f"The computer chose {computer_move}.")
+    prBlue(f"The computer chose {computer_move}.")
         
     if (player_move == rock and computer_move == scissors) or \
         (player_move == paper and computer_move == rock) or \
         (player_move == scissors and computer_move == paper):
-        print(Fore.GREEN +"You win!")
+        prGreen("You win!")
         game_continue = input("Type [yes] to play again or [no] to quit.")
         if game_continue == "yes":
             continue
         else:
-            print("Thank you for playing")
+            print("Thank you for playing.")
             break
     elif player_move == computer_move:
-        print(Fore.YELLOW + "Draw!")
+        prYellow("Draw!")
         game_continue = input("Type [yes] to play again or [no] to quit.")
         if game_continue == "yes":
             continue
         else:
-            print("Thank you for playing")
+            print("Thank you for playing.")
             break
     else:
-        print(Fore.RED + "You lose!")
+        prRed("You lose!")
         game_continue = input("Type [yes] to play again or [no] to quit.")
         if game_continue == "yes":
             continue
         else:
-            print("Thank you for playing")
+            print("Thank you for playing.")
             break
