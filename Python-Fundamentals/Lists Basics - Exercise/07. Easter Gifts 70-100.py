f6ff81618dd500aab2083fb318a40440a941ac8c
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-18 15:04:26 +0200

    Update 07. Easter Gifts 70-100.py

--- Python-Fundamentals/Lists Basics - Exercise/07. Easter Gifts 70-100.py
+++ Python-Fundamentals/Lists Basics - Exercise/07. Easter Gifts 70-100.py
@@ -1,6 +1,5 @@
 gifts_list = input().split()
 command = input()
-counter = 0
 
 while command != "No Money":
     command = command.split()
@@ -8,7 +7,7 @@ while command != "No Money":
         for i in range(len(gifts_list)):
             if gifts_list[i] == command[1]:
                 gifts_list[i] = "None"
-                counter += 1
+
     elif command[0] == "Required":
         if 0 <= int(command[2]) < len(gifts_list):
             gifts_list[int(command[2])] = command[1]
@@ -16,7 +15,7 @@ while command != "No Money":
         gifts_list[-1] = command[1]
     command = input()
     
-for j in range(counter):
+for j in gifts_list:
     if "None" in gifts_list:
         gifts_list.remove("None")
 
