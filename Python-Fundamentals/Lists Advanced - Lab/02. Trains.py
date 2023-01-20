b363dbceda3003b3d4edf1ddd8cf7fe1f5d43db4
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-20 13:39:14 +0200

    Update 02. Trains.py

--- Python-Fundamentals/Lists Advanced - Lab/02. Trains.py
+++ Python-Fundamentals/Lists Advanced - Lab/02. Trains.py
@@ -1,15 +1,14 @@
 wagons_num = int(input())
-wagons_num_list = []
-for i in range(wagons_num):
-    wagons_num_list.append(0)
+train = [0] * wagons_num
+
 command = input()
 while command != "End":
     command = command.split()
     if command[0] == "add":
-        wagons_num_list[-1] += int(command[1])
+        train[-1] += int(command[1])
     elif command[0] == "insert":
-        wagons_num_list[int(command[1])] += int(command[2])
+        train[int(command[1])] += int(command[2])
     elif command[0] == "leave":
-        wagons_num_list[int(command[1])] -= int(command[2])
+        train[int(command[1])] -= int(command[2])
     command = input()
-print(wagons_num_list)
+print(train)
