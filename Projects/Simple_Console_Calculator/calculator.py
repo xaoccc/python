918b49704634eb8932ff74af687de491c65c17fd
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-24 16:51:42 +0200

    Update calculator.py

--- Projects/Simple_Console_Calculator/calculator.py
+++ Projects/Simple_Console_Calculator/calculator.py
@@ -1,3 +1,5 @@
+import math
+
 num1_input = float(input())
 result = num1_input
 while True:
@@ -5,6 +7,15 @@ while True:
     command_input = input()
     if command_input == "=":
         break
+    elif command_input == "log":
+        result = math.log(result)
+        print(result)
+        continue
+    elif command_input == "f":
+        result = math.factorial(int(result))
+        print(result)
+        continue
+    
     num2_input = float(input())
 
     if command_input == "+":
@@ -22,8 +33,8 @@ while True:
     elif command_input == "^":
         result **= num2_input
         print(result)
-    elif command_input == "modul":
+    elif command_input == "m":
         result %= num2_input
         print(result)
-
+        
 print(result)
