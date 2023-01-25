6e4eaa0a2462bc8eafcb389ecd46fb68066bb32a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-25 16:27:57 +0200

    Update 02. Shopping List.py

--- Python-Fundamentals/04. Programming Fundamentals Mid Exam/02. Shopping List.py
+++ Python-Fundamentals/04. Programming Fundamentals Mid Exam/02. Shopping List.py
@@ -4,22 +4,20 @@ command = input()
 while command != "Go Shopping!":
     command = command.split()
     if command[0] == "Urgent":
-        if shopping_list.count(command[1]) > 0:
-            continue
-        else:
+        if shopping_list.count(command[1]) == 0:
             shopping_list.insert(0, command[1])
     elif command[0] == "Unnecessary":
         if shopping_list.count(command[1]) > 0:
             shopping_list.remove(command[1])
     elif command[0] == "Correct":
         if shopping_list.count(command[1]) > 0:
-            for i in shopping_list:
-                if i == command[1]:
-                    i = command[2]
+            for i in range(len(shopping_list)):
+                if shopping_list[i] == command[1]:
+                    shopping_list[i] = command[2]
     elif command[0] == "Rearrange": 
         if shopping_list.count(command[1]) > 0:
             shopping_list.remove(command[1])
             shopping_list.append(command[1])
             
     command = input()
-print(shopping_list)
+print(", ".join(shopping_list))
