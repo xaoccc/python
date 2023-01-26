afecd948cd11470cd57308f955cff8ef0830bd87
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-26 15:32:51 +0200

    Create 02. Treasure Hunt80-100.py

---
+++ Python-Fundamentals/06. Programming Fundamentals Mid Exam Retake/02. Treasure Hunt80-100.py
@@ -0,0 +1,35 @@
+treasure = input().split("|")
+command = input()
+stolen = []
+treasure_gain = 0
+
+while command != "Yohoho!":
+    command = command.split()
+    if command[0] == "Loot":
+        for i in range(1, len(command)):
+            if command[i] not in treasure:
+                treasure.insert(0, command[i])
+    
+    elif command[0] == "Drop":
+        if 0 <= int(command[1]) < len(treasure):
+            treasure.append(treasure[int(command[1])])
+            treasure.remove(treasure[int(command[1])])
+    
+    elif command[0] == "Steal":
+        if len(treasure) <= int(command[1]):
+            stolen = treasure
+            treasure = []
+        else:
+            stolen.extend(treasure[(len(treasure) - int(command[1])): ])
+            del treasure[(len(treasure) - int(command[1])): ]
+
+        print(", ".join(stolen))
+    command = input()
+    
+if len(treasure) == 0:
+    print("Failed treasure hunt.")
+else:
+    for i in range(len(treasure)):
+        treasure_gain += len(treasure[i])
+    treasure_gain /= len(treasure)
+    print(f"Average treasure gain: {treasure_gain:.2f} pirate credits.")
