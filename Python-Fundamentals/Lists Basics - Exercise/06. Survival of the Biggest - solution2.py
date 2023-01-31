7ac148b3ec2d7d2f2b0f928a938911ad0423d0ec
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-31 16:29:30 +0200

    Update 06. Survival of the Biggest - solution2.py

--- Python-Fundamentals/Lists Basics - Exercise/06. Survival of the Biggest - solution2.py
+++ Python-Fundamentals/Lists Basics - Exercise/06. Survival of the Biggest - solution2.py
@@ -1,22 +1,9 @@
-from itertools import chain
-
-#input
-nums_list = input()
+nums_list = [int(i) for i in input().split()]
 biggest_num = int(input())
 
-#logic
-#create one list from the input and one - for sorting, where we keep the sorted input, created with sorted(), eval() and a for loop
-nums_list = nums_list.split(" ")
-sorted_nums = sorted([eval(i) for i in nums_list])
-
-#A little confusing, the output variable is the altered input variable
-#Here we iterate the list from the beginning to the end and backwards. chain() seems to be very useful for this
-for j in chain(range(0, len(nums_list)), range(len(nums_list), -1, -1)):
-#here we make sure we do not receive an index_out_of_range error:
-    if j > len(nums_list) - 1:
-        continue
-    if int(nums_list[j]) < sorted_nums[biggest_num]:
-        nums_list.remove(nums_list[j])
-        
-#output - should be a string 
+for i in range(biggest_num):
+    smallest = min(nums_list)
+    nums_list.remove(smallest)
+    
+nums_list = [str(i) for i in nums_list]
 print(", ".join(nums_list))
