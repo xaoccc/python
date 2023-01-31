4e8351b9304aa7cb41a13f47ae5d9c76546f669a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-01-31 12:38:37 +0200

    Create 10. Pokemon Don't Go.py

---
+++ Python-Fundamentals/Lists Advanced - Exercise/10. Pokemon Don't Go.py
@@ -0,0 +1,24 @@
+pokemons = [int(i) for i in input().split()]
+removed = 0
+while len(pokemons) > 0:
+    pokemon_index = int(input())
+    if pokemon_index >= len(pokemons):
+        removed += pokemons[-1]
+        pokemons = [i + pokemons[0] for i in pokemons]
+        pokemons.pop(-1)
+        pokemons.append(pokemons[0])
+    elif pokemon_index < 0:
+        removed += pokemons[0]
+        pokemons.pop(0)
+        pokemons.insert(0, pokemons[-1])
+    else:
+        current = pokemons[pokemon_index]
+        pokemons.pop(pokemon_index)
+        for i in range(len(pokemons)):
+                if pokemons[i] <= current:
+                    pokemons[i] += current
+                else:
+                    pokemons[i] -= current
+        removed += current
+
+print(removed)
