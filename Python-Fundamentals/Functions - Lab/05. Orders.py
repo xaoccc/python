813403e9d3b690dd69f5d44c06aab9639905377a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-01 21:04:08 +0200

    Update 05. Orders.py

--- Python-Fundamentals/Functions - Lab/05. Orders.py
+++ Python-Fundamentals/Functions - Lab/05. Orders.py
@@ -1,4 +1,4 @@
-def order(order_type = input(), qty = int(input())):
+def order(order_type, qty):
     if order_type == "coffee":
         return qty * 1.5
     elif order_type == "water":
@@ -7,4 +7,4 @@ def order(order_type = input(), qty = int(input())):
         return qty * 1.4
     else:
         return qty * 2
-print(f"{order():.2f}")
+print(f"{order(input(), int(input())):.2f}")
