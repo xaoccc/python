81e7e10990a85c22c2b7c7bf74291c57e18352db
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-02 09:50:47 +0200

    Update 10. Perfect Number.py

--- Python-Fundamentals/Functions - Exercise/10. Perfect Number.py
+++ Python-Fundamentals/Functions - Exercise/10. Perfect Number.py
@@ -1,4 +1,4 @@
-def perfect_num(num_input = int(input())):
+def perfect_num(num_input):
     num_check = 0
     for i in range(1, num_input):
         if num_input % i == 0:
@@ -7,4 +7,4 @@ def perfect_num(num_input = int(input())):
         print("We have a perfect number!")
     else:
         print("It's not so perfect.")
-perfect_num()
+perfect_num(int(input()))
