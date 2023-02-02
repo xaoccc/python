d20ed5414c4061bef5f07d04d802cc4d423d98dc
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-02 09:22:13 +0200

    Update 07. Rounding.py

--- Python-Fundamentals/Functions - Lab/07. Rounding.py
+++ Python-Fundamentals/Functions - Lab/07. Rounding.py
@@ -1,8 +1,4 @@
-def rounding(nums = input()):
-#in 1 line we do 3 actions: 
-#1. nums.split() makes the string into a list 
-#2. eval(i) for i in nums turns all str elements into float
-#3. round() formats them
-    nums = [round(eval(i)) for i in nums.split()]
+def rounding(nums):
+    nums = [round(float(i)) for i in nums]
     return nums
-print(rounding())
+print(rounding(input().split()))
