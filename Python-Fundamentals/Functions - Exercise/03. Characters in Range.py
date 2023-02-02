e3de617dd1686875f1164aaa8c31fd1245a8a1fa
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-02 09:31:24 +0200

    Update 03. Characters in Range.py

--- Python-Fundamentals/Functions - Exercise/03. Characters in Range.py
+++ Python-Fundamentals/Functions - Exercise/03. Characters in Range.py
@@ -1,6 +1,6 @@
-def char_range(char_one = input(), char_two = input()):
+def char_range(char_one, char_two):
     char_list = []
-    for i in range(ord(char_one)+1, ord(char_two)):
+    for i in range(ord(char_one) + 1, ord(char_two)):
         char_list.append(chr(i))
     return (" ").join(char_list)
-print (char_range())
+print (char_range(input(), input()))
