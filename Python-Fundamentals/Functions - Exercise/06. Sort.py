8dcb94a025fa0e62c4407e9d47d5848b3b3a28f1
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-02 09:38:43 +0200

    Update 06. Sort.py

--- Python-Fundamentals/Functions - Exercise/06. Sort.py
+++ Python-Fundamentals/Functions - Exercise/06. Sort.py
@@ -1,7 +1,3 @@
-def num_sort():
-    nums_input = input().split()
-    for i in range(len(nums_input)):
-        nums_input[i] = int(nums_input[i])
-    sorted_nums = sorted(nums_input)
-    print(sorted_nums)
-num_sort()
+def num_sort(nums_input):
+    return sorted(nums_input)
+print(num_sort([int(i) for i in input().split()]))
