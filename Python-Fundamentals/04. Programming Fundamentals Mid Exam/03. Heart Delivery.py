bb82896b762bcfb5c2b8831da3579f118de147d5
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-06 22:54:18 +0200

    Update 03. Heart Delivery.py

--- Python-Fundamentals/04. Programming Fundamentals Mid Exam/03. Heart Delivery.py
+++ Python-Fundamentals/04. Programming Fundamentals Mid Exam/03. Heart Delivery.py
@@ -1,6 +1,26 @@
 couples = [int(i) for i in input().split("@")]
 command = input()
+jump = 0
 
 while command != "Love!":
     command = command.split()
+    love_index = int(command[1]) + jump
+
     if command[0] == "Jump":
+        if love_index >= len(couples):
+            love_index = 0
+        if couples[love_index] == 0:
+            print(f"Place {love_index} already had Valentine's day.")
+        else:
+            couples[love_index] -= 2
+            if couples[love_index] == 0:
+                print(f"Place {love_index} has Valentine's day.")
+    jump = love_index
+    command = input()
+
+print(f"Cupid's last position was {love_index}.")
+if sum(couples) == 0:
+    print("Mission was successful.")
+else:
+    fail_count = len(list(filter(lambda x: (x > 0), couples)))
+    print(f"Cupid has failed {fail_count} places.")
