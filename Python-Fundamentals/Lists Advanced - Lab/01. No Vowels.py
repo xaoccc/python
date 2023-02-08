32b84a3ffb6c4d54b03d935aaa77c2b2fbb117ea
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-08 19:16:45 +0200

    Update 01. No Vowels.py

--- Python-Fundamentals/Lists Advanced - Lab/01. No Vowels.py
+++ Python-Fundamentals/Lists Advanced - Lab/01. No Vowels.py
@@ -1,8 +1,4 @@
 input_text = input()
 skip_letters = ["a", "e", "o", "u", "i", "A", "E", "O", "U", "I"]
-output_text = []
-
-for i in input_text:
-    if i not in skip_letters:
-        output_text.append(i)
+output_text = [i for i in input_text if i not in skip_letters]
 print("".join(output_text))
