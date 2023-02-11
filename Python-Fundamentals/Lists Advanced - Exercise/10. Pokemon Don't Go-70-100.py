b3aa3eab870c37af98b32b457c2cae14be0d5d86
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-02-11 23:09:43 +0200

    Update 10. Pokemon Don't Go-70-100.py

--- Python-Fundamentals/Lists Advanced - Exercise/10. Pokemon Don't Go-70-100.py
+++ Python-Fundamentals/Lists Advanced - Exercise/10. Pokemon Don't Go-70-100.py
@@ -1,27 +1,27 @@
 pokemons = [int(i) for i in input().split()]
 removed = 0
 
-while len(pokemons) > 0:
+while pokemons:
     pokemon_index = int(input())
     if pokemon_index >= len(pokemons):
+        current = pokemons[-1]
         removed += pokemons[-1]
-        pokemons = [i + pokemons[0] for i in pokemons]
-        pokemons.pop(-1)
-        pokemons.append(pokemons[0])
+        pokemons[-1] = pokemons[0]
+
     elif pokemon_index < 0:
+        current = pokemons[0]
         removed += pokemons[0]
         pokemons[0] = pokemons[-1]
-        pokemons.pop(-1)
+
     else:
         current = pokemons[pokemon_index]
         pokemons.pop(pokemon_index)
-        for i in range(len(pokemons)):
-            if pokemons[i] <= current:
-                pokemons[i] += current
-            else:
-                pokemons[i] -= current
         removed += current
-    print(pokemons)
 
+    for i in range(len(pokemons)):
+        if pokemons[i] <= current:
+            pokemons[i] += current
+        else:
+            pokemons[i] -= current
 
 print(removed)
