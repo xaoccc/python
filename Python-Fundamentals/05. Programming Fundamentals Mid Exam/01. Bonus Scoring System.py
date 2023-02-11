7ae25161960e75b58ec5cfbe807e56c5139dca67
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-11 21:48:15 +0200

    Update 01. Bonus Scoring System.py

--- Python-Fundamentals/05. Programming Fundamentals Mid Exam/01. Bonus Scoring System.py
+++ Python-Fundamentals/05. Programming Fundamentals Mid Exam/01. Bonus Scoring System.py
@@ -10,5 +10,5 @@ for i in range(students_num):
     if current_bonus > max_bonus:
         max_bonus = current_bonus
         max_bonus_att = attend_num
-print(f"Max Bonus: {round(max_bonus)}.")
+print(f"Max Bonus: {ceil(max_bonus)}.")
 print(f"The student has attended {max_bonus_att} lectures.")
