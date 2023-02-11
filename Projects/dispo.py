422d21c63eb0fe0077277bc5669c32b6525522c9
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-02-11 16:35:35 +0200

    lines 13-14 elif pokemon_index < 0:

---
+++ Projects/dispo.py
@@ -0,0 +1,43 @@
+class Dispo:
+    total_km = 0
+
+    def __init__(self):
+        self.truck = input("Enter plate number:")
+        self.driver = input("Enter driver name:")
+        self.total_km = 0
+        self.avg_km = 0
+        self.days = 0
+        self.km = -1
+
+    def driver_info(self):
+        return self.driver
+
+    def truck_info(self):
+        return self.truck
+
+    def get_km(self):
+        while self.km != 0:
+            self.km = int(input("Enter km:"))
+            self.total_km += self.km
+            self.days += 1
+
+    def show_km(self):
+        return self.total_km
+
+    def avg_km_(self):
+        return self.total_km / self.days
+
+
+all_drivers = []
+
+drivers_num = int(input("Number of trucks:"))
+for i in range(drivers_num):
+    truck_data = []
+    dispo = Dispo()
+    dispo.get_km()
+    truck_data.append(dispo.driver_info())
+    truck_data.append(dispo.truck_info())
+    truck_data.append(dispo.show_km())
+    truck_data.append(dispo.avg_km_())
+    all_drivers.append(truck_data)
+print(all_drivers)
--- Python-Fundamentals/Lists Advanced - Exercise/10. Pokemon Don't Go-70-100.py
+++ Python-Fundamentals/Lists Advanced - Exercise/10. Pokemon Don't Go-70-100.py
@@ -1,5 +1,6 @@
 pokemons = [int(i) for i in input().split()]
 removed = 0
+
 while len(pokemons) > 0:
     pokemon_index = int(input())
     if pokemon_index >= len(pokemons):
@@ -9,16 +10,18 @@ while len(pokemons) > 0:
         pokemons.append(pokemons[0])
     elif pokemon_index < 0:
         removed += pokemons[0]
-        pokemons.pop(0)
-        pokemons.insert(0, pokemons[-1])
+        pokemons[0] = pokemons[-1]
+        pokemons.pop(-1)
     else:
         current = pokemons[pokemon_index]
         pokemons.pop(pokemon_index)
         for i in range(len(pokemons)):
-                if pokemons[i] <= current:
-                    pokemons[i] += current
-                else:
-                    pokemons[i] -= current
+            if pokemons[i] <= current:
+                pokemons[i] += current
+            else:
+                pokemons[i] -= current
         removed += current
+    print(pokemons)
+
 
 print(removed)
