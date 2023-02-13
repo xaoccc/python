5a24d6fe962882bb7b51fa0997f34b867e3464aa
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-13 16:12:22 +0200

    Create 01. Sino The Walker.py

---
+++ Python-Fundamentals/07. Programming Fundamentals Retake Exam - Part 1 - 6 January 2017/01. Sino The Walker.py
@@ -0,0 +1,12 @@
+start_time = [int(i) for i in input().split(":")]
+steps = int(input())
+speed = int(input())
+walk_seconds = steps * speed
+start_time_seconds = start_time[2] + start_time[1] * 60 + start_time[0] * 3600
+total_seconds = start_time_seconds + walk_seconds
+
+hours = total_seconds // 60 // 60 % 24
+minutes = total_seconds // 60 % 60
+seconds = total_seconds % 60
+
+print("Time Arrival: " + "{:02d}".format(hours) + ":" + "{:02d}".format(minutes) + ":" + "{:02d}".format(seconds))
