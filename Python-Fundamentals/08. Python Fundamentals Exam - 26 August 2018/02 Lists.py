42e36785acebe6b2d35c02cc25e9d46869509f68
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-13 21:44:01 +0200

    Update and rename 02 Lists 70-100.py to 02 Lists.py

--- Python-Fundamentals/08. Python Fundamentals Exam - 26 August 2018/02 Lists.py
+++ Python-Fundamentals/08. Python Fundamentals Exam - 26 August 2018/02 Lists.py
@@ -2,20 +2,16 @@ nums = input()
 
 while nums != "stop playing":
     unique = True
-    replace_i = []
-    replace_value = []
     nums = [int(i) for i in nums.split()]
 
 
     for i in range(len(nums)):
         if nums.count(nums[i]) > 1:
             unique = False
-            replace_i.append(i)
-            replace_value.append(nums[i] + 3)
-
-    for i in range(len(replace_i)):
-        if nums[replace_i[i]] % 2 != 0:
-            nums[replace_i[i]] = replace_value[i]
+    if not unique:
+        for i in range(len(nums)):
+            if nums[i] % 2 != 0:
+                nums[i] += 3
 
     if unique:
         for i in range(len(nums)):
@@ -24,6 +20,7 @@ while nums != "stop playing":
 
     sum_nums = sum(nums)
     nums = [str(i) for i in sorted(nums)]
+
     if unique:
         print(f"Unique list: {','.join(nums)}\nOutput: {sum_nums / len(nums):.2f}")
     else:
