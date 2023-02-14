b1d6a25bc23c51450ab80b1794a04e335f0257f3
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-14 12:27:10 +0200

    Update and rename 03. Descriptions 70-100.py to 03. Descriptions 80-100.py

--- Python-Fundamentals/08. Python Fundamentals Exam - 26 August 2018/03. Descriptions 80-100.py
+++ Python-Fundamentals/08. Python Fundamentals Exam - 26 August 2018/03. Descriptions 80-100.py
@@ -2,13 +2,15 @@ input_data = input()
 global_data = []
 
 while input_data != "make migrations":
+    if "." not in input_data:
+        break
+    input_data = input_data.split(".")
     person_data = []
     all_data = ["Name of the person: ", "Age of the person: ", "Birthdate of the person: "]
-    input_data = input_data.split(".")
     del input_data[-1]
     for i in range(3):
         person_data.append(input_data[i].split(" "))
-    if person_data[0][-2][0].isupper() and person_data[0][-1][0].isupper() and person_data[0][-2].isalpha() and person_data[0][-1].isalpha() and 9 < int(person_data[1][3]) < 100 and person_data[2][-1][2] == "-" and person_data[2][-1][5] == "-" and person_data[0][-3] == "is" and len(person_data[0][-1]) < 2:
+    if person_data[0][-2][0].isupper() and person_data[0][-1][0].isupper() and person_data[0][-2].isalpha() and person_data[0][-1].isalpha() and 9 < int(person_data[1][3]) < 100 and person_data[2][-1][2] == "-" and person_data[2][-1][5] == "-" and person_data[0][-3] == "is" and len(person_data[0][-1]) > 1:
         all_data[0] += person_data[0][-2] + " " + person_data[0][-1] + "."
         all_data[1] += person_data[1][3] + "."
         all_data[2] += person_data[2][-1] + "."
