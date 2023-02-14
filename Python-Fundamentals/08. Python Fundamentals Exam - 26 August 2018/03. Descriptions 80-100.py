54f52d1e6bdc5c243999d9d46a0ea0fcf0bc3bb1
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-14 12:48:08 +0200

    Update 03. Descriptions 80-100.py

--- Python-Fundamentals/08. Python Fundamentals Exam - 26 August 2018/03. Descriptions 80-100.py
+++ Python-Fundamentals/08. Python Fundamentals Exam - 26 August 2018/03. Descriptions 80-100.py
@@ -2,14 +2,20 @@ input_data = input()
 global_data = []
 
 while input_data != "make migrations":
-    if "." not in input_data:
-        break
+    if input_data.count(".") != 3:
+        input_data = input()
+        continue
     input_data = input_data.split(".")
     person_data = []
     all_data = ["Name of the person: ", "Age of the person: ", "Birthdate of the person: "]
     del input_data[-1]
     for i in range(3):
         person_data.append(input_data[i].split(" "))
+    
+    if person_data[0][-3] != "is" or person_data[0][-4] != "name":
+        input_data = input()
+        continue
+
     if person_data[0][-2][0].isupper() and person_data[0][-1][0].isupper() and person_data[0][-2].isalpha() and person_data[0][-1].isalpha() and 9 < int(person_data[1][3]) < 100 and person_data[2][-1][2] == "-" and person_data[2][-1][5] == "-" and person_data[0][-3] == "is" and len(person_data[0][-1]) > 1:
         all_data[0] += person_data[0][-2] + " " + person_data[0][-1] + "."
         all_data[1] += person_data[1][3] + "."
