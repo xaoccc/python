fbd34f6b7f721895296f87d2e0d364d4be5a849d
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-15 11:37:16 +0200

    Update 1.  Build a building.py

--- Python-Fundamentals/09. Python Fundamentals Exam - 24 March2019/1.  Build a building.py
+++ Python-Fundamentals/09. Python Fundamentals Exam - 24 March2019/1.  Build a building.py
@@ -1,1 +1,15 @@
-test
+budget = float(input())
+initial_capital = float(input())
+investors_num = int(input())
+
+investors_money_sum = initial_capital
+
+for i in range(1, investors_num + 1):
+    investors_money = float(input())
+    print(f"Investor {i} gave us {investors_money:.2f}.")
+    investors_money_sum += investors_money
+    if investors_money_sum >= budget:
+        print(f"We will manage to build it. Start now! Extra money - {investors_money_sum - budget:.2f}.")
+        break
+if investors_money_sum < budget:
+    print(f"Project can not start. We need {budget - investors_money_sum:.2f} more.")
