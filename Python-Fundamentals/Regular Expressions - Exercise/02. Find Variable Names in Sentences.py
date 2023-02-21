6764af55f2b217ed85894eaea84052be5f682e1f
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-22 00:06:32 +0200

    Update 02. Find Variable Names in Sentences.py

--- Python-Fundamentals/Regular Expressions - Exercise/02. Find Variable Names in Sentences.py
+++ Python-Fundamentals/Regular Expressions - Exercise/02. Find Variable Names in Sentences.py
@@ -1,23 +1,8 @@
-text_input = input()
-text_input_list = text_input.split(" ")
-variable_name = ""
-text_output_list = []
-print(text_input_list)
-
-for i in range(len(text_input_list)):
-    if text_input_list[i].find('_') == 0 and text_input_list[i].rfind('_') == 0 and (text_input_list[i][1:].isalnum() == True or text_input_list[i][1:].isalpha() == True or text_input_list[i][1:].isdigit() == True):
-
-        text_input_list[i] = text_input_list[i][1:]
-        text_output_list.append(text_input_list[i])
-
-print(",".join(text_output_list)) 
-
-
 import re
 text_input = input().split()
 scanner = ""
 for i in text_input:
-    scanner += ",".join(re.findall(r"^_[a-zA-Z0-9]+[a-zA-Z0-9]$", i))
+    scanner += ",".join(re.findall(r"^_[a-zA-Z0-9]+\b", i))
 scanner = scanner.split("_")
 del scanner[0]
 print(",".join(scanner))
