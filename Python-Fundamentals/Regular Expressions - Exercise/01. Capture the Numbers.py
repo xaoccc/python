c668ec5e822a7be0d84bdd135081ea6e29971db2
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-21 10:01:27 +0200

    Update 01. Capture the Numbers.py

--- Python-Fundamentals/Regular Expressions - Exercise/01. Capture the Numbers.py
+++ Python-Fundamentals/Regular Expressions - Exercise/01. Capture the Numbers.py
@@ -1,25 +1,9 @@
-import time
-
+import re
+input_data = []
+pattern = "\d+"
 
 while True:
-    num = ""
-    output = ""
-    time_one = time.time()
-    text = input()
-    time_two = time.time()
-    if time_two - time_one > 10:
-        print("Waited too long, exiting now...")
-        break
-    for i in range(len(text)):
-        if text[i].isdigit():
-            num += text[i]
-            if i == len(text) - 1:
-                output += num + " "
-                print(num)
-        else:
-            if num != "":
-                print(num)
-                output += num + " "
-                num = ""
-
-print(output) 
+    text_input = input()
+    input_data.append(text_input)
+    matches = re.findall(pattern, " ".join(input_data))
+print(" ".join(matches))
