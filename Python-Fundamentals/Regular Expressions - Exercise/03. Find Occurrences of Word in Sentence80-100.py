38b03dfb0012a92db66121ff9deb2c7952b46ee3
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-22 10:30:00 +0200

    Update 03. Find Occurrences of Word in Sentence80-100.py

--- Python-Fundamentals/Regular Expressions - Exercise/03. Find Occurrences of Word in Sentence80-100.py
+++ Python-Fundamentals/Regular Expressions - Exercise/03. Find Occurrences of Word in Sentence80-100.py
@@ -3,3 +3,16 @@ main_string = input().lower()
 pattern = input().lower()
 scanner = re.findall(pattern, main_string)
 print(len(scanner))
+
+
+import re
+import string
+main_string = input().lower().translate(str.maketrans("", "", string.punctuation)).split()
+pattern = input().lower()
+print(main_string.count(pattern))
+
+import re
+main_string = input().lower()
+pattern = input().lower()
+res = re.sub(r'[^\w\s]', ' ', main_string).split()
+print(res.count(pattern))
