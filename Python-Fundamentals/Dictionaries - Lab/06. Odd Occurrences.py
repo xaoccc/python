ca39d53f17d2507a8a20d3d03f9eebd63fb96857
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-23 14:18:01 +0200

    Update 06. Odd Occurrences.py

--- Python-Fundamentals/Dictionaries - Lab/06. Odd Occurrences.py
+++ Python-Fundamentals/Dictionaries - Lab/06. Odd Occurrences.py
@@ -10,3 +10,17 @@ for i in words:
 for i in dictionary:
     if len(dictionary[i]) % 2 != 0:
         print(i, end=" ")
+
+        
+# words = input().split()
+# dictionary = {}
+
+# for i in words:
+#     i = i.lower()
+#     if i not in dictionary:
+#         dictionary[i] = 0
+#     dictionary[i] += 1
+
+# for (key, value) in dictionary.items():
+#     if value % 2 != 0:
+#         print(key, end=" ")
