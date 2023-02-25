7c02dbae950ea3fb1fccb9209171c9ae56a1f4fd
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-25 15:39:23 +0200

    Create 11. Force Book66-100.py

---
+++ Python-Fundamentals/Dictionaries - Exercise/11. Force Book66-100.py
@@ -0,0 +1,33 @@
+command = input()
+all_users = {}
+
+
+while command != "Lumpawaroo":
+    if "|" in command:
+        command = command.split(" | ")
+        force_user = command[1]
+        force_side = command[0]
+        if force_side not in all_users:
+            all_users[force_side] = []
+            if force_user not in all_users:
+                all_users[force_side].append(force_user)
+
+    else:
+        command = command.split(" -> ")
+        force_user = command[0]
+        force_side = command[1]
+        if force_side not in all_users:
+            all_users[force_side] = []
+        for (key, value) in all_users.items():
+            if force_user in value:
+                value.remove(force_user)
+        all_users[force_side].append(force_user)
+        print(f"{force_user} joins the {force_side} side!")
+
+    command = input()
+
+for (key, value) in all_users.items():
+    if len(value) > 0:
+        print(f"Side: {key}, Members: {len(value)}")
+        for i in value:
+            print(f"! {i}")
