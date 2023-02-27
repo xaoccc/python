92812a91fa6ef15db1e992b6455359a82dfab509
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-27 16:11:13 +0200

    Update 02. Judge0-100.py

--- Python-Fundamentals/Dictionaries - More Exercise/02. Judge0-100.py
+++ Python-Fundamentals/Dictionaries - More Exercise/02. Judge0-100.py
@@ -1,5 +1,6 @@
 command = input()
 all_data = {}
+contest_data = {}
 
 while command != "no more time":
     command = command.split(" -> ")
@@ -9,21 +10,40 @@ while command != "no more time":
     if username not in all_data:
         all_data[username] = {}
         all_data[username][contest] = points
+        
+    if contest not in contest_data:
+        contest_data[contest] = {}
+        contest_data[contest][username] = points
     
     for i in all_data.copy():
         for j in all_data[i].copy():
-            print(j)
-            print(all_data[i][j])
-            
             if contest not in j:
                 all_data[username][contest] = points
             else:
-                if points > all_data[i][j]:
+                if username == i and points > all_data[i][j]:
                     all_data[i][j] = points
                     break
-            
-            
+    
+    for i in contest_data.copy():
+        for j in contest_data[i].copy():
+            if username not in j:
+                contest_data[contest][username] = points
+            else:
+                if points > contest_data[i][j]:
+                    contest_data[i][j] = points
+                    break       
             
     command = input()
+sorted_data = {}
+
+for i in all_data: 
+    sorted_data = dict(sorted(all_data[i].items(), key=lambda y:y[1], reverse=True))        
+    for (key, value) in sorted_data.items():
+        print(f"#{i}  {key} -> {value}") 
     
-print(all_data)
+print(contest_data)
+
+for i in contest_data: 
+    sorted_data = dict(sorted(contest_data[i].items(), key=lambda y:y[1], reverse=True))        
+    for (key, value) in sorted_data.items():
+        print(f"#{i}  {key} -> {value}") 
