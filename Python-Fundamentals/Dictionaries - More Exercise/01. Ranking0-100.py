12fb427e26e32af5b5e6228513257be53ae0f5b1
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-02-28 11:04:37 +0200

    Update 01. Ranking0-100.py

--- Python-Fundamentals/Dictionaries - More Exercise/01. Ranking0-100.py
+++ Python-Fundamentals/Dictionaries - More Exercise/01. Ranking0-100.py
@@ -1,49 +1,29 @@
-command = input()
-all_data = {}
+passwords = {}
 contest_data = {}
 
-while command != "no more time":
-    command = command.split(" -> ")
-    username = command[0]
-    contest = command[1]
-    points = int(command[2])
-    if username not in all_data:
-        all_data[username] = {}
-        all_data[username][contest] = points
-        
-    if contest not in contest_data:
-        contest_data[contest] = {}
-        contest_data[contest][username] = points
-    
-    for i in all_data.copy():
-        for j in all_data[i].copy():
-            if contest not in j:
-                all_data[username][contest] = points
-            else:
-                if points > all_data[i][j]:
-                    all_data[i][j] = points
-                    break
-    
-    for i in contest_data.copy():
-        for j in contest_data[i].copy():
-            if username not in j:
-                contest_data[contest][username] = points
-            else:
-                if points > contest_data[i][j]:
-                    contest_data[i][j] = points
-                    break       
-            
+command = input()
+while command != "end of contests":
+    command = command.split(":")
+    contest = command[0]
+    password = command[1]
+    if contest not in passwords:
+        passwords[contest] = password
     command = input()
-sorted_data = {}
-
-for i in all_data: 
-    sorted_data = dict(sorted(all_data[i].items(), key=lambda y:y[1], reverse=True))        
-    for (key, value) in sorted_data.items():
-        print(f"#{i}  {key} -> {value}") 
     
-print(contest_data)
 
-for i in contest_data: 
-    sorted_data = dict(sorted(contest_data[i].items(), key=lambda y:y[1], reverse=True))        
-    for (key, value) in sorted_data.items():
-        print(f"#{i}  {key} -> {value}") 
+contest_input = input()
+while contest_input != "end of submissions":
+    contest_input = contest_input.split("=>")
+    if len(contest_input) == 4:
+        contest = contest_input[0]
+        password = contest_input[1]
+        username = contest_input[2]
+        points = int(contest_input[3])
+        if contest in passwords:
+            if password == passwords[contest]:
+                
+
+        
+        contest_input = input()
+    else:
+        contest_input = input()
