a0478f128dee1b71b027a9a1c820751860409014
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-02 11:27:15 +0200

    Update 03. MOBA Challenger 80-100.py

--- Python-Fundamentals/Dictionaries - More Exercise/03. MOBA Challenger 80-100.py
+++ Python-Fundamentals/Dictionaries - More Exercise/03. MOBA Challenger 80-100.py
@@ -1,36 +1,35 @@
 command = input()
-players_data = {}
-total_skills = {}
-sorted_data = {}
-sorted_total = {}
+players_data, total_skills, sorted_data, sorted_total  = {}, {}, {}, {}
+
+def add_players(player, position, skill):
+    if player not in players_data:
+        players_data[player] = position_skill
+    else:
+        if position not in players_data[player] or skill > players_data[player][position]:
+            players_data[player][position] = skill
+            
+def duel(player1, player2):
+    for position1 in players_data[player1]:
+        for position2 in players_data[player2]:
+            if position1 == position2:
+                if sum(list(players_data[player1].values())) > sum(list(players_data[player2].values())):
+                    players_data.pop(player2)
+                elif sum(list(players_data[player1].values())) < sum(list(players_data[player2].values())):
+                    players_data.pop(player1)
+                break
 
 while command != "Season end":
     if " -> " in command:
-        command = command.split(" -> ")
         position_skill = {}
-        player = command[0]
-        position = command[1]
-        skill = int(command[2])
+        player, position, skill = [int(element) if element.isdigit() else element for element in command.split(" -> ")]
         position_skill[position] = skill
-        if player not in players_data:
-            players_data[player] = position_skill
-        else:
-            if position not in players_data[player] or skill > players_data[player][position]:
-                players_data[player][position] = skill
+        add_players(player, position, skill)
         
     else:
-        command = command.split(" vs ")
-        player1 = command[0]
-        player2 = command[1]
+        player1, player2 = command.split(" vs ")
         if player1 in players_data and player2 in players_data:
-            for position1 in players_data[player1]:
-                for position2 in players_data[player2]:
-                    if position1 == position2:
-                        if sum(list(players_data[player1].values())) > sum(list(players_data[player2].values())):
-                            players_data.pop(player2)
-                        elif sum(list(players_data[player1].values())) < sum(list(players_data[player2].values())):
-                            players_data.pop(player1)
-                        break
+            duel(player1, player2)
+
     command = input()
     
 for player in  players_data:
