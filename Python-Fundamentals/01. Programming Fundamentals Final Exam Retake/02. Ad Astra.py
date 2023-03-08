59c1c71e31b6e1501a47a8590d12cfda070d5770
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-08 10:47:09 +0200

    Update 02. Ad Astra.py

--- Python-Fundamentals/01. Programming Fundamentals Final Exam Retake/02. Ad Astra.py
+++ Python-Fundamentals/01. Programming Fundamentals Final Exam Retake/02. Ad Astra.py
@@ -7,7 +7,6 @@ if text[0] == "#" or text[0] == "|":
 if text[-1] == "#" or text[-1] == "|":
     text = text[ :-1]
 
-    
 if "||" in text:
     text = text.replace("||", "divide!!!")
 if "#|" in text:
@@ -21,14 +20,15 @@ for i in text:
     if "#" in i:
         i = i.split("#")
         if len(i) == 3:
+            food_test = re.match(r'[a-zA-Z ]+$', i[0])
             date_test = re.match(r'^\d{2}\/\d{2}\/\d{2}$', i[1])
-            if date_test:
+            if date_test and food_test:
                 valid_data.append(i) 
-        print(i)
     else:
         i = i.split("|")
         if len(i) == 3:
+            food_test = re.match(r'[a-zA-Z ]+$', i[0])
             date_test = re.match(r'^\d{2}\/\d{2}\/\d{2}$', i[1])
-            if date_test:
+            if date_test and food_test:
                 valid_data.append(i) 
 print(valid_data)
