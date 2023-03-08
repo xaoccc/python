fa0fd42e66552fbb18cb756b52a67fc72cc88a28
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-08 10:42:00 +0200

    Update 02. Ad Astra.py

--- Python-Fundamentals/01. Programming Fundamentals Final Exam Retake/02. Ad Astra.py
+++ Python-Fundamentals/01. Programming Fundamentals Final Exam Retake/02. Ad Astra.py
@@ -1,9 +1,11 @@
-text = "#Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|"
+import re
+text = input()
 valid_data = []
+
 if text[0] == "#" or text[0] == "|":
     text = text[1: ]
 if text[-1] == "#" or text[-1] == "|":
-    text = text[:-1 ]
+    text = text[ :-1]
 
     
 if "||" in text:
@@ -19,10 +21,14 @@ for i in text:
     if "#" in i:
         i = i.split("#")
         if len(i) == 3:
-            valid_data.append(i) 
+            date_test = re.match(r'^\d{2}\/\d{2}\/\d{2}$', i[1])
+            if date_test:
+                valid_data.append(i) 
         print(i)
     else:
         i = i.split("|")
         if len(i) == 3:
-            valid_data.append(i) 
+            date_test = re.match(r'^\d{2}\/\d{2}\/\d{2}$', i[1])
+            if date_test:
+                valid_data.append(i) 
 print(valid_data)
