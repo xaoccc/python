41b4fb0a902f93655f789313e7b51cdcfbaefce2
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-08 11:12:57 +0200

    Update 02. Ad Astra.py

--- Python-Fundamentals/01. Programming Fundamentals Final Exam Retake/02. Ad Astra.py
+++ Python-Fundamentals/01. Programming Fundamentals Final Exam Retake/02. Ad Astra.py
@@ -3,3 +3,17 @@ text = input()
 valid_data = []
 
 data_test = re.findall(r'[#|][a-zA-Z ]+[|#]\d{2}\/\d{2}\/\d{2}[|#][0-9]+[#|]', text)
+
+for i in data_test:
+    current_data = []
+    if i[0] == "#" and i[-1] == "#":
+        
+        current_data = i.split("#")
+        if len(current_data) == 5:
+            valid_data.append(current_data[1:4])
+    elif i[0] == "|" and i[-1] == "|":
+        current_data = i.split("|")
+        if len(current_data) == 5:
+            valid_data.append(current_data[1:4])
+
+print(valid_data)
