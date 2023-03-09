4eaac6e9a51abd98efa3543ba70b078a9c559da5
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-09 09:34:32 +0200

    Update 01. Reverse Strings.py

--- Python-Fundamentals/Text Processing - Lab/01. Reverse Strings.py
+++ Python-Fundamentals/Text Processing - Lab/01. Reverse Strings.py
@@ -2,3 +2,9 @@ sometext = input()
 while sometext != "end":
     print(f"{sometext} = {''.join(list(reversed(sometext)))}")
     sometext = input()
+
+    
+# sometext = input()
+# while sometext != "end":
+#     print(f"{sometext} = {sometext[::-1]}")
+#     sometext = input()
