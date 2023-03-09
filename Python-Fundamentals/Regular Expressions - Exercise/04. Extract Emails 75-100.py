b347b144a12e971053bce01d192d589373ed6d77
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-09 11:47:29 +0200

    Update 04. Extract Emails 75-100.py

--- Python-Fundamentals/Regular Expressions - Exercise/04. Extract Emails 75-100.py
+++ Python-Fundamentals/Regular Expressions - Exercise/04. Extract Emails 75-100.py
@@ -1,7 +1,7 @@
 import re
 
 text_input = input().split()
-pattern = re.compile(r'^([A-Za-z0-9])+([\.\-\_][A-Za-z0-9]+)*@([A-Za-z-]+)\.([A-Za-z-]+)([\.A-Za-z-])*[\.A-Za-z]$')
+pattern = re.compile('^[A-Za-z0-9]+([\.\-\_][A-Za-z0-9]+)*@[A-Za-z-]+\.[A-Za-z-]+[\.A-Za-z-]*[\.A-Za-z]$')
 
 for i in text_input:
     for match in re.finditer(pattern, i):
