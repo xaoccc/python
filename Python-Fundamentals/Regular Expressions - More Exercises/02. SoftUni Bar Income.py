3fe97d2f879f812c50e3eff81c3c272b0f928207
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-13 16:55:25 +0200

    Update 02. SoftUni Bar Income.py

--- Python-Fundamentals/Regular Expressions - More Exercises/02. SoftUni Bar Income.py
+++ Python-Fundamentals/Regular Expressions - More Exercises/02. SoftUni Bar Income.py
@@ -7,10 +7,10 @@ while sale_info != "end of shift":
     client_name = re.findall(r'(?<=\%)[A-Z][a-z]+(?=\%)', sale_info)
     product = re.findall(r'(?<=\<)\w+(?=\>)', sale_info)
     qty = re.search(r'(?<=\|)\d+(?=\|)', sale_info)
-    price = re.search(r'(([0]|[1-9][0-9]*)\.[0-9])|([0]|[1-9][0-9]*)+(?=\$)', sale_info)
+    price = re.search(r'(([0]|[1-9][0-9]*)\.[0-9]+\$)|([0]\$|[1-9][0-9]*\$)', sale_info)
     if len(client_name) == 1 and len(product) == 1 and qty and price:
-        print(f"{client_name[0]}: {product[0]} - {int(qty.group()) * float(price.group()):.2f}")
-        total += int(qty.group()) * float(price.group())
+        print(f"{client_name[0]}: {product[0]} - {int(qty.group()) * float(price.group()[:-1]):.2f}")
+        total += int(qty.group()) * float(price.group()[:-1])
 
     sale_info = input()
 print(f"Total income: {total:.2f}")
