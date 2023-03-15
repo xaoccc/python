61f7708f65f542f4e5334ffd2d671704ca55cf1d
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-15 12:03:59 +0200

    Create 03. Heroes of Code and Logic VII.py

---
+++ Python-Fundamentals/04. Programming Fundamentals Final Exam/03. Heroes of Code and Logic VII.py
@@ -0,0 +1,28 @@
+heroes_num = int(input())
+heroes = {}
+
+for i in range(heroes_num):
+    hero = input().split()
+    heroes[hero[0]] = [hero[1], hero[2]]
+print(heroes)
+heroes[Kyrree][0] = 69
+print(heroes)
+command = input()
+
+# while command != "End":
+#     command = command.split(" - ")
+#     if command[0] == "CastSpell":
+#         if command[1] in heroes.keys() and int(heroes[command[1]][1]) >= int(command[2]):
+#             int(heroes[command[1]][1]) -= int(command[2])
+#             print(f"{command[1]} has successfully cast {command[3]} and now has {int(heroes[command[1]][1])} MP!")
+#         else:
+#             print(f"{command[1]} does not have enough MP to cast {command[3]}!")
+#     elif command[0] == "TakeDamage":
+#         pass
+#     elif command[0] == "Reachrge":
+#         pass
+#     elif command[0] == "Heal":
+#         pass
+    
+    
+    # command = input()
