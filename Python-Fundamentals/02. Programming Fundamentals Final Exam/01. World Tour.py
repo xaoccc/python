5aefcd85dd792479142ba0dd75812e6a96ded2cf
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-16 10:00:52 +0200

    Update and rename 01. World Tour83-100.py to 01. World Tour.py

--- Python-Fundamentals/02. Programming Fundamentals Final Exam/01. World Tour.py
+++ Python-Fundamentals/02. Programming Fundamentals Final Exam/01. World Tour.py
@@ -1,12 +1,12 @@
+
 stops = input()
 command = input().split(":")
 while command[0] != "Travel":    
     if command[0] == "Add Stop":
         if 0 <= int(command[1]) < len(stops):
             stops = stops[ :int(command[1])] + command[2] + stops[int(command[1]): ]
-    elif command[0] == "Remove Stop":
-        if 0 <= int(command[1]) < len(stops) and 0 <= int(command[1]) < len(stops):
-            stops = stops[ :int(command[1])] + stops[int(command[2]) + 1: ]
+    elif command[0] == "Remove Stop" and 0 <= int(command[1]) < len(stops) and 0 <= int(command[2]) < len(stops):
+        stops = stops[ :int(command[1])] + stops[int(command[2]) + 1: ]
     elif command[0] == "Switch":
         stops = stops.replace(command[1], command[2])
     print(stops)
