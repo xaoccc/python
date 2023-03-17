db8f63651d7e5b585d0f3d4ca7df5a616c026acc
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-17 19:33:10 +0200

    Update 03. Find Occurrences of Word in Sentence.py

--- Python-Fundamentals/Regular Expressions - Exercise/03. Find Occurrences of Word in Sentence.py
+++ Python-Fundamentals/Regular Expressions - Exercise/03. Find Occurrences of Word in Sentence.py
@@ -4,3 +4,10 @@ check_string = input().lower()
 pattern = r'(^|(?<=[\W\_]))' + re.escape(check_string) + r'($|(?=[\W\_]))'
 res = re.findall(pattern, main_string)
 print(len(res))
+
+
+# import re
+# main_string = input().lower()
+# check_string = input().lower()
+# matches = re.findall(rf"\b{check_string}\b", main_string)
+# print(len(matches))
