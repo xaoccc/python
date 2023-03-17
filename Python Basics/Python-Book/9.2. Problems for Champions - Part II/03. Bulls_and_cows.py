a40230a407af152d3cdc77837b7f9bdeb93520a7
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-17 17:06:11 +0200

    Update 03. Bulls_and_cows.py

--- Python Basics/Python-Book/9.2. Problems for Champions - Part II/03. Bulls_and_cows.py
+++ Python Basics/Python-Book/9.2. Problems for Champions - Part II/03. Bulls_and_cows.py
@@ -81,7 +81,86 @@ for digit_1 in range(1, 10):
                     print(f"{digit_1}{digit_2}{digit_3}{digit_4}", end="")
                     solution_found = True
                 
-if not solution_found:
-    print("No")
+# if not solution_found:
+#     print("No")
 
+# num = input()
+# bulls = int(input())
+# cows = int(input())
+
+# solution_found = False
+
+# for i in range(1111, 10000):
+#     i = str(i)
+#     if "0" in i:
+#         continue
+#     guess_digit_1, guess_digit_2, guess_digit_3, guess_digit_4 = num[0], num[1], num[2], num[3]
+#     digit_to_check_1, digit_to_check_2, digit_to_check_3, digit_to_check_4 = i[0], i[1], i[2], i[3]
     
+#     bulls_num = 0
+#     cows_num = 0
+#     if digit_to_check_1 == guess_digit_1:
+#         bulls_num += 1
+#         guess_digit_1 = "x"
+#         digit_to_check_1 = "y"
+#     if digit_to_check_2 == guess_digit_2:
+#         bulls_num += 1
+#         guess_digit_2 = "x"
+#         digit_to_check_2 = "y"
+#     if digit_to_check_3 == guess_digit_3:
+#         bulls_num += 1
+#         guess_digit_3 = "x"
+#         digit_to_check_3 = "y"
+#     if digit_to_check_4 == guess_digit_4:
+#         bulls_num += 1
+#         guess_digit_4 = "x"
+#         digit_to_check_4 = "y"
+        
+#     if digit_to_check_1 == guess_digit_2:
+#         cows_num += 1
+#         guess_digit_2 = "x"
+#     elif digit_to_check_1 == guess_digit_3:
+#         cows_num += 1
+#         guess_digit_3 = "x"
+#     elif digit_to_check_1 == guess_digit_4:
+#         cows_num += 1
+#         guess_digit_4 = "x"
+        
+#     if digit_to_check_2 == guess_digit_1:
+#         cows_num += 1
+#         guess_digit_1 = "x"
+#     elif digit_to_check_2 == guess_digit_3:
+#         cows_num += 1
+#         guess_digit_3 = "x"
+#     elif digit_to_check_2 == guess_digit_4:
+#         cows_num += 1
+#         guess_digit_4 = "x"
+        
+#     if digit_to_check_3 == guess_digit_1:
+#         cows_num += 1
+#         guess_digit_1 = "x"
+#     elif digit_to_check_3 == guess_digit_2:
+#         cows_num += 1
+#         guess_digit_2 = "x"
+#     elif digit_to_check_3 == guess_digit_4:
+#         cows_num += 1
+#         guess_digit_4 = "x"
+        
+#     if digit_to_check_4 == guess_digit_1:
+#         cows_num += 1
+#         guess_digit_1 = "x"
+#     elif digit_to_check_4 == guess_digit_2:
+#         cows_num += 1
+#         guess_digit_2 = "x"
+#     elif digit_to_check_4 == guess_digit_3:
+#         cows_num += 1
+#         guess_digit_3 = "x"
+    
+#     if cows == cows_num and bulls == bulls_num:
+#         if solution_found:
+#             print(" ", end="")
+#         print(i, end="")
+#         solution_found = True
+                
+# if not solution_found:
+#     print("No")  
