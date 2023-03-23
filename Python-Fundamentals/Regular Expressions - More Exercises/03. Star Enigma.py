119e1cf533ecc8107fca1860c29c5c175f093ae2
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-23 17:19:10 +0200

    Update 03. Star Enigma.py

--- Python-Fundamentals/Regular Expressions - More Exercises/03. Star Enigma.py
+++ Python-Fundamentals/Regular Expressions - More Exercises/03. Star Enigma.py
@@ -1,37 +1,32 @@
 import re
-message_num = int(input())
-decode_data = ["s", "t", "a", "r", "S", "T", "A", "R"]
-attacked, destroyed = [], []
+planets_num = int(input())
+decode_data = ["s", "t", "a", "r"]
+attacked, destroyed, planets = [], [], []
+pattern = r"@(?P<planet>[a-zA-Z]+)([^\@\-\!\:\>]*):(?P<population>[0-9]+)([^\@\-\!\:\>]*)(\!)(?P<attack>[A,D])!([^\@\-\!\:\>]*)->(?P<army>[0-9]+)"
 
-#for each compile group/name we add starting chars equal to the sum of all starting chars @!:-> Logic is that after each group we have next group.
-planet = re.compile(
-    r"@(?P<planet>[a-zA-Z]+)([^\@\-\!\:\>]*)" 
-    r":(?P<population>[0-9]+)([^\@\-\!\:\>]*)" 
-    r"(\!)(?P<attack>[A,D])!([^\@\-\!\:\>]*)"
-    r"->(?P<army>[0-9]+)")
+for i in range(planets_num):
+    count = 0
+    planet_name = ""
+    planet = input()
+    for j in planet:
+        if j.lower() in decode_data:
+            count += 1
+    for j in planet:
+        planet_name += chr(ord(j) - count)
+    planets.append(planet_name)
 
-for i in range(message_num):
-    decode_chars = 0
-    decoded_message = ""
-    message = input()
-    for char in message:
-        if char in decode_data:
-            decode_chars += 1
-    for char in message:
-        decoded_message += chr(ord(char) - decode_chars)
-    result = re.finditer(planet, decoded_message)
-    
-    for show in result:
-        if show["attack"] == "A":
-            attacked.append(show["planet"])
-        elif show["attack"] == "D":
-            destroyed.append(show["planet"])
-            
-attacked, destroyed = sorted(attacked), sorted(destroyed)
-       
+for p in planets:
+    match = re.finditer(pattern, p)
+    for i in match:
+        if i["attack"] == "A":
+            attacked.append(i["planet"])
+        elif i["attack"] == "D":
+            destroyed.append(i["planet"])
+attacked = sorted(attacked)
+destroyed = sorted(destroyed)
 print(f"Attacked planets: {len(attacked)}")
-for i in attacked:
-    print(f"-> {i}")
+for planet in attacked:
+    print(f"-> {planet}")
 print(f"Destroyed planets: {len(destroyed)}")
-for i in destroyed:
-    print(f"-> {i}")
+for planet in destroyed:
+    print(f"-> {planet}") 
