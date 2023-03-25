1e132bf9f93632e17c8fec675f32a69fae2b099b
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-25 23:09:59 +0200

    Update 05. Dots 40-100.py

--- Python-Fundamentals/Lists Advanced - More Exercises/05. Dots 40-100.py
+++ Python-Fundamentals/Lists Advanced - More Exercises/05. Dots 40-100.py
@@ -2,11 +2,9 @@ size = int(input())
 chars_list = []
 max_count = 0
 current_count = 0
-
 for i in range(size):
     current_row = input().split()
     chars_list.append(current_row)
-
 for i in range(len(chars_list)):
     if "." in chars_list[i]:
         for j in range(len(current_row)):
