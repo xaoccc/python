db6183e3596331150a93cff9d64a66f7be9672b5
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-27 10:54:37 +0300

    Update 02. Emoji Detector.py

--- Python-Fundamentals/05. Programming Fundamentals Final Exam/02. Emoji Detector.py
+++ Python-Fundamentals/05. Programming Fundamentals Final Exam/02. Emoji Detector.py
@@ -18,3 +18,25 @@ print(f"Cool threshold: {cool_threshold}\n{emojis_count} emojis found in the tex
 for i in cool_emojis:
     print(i)
 
+
+    
+# import re
+# text = input()
+# cool_threshold, cool_emojis, emojis_count = 1, [], 0
+# pattern = r"(::|\*\*)(?P<emojiname>[A-Z][a-z]{2,})\1"
+# match = re.finditer(pattern, text)
+
+# for num in text:
+#     if num.isdigit():
+#         cool_threshold *= int(num)
+        
+# for emoji in match:
+#     emojis_count += 1
+#     emoji_coolness = 0
+#     for char in emoji["emojiname"]:
+#         emoji_coolness += ord(char)
+#     if emoji_coolness >= cool_threshold:
+#         cool_emojis.append(emoji.group())
+# print(f"Cool threshold: {cool_threshold}\n{emojis_count} emojis found in the text. The cool ones are:")
+# for i in cool_emojis:
+#     print(i)
