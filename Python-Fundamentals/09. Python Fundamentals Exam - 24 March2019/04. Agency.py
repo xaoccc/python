a3f810d22f75885466cbb4d6a3c77bf67d578dec
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-28 11:54:20 +0300

    Create 04. Agency.py

---
+++ Python-Fundamentals/09. Python Fundamentals Exam - 24 March2019/04. Agency.py
@@ -0,0 +1,59 @@
+import re
+pattern = r"(?P<type>[A-Za-z]+)\((?P<data>.+)\)"
+all_data = {}
+taken = {}
+db = []
+
+command = input()
+while command != "start_selling":
+    match = re.match(pattern, command)
+    current = match["data"].replace('"', "").split(", ")
+    
+    if (match["type"] == "OfficeApartment" or match["type"] == "LivingApartment") and len(current) < 5:
+        print("__init__() missing 1 required positional argument: 'price'")
+    elif match["type"] == "Apartment":
+        print("Can't instantiate abstract class Apartment with abstract methods __str__")
+        
+    else:
+        if match["type"] not in all_data:
+            all_data[match["type"]] = {current[0]: current[1:]}
+        else:
+            all_data[match["type"]][current[0]] = current[1:]
+        db.append(current[0])
+
+    command = input()
+
+action = input()
+while action != "taken" and action != "free":
+    action = action.split()
+    if action[1] not in db:
+        print(f"Apartment with id - {action[1]} does not exist!")
+    elif action[0] == "rent" and action[1] not in all_data["OfficeApartment"].keys():
+        print(f"Apartment with id - {action[1]} is only for selling!")
+    elif action[0] == "buy" and action[1] not in all_data["LivingApartment"].keys():
+        print(f"Apartment with id - {action[1]} is only for renting!")
+    elif action[0] == "rent" and action[1] in all_data["OfficeApartment"].keys():
+        if "OfficeApartment" not in taken.keys():
+            taken["OfficeApartment"] = {action[1]: all_data["OfficeApartment"][action[1]]}
+        else:
+            taken["OfficeApartment"][action[1]] = all_data["OfficeApartment"][action[1]]
+        del all_data["OfficeApartment"][action[1]]
+    elif action[0] == "buy" and action[1] in all_data["LivingApartment"].keys():
+        if "LivingApartment" not in taken.keys():
+            taken["LivingApartment"] = {action[1]: all_data["LivingApartment"][action[1]]}
+        else:
+            taken["LivingApartment"][action[1]] = all_data["LivingApartment"][action[1]]
+        del all_data["LivingApartment"][action[1]]
+    action = input()
+    
+print(taken)    
+if (action == "taken" and len(taken) == 0) or (action == "free" and len(all_data) == 0):
+    print("No information for this query")
+elif action == "taken":
+    for i in taken:
+        taken = dict(sorted(taken[i].items(), key = lambda x: (x[1][3], x[1][2])))
+    print(taken)
+
+# elif command == "free":
+#     for ap_type, ap_data in taken.items():
+#         print()
