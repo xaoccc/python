a7ee62b19cb39b259ba848b0deddd3b429b78968
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-03-29 22:06:29 +0300

    ready

--- Projects/djangoproject/djangoproject/phonebook/urls.py
+++ Projects/djangoproject/djangoproject/phonebook/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import landing_page
+from .views import landing_page, create_phone_number
 
 urlpatterns = [
-    path('', landing_page, name="landing-page")
+    path('', landing_page, name="landing-page"),
+    path('new-contact', create_phone_number, name="new-contact")
 ]
--- Projects/djangoproject/djangoproject/phonebook/views.py
+++ Projects/djangoproject/djangoproject/phonebook/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render, redirect
+from djangoproject.phonebook.models import Contact
 
 
 def landing_page(request):
-    return render(request, "phonebook/index.html")
+    context = {
+        'contacts': Contact.objects.all()
+    }
+    return render(request, "phonebook/index.html", context)
+
+
+def create_phone_number(request):
+    name = request.POST['name']
+    number = request.POST['number']
+    contact = Contact(name=name, number=number)
+    contact.save()
+    return redirect('landing-page')
+
+
+
