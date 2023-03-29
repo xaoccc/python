a70486290d5f16e167fc2459ec6d7b7202ff687a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-29 13:22:25 +0300

    Update calculator.py

--- Projects/Simple_Console_Calculator/calculator.py
+++ Projects/Simple_Console_Calculator/calculator.py
@@ -1,22 +1,14 @@
 import math
 
-num1_input = float(input())
+num1_input = float(input("Enter a number:"))
 result = num1_input
 while True:
 
-    command_input = input()
+    command_input = input("Enter a command:")
     if command_input == "=":
         break
-    elif command_input == "log":
-        result = math.log(result)
-        print(result)
-        continue
-    elif command_input == "f":
-        result = math.factorial(int(result))
-        print(result)
-        continue
     
-    num2_input = float(input())
+    num2_input = float(input("Enter a number:"))
 
     if command_input == "+":
         result += num2_input
@@ -36,5 +28,11 @@ while True:
     elif command_input == "m":
         result %= num2_input
         print(result)
-        
+    elif command_input == "log":
+        result = math.log(result)
+        print(result)
+    elif command_input == "f":
+        result = math.factorial(int(result))
+        print(result)
+
 print(result)
