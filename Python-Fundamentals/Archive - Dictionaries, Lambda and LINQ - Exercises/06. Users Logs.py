9f0c4b0114c3d37a7b5809dba6ff870ade938544
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-03-30 09:22:51 +0300

    Update 06. Users Logs.py

--- Python-Fundamentals/Archive - Dictionaries, Lambda and LINQ - Exercises/06. Users Logs.py
+++ Python-Fundamentals/Archive - Dictionaries, Lambda and LINQ - Exercises/06. Users Logs.py
@@ -7,13 +7,24 @@ while command != "end":
     command = command.replace("message=", "")
     command = command.replace("user=", "")
     command = command.split()
-    if command[0] not in log.keys():
-        command = input()
+    user_name = command[2]
+    ip = command[0]
+    if user_name not in log.keys():
+        log[user_name] = {ip: 1}
+    else:
+        if ip not in log[user_name].keys():
+            log[user_name][ip] = 1
+        else:
+            log[user_name][ip] += 1
+    
+    command = input()
+
+log = dict(sorted(log.items(), key=lambda x: x[0]))
 
 
-
-# log = dict(sorted(log.items(), key=lambda x: x[0]))
-# for user, conn in log.items():
-#     print(f"{user}: ")
-#     for ip_num, visit_num in conn.items():
-#         print(f"{ip_num} => {visit_num}.")
+for user, conn in log.items():
+    current_result = []
+    print(f"{user}: ")
+    for ip_num, visit_num in conn.items():
+        current_result.append(f"{ip_num} => {visit_num}")
+    print(*current_result, sep=",\n", end=".\n")
