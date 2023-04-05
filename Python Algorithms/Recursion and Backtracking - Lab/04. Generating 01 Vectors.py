89cc7ae5b32226f5f8b4c3d17a69e2c764427e6a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-05 15:58:53 +0300

    Update 04. Generating 01 Vectors.py

--- Python Algorithms/Recursion and Backtracking - Lab/04. Generating 01 Vectors.py
+++ Python Algorithms/Recursion and Backtracking - Lab/04. Generating 01 Vectors.py
@@ -2,7 +2,7 @@ num = int(input())
 vector = [0] * num
 def vectors(idx, vector):
     if idx == len(vector):
-        print(''.join(map(str, vector)))
+        print(*vector, sep="")
         return
     for i in range(2):
         vector[idx] = i
