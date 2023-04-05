532ff27458ddfe118a1f2c5b6bcc105646799a70
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-04-05 20:56:46 +0300

    Update 05. Paths in Labyrinth.py

--- Python Algorithms/Recursion and Backtracking - Lab/05. Paths in Labyrinth.py
+++ Python Algorithms/Recursion and Backtracking - Lab/05. Paths in Labyrinth.py
@@ -4,8 +4,9 @@ labir = []
 for i in range(rows):
     labir.append(list(input()))
 
+
 def find_path(row, col, labir, direct, path):
-    if rows <= row < 0 or columns <= col < 0 :
+    if rows <= row or row < 0 or columns <= col or col < 0:
         return
     
     if labir[row][col] == "*":
@@ -13,24 +14,20 @@ def find_path(row, col, labir, direct, path):
     
     if labir[row][col] == "visited":
         return
-    
     path.append(direct)
-    
+
     if labir[row][col] == "e":
         print("".join(path))
-        path.pop()
-        
+
     else:
-        labir[row][col] == "visited"
-        
+        labir[row][col] = "visited"
+
         find_path(row - 1, col, labir, "U", path)
         find_path(row + 1, col, labir, "D", path)
         find_path(row, col - 1, labir, "L", path)
         find_path(row, col + 1, labir, "R", path)
-        #mark current cell as free for further use for another possible path:
-        labir[row][col] == "-"
-        
-    #remove the last visited cell
+        labir[row][col] = "-"
+
     path.pop()
         
 
