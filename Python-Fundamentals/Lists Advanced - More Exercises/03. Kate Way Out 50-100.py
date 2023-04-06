0470f743da78264b4dacebccb73ec5b3593fa150
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-06 12:53:08 +0300

    Update 03. Kate Way Out 50-100.py

--- Python-Fundamentals/Lists Advanced - More Exercises/03. Kate Way Out 50-100.py
+++ Python-Fundamentals/Lists Advanced - More Exercises/03. Kate Way Out 50-100.py
@@ -12,31 +12,33 @@ if start_row == 0 or start_row == rows - 1 or start_column == 0 or start_column 
     print(f"Kate got out in 1 moves")
 
 else:
-    def find_path(row, col, maze, direct, path):
-    
+    def find_path(row, col, maze, path):
+
         if row < rows and col < columns and row > -1 and columns > -1:
             if maze[row][col] == "visited":
                 return
         if row < rows and col < columns and row > -1 and columns > -1:
             if maze[row][col] == "#":
                 return
-        if rows == row or row == 0 or columns == col or col == 0:
+        if row == rows or row == -1 or col == columns or col == -1:
+            #debug code solutions.append(path)
             solutions.append(len(path))
             return
     
         maze[row][col] = "visited"
-        path.append(direct)
+        path.append([row, col])
     
-        find_path(row - 1, col, maze, "U", path)
-        find_path(row + 1, col, maze, "D", path)
-        find_path(row, col - 1, maze, "L", path)
-        find_path(row, col + 1, maze, "R", path)
+        find_path(row - 1, col, maze, path)
+        find_path(row + 1, col, maze, path)
+        find_path(row, col - 1, maze, path)
+        find_path(row, col + 1, maze, path)
     
         maze[row][col] = " "
     
-    find_path(start_row, start_column, maze, '', [])
+    find_path(start_row, start_column, maze, [])
     
     if len(solutions) == 0:
         print("Kate cannot get out")
     else:
-        print(f"Kate got out in {min(solutions)} moves")
+        # debug code print(solutions)
+        print(f"Kate got out in {max(solutions)} moves")
