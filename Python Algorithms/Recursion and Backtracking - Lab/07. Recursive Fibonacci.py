c1bbde02bb38e2078a64660e971977ebdba528fb
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-06 16:46:14 +0300

    Create 07. Recursive Fibonacci.py

---
+++ Python Algorithms/Recursion and Backtracking - Lab/07. Recursive Fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(num = int(input())):
+    if num == 1:
+        print(1)
+    elif num == 2:
+        print(2)
+    else:
+        fib_seq = [1, 1]
+        for i in range(num-2):
+
+            fib_seq.append(fib_seq[i] + fib_seq[i+1])
+
+        fib_seq = list(map(int, fib_seq))
+        print(fib_seq[-1] + fib_seq[-2])
+fibonacci()
