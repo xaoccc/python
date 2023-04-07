127b950c636c364356f82ee880c4522da064b1ed
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-04-07 11:47:03 +0300

    Update 01. Reverse Array.py

--- Python Algorithms/Recursion and Backtracking - Exercise/01. Reverse Array.py
+++ Python Algorithms/Recursion and Backtracking - Exercise/01. Reverse Array.py
@@ -13,3 +13,20 @@ else:
             print(" ".join(elements))
 
     reverse_array(0, elements)
+
+
+# elements = input().split()
+# 
+# def swap(idx, elements):
+#     if len(elements) == 1:
+#         return elements[idx]
+#     if idx < (len(elements) // 2) - 1:
+#         swap_idx = len(elements) - 1 - idx
+#         elements[idx], elements[swap_idx] = elements[swap_idx], elements[idx]
+#         swap(idx + 1, elements)
+#     else:
+#         return
+#
+# swap(0, elements)
+#
+# print(" ".join(elements))
