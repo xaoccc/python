8e93781a09683c607bb1011379d38c547181c151
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-04-07 18:09:04 +0300

    Update 03. Move Down-Right.py

--- Python Algorithms/Recursion and Backtracking - Exercise/03. Move Down-Right.py
+++ Python Algorithms/Recursion and Backtracking - Exercise/03. Move Down-Right.py
@@ -1,12 +1,18 @@
 def unique_paths(row, col, rows, cols):
+    #if we hit cell outside the map, we have no valid path, so return 0 (No path to count)
     if row == rows or col == cols:
         return 0
+
+    # Every time when in unique_paths(row + 1, col, rows, cols) or unique_paths(row, col + 1, rows, cols)
+    # row == rows - 1 and col == cols - 1, the result is 1
     if row == rows - 1 and col == cols - 1:
         return 1
 
     result = 0
+    # We add each result(return) of the nested function to the total result
     result += unique_paths(row + 1, col, rows, cols)
     result += unique_paths(row, col + 1, rows, cols)
+    # Here is the place for the result
     return result
 
 
