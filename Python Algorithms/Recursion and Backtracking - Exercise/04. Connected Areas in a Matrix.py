e94b686d8a6be10225a23657132e557086f36eea
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-04-08 19:15:40 +0300

    Update 04. Connected Areas in a Matrix.py

--- Python Algorithms/Recursion and Backtracking - Exercise/04. Connected Areas in a Matrix.py
+++ Python Algorithms/Recursion and Backtracking - Exercise/04. Connected Areas in a Matrix.py
@@ -29,6 +29,7 @@ for row in range(rows):
         if area > 0:
             total_areas += 1
             res.append(f"Area #{total_areas} at ({row}, {col}), size: {area}")
+print(total_areas)
 print(f"Total areas found: {total_areas}")
 for i in range(total_areas):
     print(res[i])
