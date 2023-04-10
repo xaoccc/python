e5b34a314baf7c580588a5d77690714a6d106e72
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-04-10 16:01:04 +0300

    sada

--- Python-Fundamentals/Lists Advanced - More Exercises/03. Kate Way Out 50-100.py
+++ Python-Fundamentals/Lists Advanced - More Exercises/03. Kate Way Out 50-100.py
@@ -10,35 +10,29 @@ for i in range(rows):
         start_column = current_row.index("k")
 
      
-if start_row == 0 or start_row == rows - 1 or start_column == 0 or start_column == columns - 1:
-    print(f"Kate got out in 1 moves")
+def find_path(row, col, maze, path):
 
+    if row == rows or row == -1 or col == columns or col == -1:
+        solutions.append(len(path))
+        return
+
+    if 0 > row >= rows or 0 > col >= columns or maze[row][col] == "visited" or maze[row][col] == "#":
+        return
+
+    path.append([row, col])
+    maze[row][col] = "visited"
+
+
+    find_path(row - 1, col, maze, path)
+    find_path(row + 1, col, maze, path)
+    find_path(row, col - 1, maze, path)
+    find_path(row, col + 1, maze, path)
+    maze[row][col] = " "
+    path.pop()
+
+find_path(start_row, start_column, maze, [])
+
+if len(solutions) == 0:
+    print("Kate cannot get out")
 else:
-    def find_path(row, col, maze, path):
-
-        if row < rows and col < columns and row > -1 and columns > -1:
-            if maze[row][col] == "visited" or maze[row][col] == "#":
-                return
-        if row == rows or row == -1 or col == columns or col == -1:
-            #debug code solutions.append(path)
-            solutions.append(len(path))
-            return
-    
-        maze[row][col] = "visited"
-        path.append([row, col])
-    
-        find_path(row - 1, col, maze, path)
-        find_path(row + 1, col, maze, path)
-        find_path(row, col - 1, maze, path)
-        find_path(row, col + 1, maze, path)
-    
-        maze[row][col] = " "
-        path.pop()
-    
-    find_path(start_row, start_column, maze, [])
-    
-    if len(solutions) == 0:
-        print("Kate cannot get out")
-    else:
-        # debug code print(solutions)
-        print(f"Kate got out in {max(solutions)} moves")
+    print(f"Kate got out in {max(solutions)} moves")
