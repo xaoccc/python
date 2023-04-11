ed114157b9423dff82cfffc0c7cc9c4f1350d935
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-11 13:55:51 +0300

    Update 03. Supermarket.py

--- Python Advanced/Lists as Stacks and Queues - Lab/03. Supermarket.py
+++ Python Advanced/Lists as Stacks and Queues - Lab/03. Supermarket.py
@@ -10,3 +10,17 @@ while customer != "End":
     customer = input()
 
 print(f"{len(queue)} people remaining.")
+
+
+# from collections import deque
+# queue = deque()
+# while True:
+#     customer = input()
+#     if customer == "Paid":
+#         while len(queue):
+#             print(queue.popleft())
+#     elif customer == "End":
+#         print(f"{len(queue)} people remaining.")
+#         break
+#     else:
+#         queue.append(customer)
