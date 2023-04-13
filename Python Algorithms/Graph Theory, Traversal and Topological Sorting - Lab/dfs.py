69e8146975e75884c1ecfcca3fd493e3f23d863f
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-13 16:14:52 +0300

    Update dfs.py

--- Python Algorithms/Graph Theory, Traversal and Topological Sorting - Lab/dfs.py
+++ Python Algorithms/Graph Theory, Traversal and Topological Sorting - Lab/dfs.py
@@ -1,3 +1,18 @@
+# For nodes which are not ordered, we use set()
+visited = set()
+def dfs(node, graph, visited):
+    # If the current node is added to the visited set, we do not add it to the result anymore
+    # The breakpoint of the recusrion is here
+    if node in visited:
+        return
+    # If node is not visited, we add it to the set
+    visited.add(node)
+    # For each child of the node we check for unvisited nodes and add the recursively to the set
+    for child in graph[node]:
+        dfs(child, graph, visited)
+    
+    print(node)
+
 graph = {
     1: [19, 21, 14],
     19: [7, 12, 31, 21],
@@ -6,6 +21,9 @@ graph = {
     31: [21],
     21: [14],
     14: [6, 23],
-    32: [21],
+    23: [21],
     6: []
 }
+
+for node in graph:
+    dfs(node, graph, visited)
