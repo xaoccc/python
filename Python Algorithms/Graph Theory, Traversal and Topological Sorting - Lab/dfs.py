0cc5f1e0db302af958acdcf2b9d2a2f45f69fdc9
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-13 16:50:33 +0300

    Update dfs.py

--- Python Algorithms/Graph Theory, Traversal and Topological Sorting - Lab/dfs.py
+++ Python Algorithms/Graph Theory, Traversal and Topological Sorting - Lab/dfs.py
@@ -1,4 +1,4 @@
-# For nodes which are not ordered, we use set()
+# For ordered nodes we use set()
 visited = set()
 def dfs(node, graph, visited):
     # If the current node is added to the visited set, we do not add it to the result anymore
@@ -27,3 +27,28 @@ graph = {
 
 for node in graph:
     dfs(node, graph, visited)
+
+# For non-ordered nodes we use list()    
+#     graph = [
+#     [3, 6],
+#     [2, 3, 4, 5, 6],
+#     [1, 4, 5],
+#     [0, 1, 5],
+#     [1, 2, 6],
+#     [2, 1, 3],
+#     [0, 1, 4]
+#     ]
+    
+    
+# def dfs(node, graph, visited):
+#     if visited[node]:
+#         return
+#     visited[node] = True
+#     for child in graph[node]:
+#         dfs(child, graph, visited)
+#     print(node)
+    
+# visited = [False] * len(graph)
+
+# for node in range(len(graph)):
+#     dfs(node, graph, visited)
