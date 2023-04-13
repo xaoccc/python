1cd0d507f9d3b3625300e693db543a074b7a9388
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-13 09:51:08 +0300

    Update 07. Robotics.py

--- Python Advanced/Lists as Stacks and Queues - Exercise/07. Robotics.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/07. Robotics.py
@@ -1,6 +1,11 @@
 import datetime
 from datetime import timedelta
 
+def get_key(val):
+    for key, value in robots_ready.items():
+        if val == value:
+            return key
+
 robots = input().split(";")
 robots_data = {}
 robots_ready = {}
@@ -15,24 +20,24 @@ new_time = datetime.datetime(2000,7,7,start_time[0], start_time[1], start_time[2
 command = input()
 while command != "End":
     new_time += timedelta(seconds=1)
-    print_time = new_time.strftime("%H:%M:%S")
     for robot, time in robots_data.items():
+        
         if sum(robots_data.values()) == 0:
             new_time = min(robots_ready.values())
-            print(f"{robots_ready[robot]} - {command} [{robots_ready.values()}]")
+            print(f"{get_key(min(robots_ready.values()))} - {command} [{min(robots_ready.values()).strftime('%H:%M:%S')}]")
+            ready = new_time+timedelta(seconds=robots_database[robot])
+            robots_ready[robot] = ready
+            break
         
         elif time > 0:
-            print(f"{robot} - {command} [{print_time}]")
+            print(f"{robot} - {command} [{new_time.strftime('%H:%M:%S')}]")
             ready = new_time+timedelta(seconds=time)
-            print(f"Will be available again at {ready.strftime('%H:%M:%S')}")
-            robots_ready[robot] = ready.strftime('%H:%M:%S')
+            robots_ready[robot] = ready
             robots_data[robot] = 0
             break
 
-        
-    print(robots_ready)
     for robot, time in robots_ready.items():
-        if print_time == time:
+        if new_time.strftime('%H:%M:%S') == time:
             robots_data[robot] = robots_database[robot]
     
     command = input()
