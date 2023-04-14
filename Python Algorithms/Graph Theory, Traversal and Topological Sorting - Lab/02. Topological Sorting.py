bcf52d2719aa2556477fcf7f4606e08163bbcaf6
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-04-14 23:41:22 +0300

    Update 02. Topological Sorting.py

--- Python Algorithms/Graph Theory, Traversal and Topological Sorting - Lab/02. Topological Sorting.py
+++ Python Algorithms/Graph Theory, Traversal and Topological Sorting - Lab/02. Topological Sorting.py
@@ -1,31 +1,49 @@
-nodes = int(input())
+
+def find_dependencies(graph):
+    result = {}
+    for nude, children in graph.items():
+        if nude not in result:
+            result[nude] = 0
+        for child in children:
+            if child not in result:
+                result[child] = 1
+            else:
+                result[child] += 1
+    return result
+
+def find_nude_without_depencencies(dependencies_by_nude):
+    for nude, dependencies in dependencies_by_nude.items():
+        if dependencies == 0:
+            return nude
+    return None
+
+nudes = int(input())
 graph = {}
-single_children = []
-children = []
-result = []
-invalid = False
-for node in range(nodes):
-    current_node = input().split("->")
-    if current_node[1].strip() in graph.keys() and current_node[0].strip() in single_children:
-        print("Invalid topological sorting")
-        invalid = True
+
+for i in range(nudes):
+    current_nude = input().split("->")
+    nude = current_nude[0].strip()
+    children = current_nude[1].strip().split(", ") if current_nude[1] else []
+    graph[nude] = children
+dependencies_by_nude = find_dependencies(graph)
+
+loop = False
+sorted_nudes = []
+
+while dependencies_by_nude:
+    nude_to_remove = find_nude_without_depencencies(dependencies_by_nude)
+    if nude_to_remove is None:
+        loop = True
         break
-    graph[current_node[0].strip()] = current_node[1].strip().split(", ")
-    if len(current_node[1].strip()) == 1:
-        single_children += current_node[1].strip().split(", ")
 
+    dependencies_by_nude.pop(nude_to_remove)
+    sorted_nudes.append(nude_to_remove)
 
-if not invalid:
-    visited = []
-    for value in graph.values():
-        children += value
+    for child in graph[nude_to_remove]:
+        dependencies_by_nude[child] -= 1
 
-    for node in graph:
-        if node not in children:
-            visited.append(node)
+print(sorted_nudes)
 
-    for child in children:
-        if child not in visited:
-            visited.append(child)
+if loop:
+    print("Invalid topological sorting")
 
-print(f"Topological sorting: {', '.join(visited)}")
