eb5d90732959438ed73dd837eb4ffa4a14f42ae7
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-04-14 12:03:30 +0300

    Update bfs.py

--- Python Algorithms/Graph Theory, Traversal and Topological Sorting - Lab/bfs.py
+++ Python Algorithms/Graph Theory, Traversal and Topological Sorting - Lab/bfs.py
@@ -1,5 +1,20 @@
 from collections import deque
 
+
+def bfs(node, graph, visited):
+    if node in visited:
+        return
+    queue = deque([node])
+    visited.add(node)
+    while queue:
+        current_node = queue.popleft()
+        print(current_node, end=' ')
+
+        for child in graph[current_node]:
+            if child not in visited:
+                visited.add(child)
+                queue.append(child)
+
 graph = {
     1: [19, 21, 14],
     19: [7, 12, 31, 21],
@@ -13,17 +28,5 @@ graph = {
 }
 visited = set()
 
-
-def bfs(node, graph, vosoted):
-    if node in visited:
-        return
-    queue = deque([node])
-    visited.add(node)
-    while queue:
-        current_node = queue.popleft()
-
-    return
-
-
 for node in graph:
-    bfs(node, graph, vosoted)
+    bfs(node, graph, visited)
