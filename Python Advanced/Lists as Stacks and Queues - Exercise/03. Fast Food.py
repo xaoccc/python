91b1e8a4811a5b566d321719f1093e95f825313a
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-04-15 15:24:14 +0300

    Update 03. Fast Food.py

--- Python Advanced/Lists as Stacks and Queues - Exercise/03. Fast Food.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/03. Fast Food.py
@@ -13,3 +13,18 @@ if len(left) > 0:
     print(f"Orders left: {' '.join([str(i) for i in left])}")
 else:
     print("Orders complete")
+
+
+# from collections import deque
+# food = int(input())
+# orders = deque([int(i) for i in input().split()])
+# print(max(total_orders))
+# for order in total_orders.copy():
+#     if food - order >= 0:
+#         orders.popleft()
+#         food -= order
+#     else:
+#         print(f"Orders left: {' '.join([str(order) for order in orders])}")
+#         break
+# else:
+#     print("Orders complete")
