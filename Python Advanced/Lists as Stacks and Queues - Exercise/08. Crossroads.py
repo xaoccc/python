9c11c5c9b643189b882e224583d87d1a501b0e08
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-04-17 23:49:14 +0300

    Update 08. Crossroads.py

--- Python Advanced/Lists as Stacks and Queues - Exercise/08. Crossroads.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/08. Crossroads.py
@@ -19,6 +19,8 @@ for car in traffic_data:
             if green_light > 0:
                 successfully_passed_cars += 1
                 green_light = 0
+        elif len(car) > (green_light + free_window) and green_light == 0:
+            traffic_data.append(car)
         elif len(car) > (green_light + free_window) and green_light > 0:
             print(f"A crash happened!\n{car} was hit at {car[(green_light + free_window) - len(car)]}.")
             crash = True
