821b64ba951cefcad2b741843c0242e6746f0056
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-18 13:33:42 +0300

    Update 09. Key Revolver.py

--- Python Advanced/Lists as Stacks and Queues - Exercise/09. Key Revolver.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/09. Key Revolver.py
@@ -2,33 +2,32 @@ from collections import deque
 
 bullet_price = int(input())
 gun_barrel_size = int(input())
-bullets = [int(i) for i in input().split()]
-bullets = list(reversed(bullets))
+bullets = list(reversed([int(i) for i in input().split()]))
 locks = deque(int(i) for i in input().split())
 budget = int(input())
 total_bullets = len(bullets)
 
-
 while locks:
+    if gun_barrel_size > len(bullets):
+        gun_barrel_size = len(bullets)
     for i in range(gun_barrel_size):
         if bullets[i] <= locks[0]:
             print("Bang!")
             locks.popleft()
-        else:
+        elif bullets[i] > locks[0]:
             print("Ping!")
             locks.appendleft(locks.popleft())
-
-        if len(bullets) == 0:
-            print(f"Couldn't get through. Locks left: {len(locks)}")
+        elif len(bullets) == 0:
             break
+        print(bullets)
+        print(gun_barrel_size)
     bullets = bullets[gun_barrel_size: ]
-    if gun_barrel_size > len(bullets) and len(bullets) != 0:
-        gun_barrel_size = len(bullets)
-    if len(bullets) > 0:
+
+    if bullets:
         print("Reloading!")
-    if len(locks) == 0:
+    if not locks:
         print(f"{len(bullets)} bullets left. Earned ${budget - (bullet_price * (total_bullets - len(bullets)))}")
         break
-    elif len(bullets) == 0:
+    elif not bullets:
         print(f"Couldn't get through. Locks left: {len(locks)}")
         break
