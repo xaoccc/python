a038e28983e03a6c7ea58e61da12d820e04fb84a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-19 11:46:35 +0300

    Create 06. Summation Pairs.py

---
+++ Python Advanced/Tuples and Sets - Lab/06. Summation Pairs.py
@@ -0,0 +1,29 @@
+import time
+start = time.time()
+nums = [int(i) for i in input().split()]
+target = int(input())
+i = 0
+
+while True:
+    j = i + 1
+    while True:
+        found = False
+        if nums[i] + nums[j] == target and i != j:
+            print(f"{nums[i]} + {nums[j]} = {target}")
+            if i > j:
+                nums.pop(i)
+                nums.pop(j)
+            else:
+                nums.pop(j)
+                nums.pop(i)
+            found = True
+            j = 0
+        if j == len(nums) - 1 or i >= len(nums):
+            break
+        if not found:
+            j += 1
+    if i >= len(nums) - 1:
+        break
+    i += 1
+end = time.time()
+print(f"Total time:{end - start}")
