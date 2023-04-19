ab45a320daa8136e8e290bb87cd60b7d28b6f4bb
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-19 13:23:34 +0300

    Create 01. Numbers.py

---
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/01. Numbers.py
@@ -0,0 +1,25 @@
+nums_one = set([int(i) for i in input().split()])
+nums_two = set([int(i) for i in input().split()])
+n = int(input())
+
+for i in range(n):
+    command = [int(i) if i.isdigit() else i for i in input().split()]
+    if command[0] == "Add":
+        if command[1] == "First":
+            nums_one = nums_one | set(command[2:])
+        elif command[1] == "Second":
+            nums_two = nums_two | set(command[2:])
+    elif command[0] == "Remove":
+        if command[1] == "First":
+            nums_one = nums_one - set(command[2:])
+        elif command[1] == "Second":
+            nums_two = nums_two - set(command[2:])
+    elif command[0] == "Check":
+        if nums_one < nums_two or nums_one > nums_two:
+            print(True)
+        else:
+            print(False)
+nums_one = sorted(nums_one)
+nums_two = sorted(nums_two)
+print(", ".join([str(i) for i in nums_one]))
+print(", ".join([str(i) for i in nums_two]))
