f851c047eaaa33307aa7e1a373dc48a6de28764a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-20 14:29:39 +0300

    Update 04. Honey.py

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/04. Honey.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/04. Honey.py
@@ -7,19 +7,20 @@ total_nectar = 0
 
 while bees and nectar and process:
     if bees[0] <= nectar[-1]:
+        if (nectar[-1] == 0 or bees[0] == 0) and process[0] == "/":
+            process.popleft()
+            nectar.pop()
+            bees.popleft()
+            continue
+
         op_func = {
             "+": operator.add,
             "-": operator.sub,
             "*": operator.mul,
-            "/": operator.truediv,
-            "%": operator.mod,
-        }.get(process[0])
-        total_nectar += abs(op_func(bees[0], nectar[-1]))
-        bees.popleft()
-        nectar.pop()
-        process.popleft()
-    else:
-        nectar.pop()
+            "/": operator.truediv
+        }.get(process.popleft())
+        total_nectar += abs(op_func(bees.popleft(), nectar[-1]))
+    nectar.pop()
 
 print(f"Total honey made: {total_nectar}")
 if bees:
