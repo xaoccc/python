3d8752c8030e73e2660fe6c61dbfab054debcb91
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-20 11:32:28 +0300

    Update 03. Milkshakes.py

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/03. Milkshakes.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/03. Milkshakes.py
@@ -2,30 +2,21 @@ from collections import deque
 choco = [int(i) for i in input().split(", ")]
 milk = deque([int(i) for i in input().split(", ")])
 milkshakes = 0
-i = len(choco) - 1
 
-while True:
+while choco and milk and milkshakes < 5:
     if milk[0] <= 0:
         milk.popleft()
-    if choco[i] <= 0:
-        choco.pop(i)
-        i -= 1
-    if i == -1:
-        i = len(choco) - 1
+    if choco[-1] <= 0:
+        choco.pop()
     if not choco or not milk:
         break
-    if choco[i] == milk[0]:
+    if choco[-1] == milk[0]:
         milkshakes += 1
         milk.popleft()
-        choco.pop(i)
+        choco.pop()
     else:
         milk.append(milk.popleft())
-        choco[i] -= 5
-    i -= 1
-    if not choco or not milk or milkshakes == 5:
-        break
-    if i == -1:
-        i = len(choco) - 1
+        choco[-1] -= 5
 
 if milkshakes < 5:
     print("Not enough milkshakes.")
