91e9c7b0664c47a0250db9bb74a85ce64ad54342
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-21 15:29:14 +0300

    Update 06. Paint Colors 60-100.py

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/06. Paint Colors 60-100.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/06. Paint Colors 60-100.py
@@ -43,3 +43,38 @@ for color in colors_found:
             colors_found.remove(color)
             
 print(colors_found)
+
+
+# from collections import deque
+# text = deque(input().split())
+# main_colors = ["red", "yellow", "blue"]
+# secondary_colors = ["orange", "purple", "green"]
+# colors_found = []
+# while text:
+#     if len(text) > 1:
+#         word_one = text.popleft()
+#         word_two = text.pop()
+    
+#         if word_one + word_two in (main_colors or secondary_colors):
+#             colors_found.append(word_one + word_two)
+#         elif word_two + word_one in (main_colors or secondary_colors):
+#             colors_found.append(word_two + word_one)
+#         else:
+#             text.insert(len(text)//2, word_one[:-1] + word_two[:-1])
+#     else:
+#         word_one = text.popleft()
+#         if word_one in main_colors or secondary_colors:
+#             colors_found.append(word_one)
+
+# for color in colors_found:
+#     if color == secondary_colors[0]:
+#         if (main_colors[0] and main_colors[1]) not in colors_found:
+#             colors_found.remove(color)
+#     elif color == secondary_colors[1]:
+#         if (main_colors[0] and main_colors[2]) not in colors_found:
+#             colors_found.remove(color)
+#     elif color == secondary_colors[2]:
+#         if (main_colors[1] and main_colors[2]) not in colors_found:
+#             colors_found.remove(color)
+            
+# print(colors_found)
