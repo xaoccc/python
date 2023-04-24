177245b6c72844124fe6698c535b647fb334b786
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-24 10:10:35 +0300

    Update and rename 06. Paint Colors 70-100.py to 06. Paint Colors.py

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/06. Paint Colors.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/06. Paint Colors.py
@@ -11,17 +11,16 @@ while text:
             colors_found.append(word_one + word_two)
         elif word_two + word_one in main_colors or word_two + word_one in secondary_colors:
             colors_found.append(word_two + word_one)
-        elif len(text) % 2 != 0:
-            if len(word_one[:-1]) > 0:
-                text.insert(len(text)//2, word_one[:-1])
-            if len(word_two[:-1]) > 0:
-                text.insert(len(text)//2, word_two[:-1])
         else:
-            text.insert(len(text)//2, word_one[:-1] + word_two[:-1])
+            if word_one[:-1]:
+                text.insert(len(text)//2, word_one[:-1])
+            if word_two[:-1]:
+                text.insert(len(text)//2, word_two[:-1])
     else:
         word_one = text.popleft()
         if word_one in main_colors or word_one in secondary_colors:
             colors_found.append(word_one)
+
 for color in colors_found:
     if color == secondary_colors[0]:
         if main_colors[0] not in colors_found or main_colors[1] not in colors_found:
