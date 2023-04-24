8fd15acbfa9a2e451877bea8dec5dd61b5c2a593
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-24 11:55:14 +0300

    Update 03. Periodic Table.py

--- Python Advanced/Tuples and Sets - Exercise/03. Periodic Table.py
+++ Python Advanced/Tuples and Sets - Exercise/03. Periodic Table.py
@@ -1,15 +1,5 @@
 inputs = int(input())
 all_elements = []
-remove_list = []
 for i in range(inputs):
-    current_elements = input().split()
-    if len(current_elements) != len(set(current_elements)):
-        for el in current_elements:
-            if current_elements.count(el) > 1:
-                remove_list.append(el)
-    all_elements += current_elements
-
-if not remove_list:
-    print(*set(all_elements), sep="\n")
-else:
-    print(*set(all_elements) - set(remove_list), sep="\n")
+    all_elements += input().split()
+print(*set(all_elements), sep="\n")
