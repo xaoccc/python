6c699af90507c45e12429c99c1cbe950fb45da34
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-04-25 10:22:27 +0300

    Update 05. Primary Diagonal.py

--- Python Advanced/Multidimensional Lists - Lab/05. Primary Diagonal.py
+++ Python Advanced/Multidimensional Lists - Lab/05. Primary Diagonal.py
@@ -3,3 +3,9 @@ sum = 0
 for i in range(rows):
     sum += [int(i) for i in input().split()][i]
 print(sum)
+
+# def primary_dialonal_sum(rows, total):
+#     for i in range(rows):
+#         total += [int(i) for i in input().split()][i]
+#     return total
+# print(primary_dialonal_sum(int(input()), 0))
