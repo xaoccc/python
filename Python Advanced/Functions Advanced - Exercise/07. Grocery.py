470d45335c3a48e88d8b42c0c911d67e7dca2f3b
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-03 16:46:08 +0300

    Update and rename 07. Grocery 0-100.py to 07. Grocery.py

--- Python Advanced/Functions Advanced - Exercise/07. Grocery.py
+++ Python Advanced/Functions Advanced - Exercise/07. Grocery.py
@@ -2,8 +2,5 @@ def grocery_store(**kwargs):
     kwargs = dict(sorted(kwargs.items(), key=lambda x: (-x[1], -len(x[0]), x[0])))
     result = ""
     for product, quantity in kwargs.items():
-        if product == list(kwargs.keys())[-1]:
-            result += f"{product}: {str(quantity)}"
-        else:
-            result += f"{product}: {str(quantity)} \n"
+        result += f"{product}: {str(quantity)}\n"
     return result
