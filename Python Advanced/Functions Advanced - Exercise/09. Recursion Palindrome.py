b2af67067b20532a10d59e090fe439dc38dd9007
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-05 14:03:30 +0300

    Update 09. Recursion Palindrome.py

--- Python Advanced/Functions Advanced - Exercise/09. Recursion Palindrome.py
+++ Python Advanced/Functions Advanced - Exercise/09. Recursion Palindrome.py
@@ -1,9 +1,26 @@
-def palindrome(*args):
-    palindrome = True
-    for i in range(len(args[0]) // 2):
-        if args[0][i] != args[0][len(args[0]) - i -1]:
-            palindrome = False
-    if palindrome:
-        return f"{args[0]} is a palindrome"
+def palindrome(text, idx):
+    # If we find a char, which is not the same as the mirror char in the word, we end the loop
+    if text[idx] != text[len(text) - idx - 1]:
+        return f"{text} is not a palindrome"
+    # If we check all chars and we do not find a non-palidrome char, we assume the word is a palindrome
+    elif idx == len(text) - 1:
+        return f"{text} is a palindrome"
+    # Here we start checking the first and the last letter for each iteration. 
     else:
-        return f"{args[0]} is not a palindrome"
+        return palindrome(text, idx + 1)
+    
+print(palindrome("abcba", 0)) 
+
+
+
+
+
+# def palindrome(*args):
+#     palindrome = True
+#     for i in range(len(args[0]) // 2):
+#         if args[0][i] != args[0][len(args[0]) - i -1]:
+#             palindrome = False
+#     if palindrome:
+#         return f"{args[0]} is a palindrome"
+#     else:
+#         return f"{args[0]} is not a palindrome"
