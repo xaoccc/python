8c66b2f9335d63551a67eb51941ae7dc1f764b02
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-09 13:07:27 +0300

    Function Executor 100/100

--- Python Advanced/Functions Advanced - Exercise/06. Function Executor.py
+++ Python Advanced/Functions Advanced - Exercise/06. Function Executor.py
@@ -5,6 +5,6 @@ def func_executor(*funcs):
         args_func = []
         for i in range(len(funcs[j][1])):
             args_func.append(funcs[j][1][i])        
-        result.append(f"{funcs[j][0].__name__} - {funcs[j][0](*args_func)} \n")
+        result.append(f"{funcs[j][0].__name__} - {funcs[j][0](*args_func)}\n")
 
     return "".join([str(i) for i in result])
