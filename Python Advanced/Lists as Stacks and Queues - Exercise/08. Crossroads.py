c449930cf73d62dae7062666d9773bcb1d7aae40
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-09 17:00:53 +0300

    100/100 - multiple green lights bug fixed
    Fix: added green lights counter
    if there are any cars on the crossroad, and green light == 0, we should wait for the next green light and decrease green lights number.

--- Python Advanced/Lists as Stacks and Queues - Exercise/08. Crossroads.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/08. Crossroads.py
@@ -6,10 +6,13 @@ free_window = int(input())
 traffic_data = deque()
 successfully_passed_cars = 0
 crash = False
+green = 0
 
 command = input()
 while command != "END":
     traffic_data.append(command)
+    if command == "green":
+        green += 1
     command = input()
 
 while traffic_data:
@@ -21,12 +24,19 @@ while traffic_data:
         elif len(car) <= (green_light + free_window) and green_light > 0:
             successfully_passed_cars += 1
             green_light = 0
+        elif green_light + free_window == 0 and green > 0:
+            successfully_passed_cars += 1
+            green_light = green_light_restore
+            del traffic_data[traffic_data.index("green")]
+            green -= 1
         elif len(car) > (green_light + free_window) and green_light > 0:
             print(f"A crash happened!\n{car} was hit at {car[(green_light + free_window) - len(car)]}.")
             crash = True
             break
+
     else:
         green_light = green_light_restore
         traffic_data.popleft()
+        green -= 1
 if not crash:
     print(f"Everyone is safe.\n{successfully_passed_cars} total cars passed the crossroads.")
