cbd070053d49010a508b8fc471f275d9929885ee
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-10 13:31:07 +0300

    Update 05. Longest Intersection 75-100.py

--- Python Advanced/Tuples and Sets - Exercise/05. Longest Intersection 75-100.py
+++ Python Advanced/Tuples and Sets - Exercise/05. Longest Intersection 75-100.py
@@ -7,14 +7,24 @@ for i in range(rows):
     nums = input().split("-")
     pair_one = tuple(map(int, nums[0].split(",")))
     pair_two = tuple(map(int, nums[1].split(",")))
-    if min(pair_one) >= max(pair_two) or max(pair_one) <= min(pair_two):
+    if min(pair_one) > max(pair_two) or max(pair_one) < min(pair_two):
         continue
+    if min(pair_one) == max(pair_two) or max(pair_one) == min(pair_two):
+        min_num = max(pair_one[0], pair_two[0])
+        max_num = min(pair_one[1], pair_two[1])
+        if max_len < 1:
+            max_len = 1
     else:
         intersection = min(pair_one[1], pair_two[1]) - max(pair_one[0], pair_two[0])
         if intersection > max_len:
             max_len = intersection + 1
             min_num = max(pair_one[0], pair_two[0])
             max_num = min(pair_one[1], pair_two[1])
+        if intersection == max_len and max_len == 0:
+            min_num = max(pair_one[0], pair_two[0])
+            max_num = min(pair_one[1], pair_two[1])
+            max_len = 1
+            
 
 if min_num != None and max_num != None:
     for i in range(min_num, max_num + 1):
