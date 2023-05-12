5d90816f62bab807c8f8fc85ddb9834a2c1ba9f4
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-12 18:49:56 +0300

    Update 02. Sets of Elements.py

--- Python Advanced/Tuples and Sets - Exercise/02. Sets of Elements.py
+++ Python Advanced/Tuples and Sets - Exercise/02. Sets of Elements.py
@@ -5,3 +5,13 @@ for i in range(sets_len[0]):
 for i in range(sets_len[1]):
     set_two.append(input())
 print(*set(set_one).intersection(set(set_two)), sep="\n")
+
+# n, m = [int(i) for i in input().split()]
+# first_set = {int(input()) for i in range(n)}
+# second_set = {int(input()) for i in range(m)}
+# print(*first_set.intersection(second_set), sep="\n")
+
+# n, m = [int(i) for i in input().split()]
+# first_set = {int(input()) for i in range(n)}
+# second_set = {int(input()) for i in range(m)}
+# print(*first_set & second_set, sep="\n")
