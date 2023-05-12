f7972f9d8a73d252cd3556d50e470b16f7996a51
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-12 18:58:23 +0300

    Update 03. Periodic Table.py

--- Python Advanced/Tuples and Sets - Exercise/03. Periodic Table.py
+++ Python Advanced/Tuples and Sets - Exercise/03. Periodic Table.py
@@ -3,3 +3,9 @@ all_elements = []
 for i in range(inputs):
     all_elements += input().split()
 print(*set(all_elements), sep="\n")
+
+# elements - set()
+# for i in range(int(input())):
+#     for el in input().split():
+#         table.add(el)
+# print(*table, sep="\n")
