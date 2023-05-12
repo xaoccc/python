2931a668e1c889402d0dba364617cd0cb6dbef5c
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-12 19:11:30 +0300

    Update 04. Count Symbols.py

--- Python Advanced/Tuples and Sets - Exercise/04. Count Symbols.py
+++ Python Advanced/Tuples and Sets - Exercise/04. Count Symbols.py
@@ -1,4 +1,11 @@
-text = input()
-text = [*text]
-for char in sorted(list(set(text))):
+text = [*input()]
+for char in sorted(set(text)):
     print(f"{char}: {text.count(char)} time/s")
+    
+# occurences = {}
+# for letter in input():
+#     occurences[letter] = occurences.get(letter, 0) + 1
+# for letter, times in sorted(occurences.items()):
+#     print(f"{letter}: {times} time/s")
+
+
