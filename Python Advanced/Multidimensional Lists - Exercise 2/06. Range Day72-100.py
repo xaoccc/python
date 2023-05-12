327002953d1bbcd70f2dc0fe8ae00e41c5b7bd3a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-12 14:27:07 +0300

    Update 06. Range Day72-100.py

--- Python Advanced/Multidimensional Lists - Exercise 2/06. Range Day72-100.py
+++ Python Advanced/Multidimensional Lists - Exercise 2/06. Range Day72-100.py
@@ -5,31 +5,24 @@ for i in range(5):
         curr_pos = [i, shoot_area[i].index("A")]
     if "x" in shoot_area[i]:
         total_targets += shoot_area[i].count("x")
-
+def mark_visited(pos, steps):
+    shoot_area[curr_pos[0]][curr_pos[1]] = "."
+    curr_pos[pos] += steps
+    shoot_area[curr_pos[0]][curr_pos[1]] = "A"
+    
 def move(direction, steps, curr_pos):
     if direction == "right":
         if curr_pos[1] + steps < 5 and shoot_area[curr_pos[0]][curr_pos[1] + steps] != "x":
-            shoot_area[curr_pos[0]][curr_pos[1]] = "."
-            curr_pos[1] += steps
-            shoot_area[curr_pos[0]][curr_pos[1]] = "A"
-
+            mark_visited(1, steps)
     elif direction == "left":
         if curr_pos[1] - steps >= 0 and shoot_area[curr_pos[0]][curr_pos[1] - steps] != "x":
-            shoot_area[curr_pos[0]][curr_pos[1]] = "."
-            curr_pos[1] -= steps
-            shoot_area[curr_pos[0]][curr_pos[1]] = "A"
-                
+            mark_visited(1, -steps)
     elif direction == "up":
         if curr_pos[0] - steps >= 0 and shoot_area[curr_pos[0] - steps][curr_pos[1]] != "x":
-            shoot_area[curr_pos[0]][curr_pos[1]] = "."
-            curr_pos[0] -= steps
-            shoot_area[curr_pos[0]][curr_pos[1]] = "A"
-                
+            mark_visited(0, -steps)
     elif direction == "down":
         if curr_pos[0] + steps < 5 and shoot_area[curr_pos[0] + steps][curr_pos[1] + steps] != "x":
-            shoot_area[curr_pos[0]][curr_pos[1]] = "."
-            curr_pos[0] += steps
-            shoot_area[curr_pos[0]][curr_pos[1]] = "A"
+            mark_visited(0, steps)
     return 
 
 def shoot(a, b, count_):
