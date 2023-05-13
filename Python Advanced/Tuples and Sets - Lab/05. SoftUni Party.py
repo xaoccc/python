93ba1dbfb8ca4e98d5c59ec84b1e4426ff54b5a5
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-13 11:10:13 +0300

    Update 05. SoftUni Party.py

--- Python Advanced/Tuples and Sets - Lab/05. SoftUni Party.py
+++ Python Advanced/Tuples and Sets - Lab/05. SoftUni Party.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 guests_num = int(input())
+start = datetime.now()
 guests_list = []
 guests_arrived = []
 
@@ -6,12 +9,35 @@ while len(set(guests_list)) < guests_num:
     code = input()
     if len(code) == 8:
         guests_list.append(code)
-    
+
 code = input()
 while code != "END":
     if len(code) == 8:
         guests_arrived.append(code)
     code = input()
-result = sorted(set(guests_list) - set(guests_arrived))    
+
+result = sorted(set(guests_list) - set(guests_arrived))
 print(len(result))
 print(*result, sep="\n")
+end = datetime.now()
+print(end - start)
+
+# n = int(input())
+# start = datetime.now()
+# reservations = set()
+#
+# for i in range(n):
+#     reservation_num = input()
+#     reservations.add(reservation_num)
+#
+# guest_reservation_num = input()
+# while guest_reservation_num != "END":
+#     reservations.remove(guest_reservation_num)
+#     guest_reservation_num = input()
+#
+# print(len(reservations))
+# for num in sorted(reservations):
+#     print(num)
+#
+# end = datetime.now()
+# print(end - start)
