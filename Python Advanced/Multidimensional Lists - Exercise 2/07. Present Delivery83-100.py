b8e59d72ddf565a53351b1d4279bf12ff3c87d33
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-13 17:18:08 +0300

    Update 07. Present Delivery83-100.py

--- Python Advanced/Multidimensional Lists - Exercise 2/07. Present Delivery83-100.py
+++ Python Advanced/Multidimensional Lists - Exercise 2/07. Present Delivery83-100.py
@@ -1,5 +1,11 @@
 presents, n = int(input()), int(input())
 neigh_map, good_kids, good_kids_with_presents, cookies = [], 0, 0, 0
+def printer():
+    for row in range(n):
+        for i in range(n):
+            if neigh_map[row][i] == "XXX":
+                neigh_map[row][i] = "-"
+        print(*neigh_map[row])
 
 for i in range(n):
     neigh_map.append(input().split())
@@ -22,46 +28,49 @@ while True:
         break
     neigh_map[santa[0]][santa[1]] = "-"
     santa[0], santa[1] = santa[0] + directions[command][0], santa[1] + directions[command][1]
-    
+
+    if neigh_map[santa[0]][santa[1]] == "XXX":
+        santa[0], santa[1] = cookie_location
+        neigh_map[santa[0]][santa[1]] = "S"
+
     if neigh_map[santa[0]][santa[1]] == "C":
+        cookie_location = [santa[0], santa[1]]
         if (neigh_map[santa[0] - 1][santa[1]] == "X" or neigh_map[santa[0] - 1][santa[1]] == "V") and presents >= 1:
             if neigh_map[santa[0] - 1][santa[1]] == "V":
                 good_kids_with_presents += 1
             presents -= 1
-            neigh_map[santa[0] - 1][santa[1]] = "-"
+            neigh_map[santa[0] - 1][santa[1]] = "XXX"
         if (neigh_map[santa[0] + 1][santa[1]] == "X" or neigh_map[santa[0] + 1][santa[1]] == "V") and presents >= 1:
             if neigh_map[santa[0] + 1][santa[1]] == "V":
                 good_kids_with_presents += 1
             presents -= 1
-            neigh_map[santa[0] + 1][santa[1]] = "-"
+            neigh_map[santa[0] + 1][santa[1]] = "XXX"
         if (neigh_map[santa[0]][santa[1] - 1] == "X" or neigh_map[santa[0]][santa[1] - 1] == "V") and presents >= 1:
             if neigh_map[santa[0]][santa[1] - 1] == "V":
                 good_kids_with_presents += 1
             presents -= 1
-            neigh_map[santa[0]][santa[1] - 1] = "-"
+            neigh_map[santa[0]][santa[1] - 1] = "XXX"
         if (neigh_map[santa[0]][santa[1] + 1] == "X" or neigh_map[santa[0]][santa[1] + 1] == "V") and presents >= 1:
             if neigh_map[santa[0]][santa[1] + 1] == "V":
                 good_kids_with_presents += 1
             presents -= 1
-            neigh_map[santa[0]][santa[1] + 1] = "-"
-            
+            neigh_map[santa[0]][santa[1] + 1] = "XXX"
+
     elif neigh_map[santa[0]][santa[1]] == "V":
         if presents > 0:
             presents -= 1
             good_kids_with_presents += 1
             if good_kids_with_presents == good_kids:
                 neigh_map[santa[0]][santa[1]] = "S"
-                for row in neigh_map:
-                    print(*row)
+                printer()
                 print(f"Good job, Santa! {good_kids} happy nice kid/s.")
                 break
     neigh_map[santa[0]][santa[1]] = "S"
     if presents == 0:
+        print("Santa ran out of presents!")
         break
     command = input()
-    
+
 if good_kids_with_presents != good_kids:
-    print("Santa ran out of presents!")
-    for row in neigh_map:
-        print(*row)
+    printer()
     print(f"No presents for {good_kids - good_kids_with_presents} nice kid/s.")
