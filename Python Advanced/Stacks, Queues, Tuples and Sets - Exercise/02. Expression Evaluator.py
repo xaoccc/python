08e5df113454b3648708c8947d6b5405af55321b
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-15 20:19:29 +0300

    Update 02. Expression Evaluator.py

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/02. Expression Evaluator.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/02. Expression Evaluator.py
@@ -1,29 +1,46 @@
-expression = [int(i) if i.isnumeric() or i.lstrip("-").isdigit() else i for i in input().split()]
-current = []
-result = 0
-for i in range(len(expression)):
-    if type(expression[i]) == int:
-        current.append(expression[i])
-    else:
-        if expression[i] == "-":
-            result = current[0]
-            for num in range(1, len(current)):
-                result -= current[num]
-            current = [result]
-        elif expression[i] == "+":
-            result = current[0]
-            for num in range(1, len(current)):
-                result += current[num]
-            current = [result]
-        elif expression[i] == "*":
-            result = current[0]
-            for num in range(1, len(current)):
-                result *= current[num]
-            current = [result]
-        elif expression[i] == "/":
-            result = current[0]
-            for num in range(1, len(current)):
-                result //= current[num]
-            current = [result]
-        
-print(result)
+#
+# expression = [int(i) if i.isnumeric() or i.lstrip("-").isdigit() else i for i in input().split()]
+# current = []
+# result = 0
+# for i in range(len(expression)):
+#     if type(expression[i]) == int:
+#         current.append(expression[i])
+#     else:
+#         if expression[i] == "-":
+#             result = current[0]
+#             for num in range(1, len(current)):
+#                 result -= current[num]
+#             current = [result]
+#         elif expression[i] == "+":
+#             result = current[0]
+#             for num in range(1, len(current)):
+#                 result += current[num]
+#             current = [result]
+#         elif expression[i] == "*":
+#             result = current[0]
+#             for num in range(1, len(current)):
+#                 result *= current[num]
+#             current = [result]
+#         elif expression[i] == "/":
+#             result = current[0]
+#             for num in range(1, len(current)):
+#                 result //= current[num]
+#             current = [result]
+#
+# print(result)
+
+from collections import deque
+from math import floor
+expression = deque(input().split())
+idx = 0
+
+while idx < len(expression):
+    element = expression[idx]
+    if element in "-+/*":
+        for i in range(idx - 1):
+            expression.appendleft(eval(f"{int(expression.popleft())} {element} {int(expression.popleft())}"))
+        del expression[1]
+        idx = 1
+    idx += 1
+
+print(floor(int(expression[0])))
