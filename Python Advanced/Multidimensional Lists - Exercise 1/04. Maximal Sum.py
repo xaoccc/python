7f889331dfc1b36777b2a63e79caf442daf02cf5
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-20 21:34:12 +0300

    Update 04. Maximal Sum.py

--- Python Advanced/Multidimensional Lists - Exercise 1/04. Maximal Sum.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/04. Maximal Sum.py
@@ -6,10 +6,10 @@ for i in range(rows):
 
 for i in range(rows - 2):
     for j in range(cols - 2):
-        current_sum = matrix[i][j] + matrix[i + 1][j] + matrix[i][j + 1] + matrix[i + 1][j + 1] + matrix[i + 2][j] + matrix[i][j + 2] + matrix[i + 1][j + 2] + matrix[i + 2][j + 1] + matrix[i + 2][j + 2]
+        current_sum = sum(matrix[i][j:j+3]) + sum(matrix[i+1][j:j+3]) + sum(matrix[i+2][j:j+3])
         if  current_sum > max_sum:
             max_sum = current_sum
-            result = [[matrix[i][j], matrix[i][j + 1], matrix[i][j + 2]], [matrix[i + 1][j], matrix[i + 1][j + 1], matrix[i + 1][j + 2]], [matrix[i + 2][j], matrix[i + 2][j + 1], matrix[i + 2][j + 2]]]
+            result = [matrix[i][j:j+3], matrix[i+1][j:j+3], matrix[i+2][j:j+3]]
 print(f"Sum = {max_sum}")
 for row in result:
     print(*row)
