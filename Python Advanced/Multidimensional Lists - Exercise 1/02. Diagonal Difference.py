2bbf4c500ef1ad6ce7f5bde8aea45df2a967fcd9
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-20 21:19:33 +0300

    Update 02. Diagonal Difference.py

--- Python Advanced/Multidimensional Lists - Exercise 1/02. Diagonal Difference.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/02. Diagonal Difference.py
@@ -6,8 +6,6 @@ for i in range(n):
     matrix.append([int(i) for i in input().split()])
 
 for i in range(n):
-    for j in range(n-1, -1, -1):
-        primary_diagonal.append(matrix[i][i])
-        secondary_diagonal.append(matrix[i][j-i])
-        break
+    primary_diagonal.append(matrix[i][i])
+    secondary_diagonal.append(matrix[i][n-i-1])
 print(abs(sum(primary_diagonal) - sum(secondary_diagonal)))
