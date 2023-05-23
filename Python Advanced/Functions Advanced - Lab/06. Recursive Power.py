ff1137ca4032d84930b73a9ba57dcc1c3d56298c
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-23 18:55:51 +0300

    Update 06. Recursive Power.py

--- Python Advanced/Functions Advanced - Lab/06. Recursive Power.py
+++ Python Advanced/Functions Advanced - Lab/06. Recursive Power.py
@@ -1,2 +1,11 @@
-def recursive_power(*nums):
-     return nums[0] ** nums[1]
+# def recursive_power(*nums):
+#      return nums[0] ** nums[1]
+
+def recursive_power(num, power):
+
+     if power == 1:
+          return num
+     return num * recursive_power(num, power-1)
+
+
+print(recursive_power(2, 900))
