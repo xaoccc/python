49e47b266c939ef3e48ac9b1564202cc4eec0c87
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-25 17:42:03 +0300

    Update 03. Naughty or Nice 38-100.py

--- Python Advanced/Exams/Python Advanced Retake Exam - 15 December 2021/03. Naughty or Nice 38-100.py
+++ Python Advanced/Exams/Python Advanced Retake Exam - 15 December 2021/03. Naughty or Nice 38-100.py
@@ -18,20 +18,25 @@ def naughty_or_nice_list(fargs, *args, **kwargs):
     nice_naughty = {"Nice":[], "Naughty": [], "Not found":[]}
     for kid in santa_list.values():
         all_kids_in_santa_list += kid
-
+    
     for number, kids_names in copy.deepcopy(santa_list).items():
         if len(kids_names) == 1:
             if number in kids_filter:
                 nice_naughty[kids_filter[number]].append(kids_names[0])
-            else:
-                nice_naughty["Not found"].append(kids_names[0])
-        else:
+                santa_list.pop(number)        
+    
+              
+    for number, kids_names in copy.deepcopy(santa_list).items():
+        for kid in kids_names:
+            if all_kids_in_santa_list.count(kid) == 1 and number in kids_filter:
+                nice_naughty[kwargs[kid]].append(kid)
+        if number in kids_filter:
+            santa_list.pop(number)
+            
+    if santa_list:
+        for number, kids_names in santa_list.items():
             for kid in kids_names:
-                if all_kids_in_santa_list.count(kid) == 1:
-                    nice_naughty[kwargs[kid]].append(kid)
-                else:
-                    nice_naughty["Not found"].append(kids_names[0])
-        santa_list.pop(number)
+                nice_naughty["Not found"].append(kid)
 
     for key, names in nice_naughty.items():
         if names:
