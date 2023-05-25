05279db9b1a7a37625c28a69c63f1c8e1f25a255
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-25 22:13:25 +0300

    SDASDC

---
+++ Python Advanced/File Handling/02. Line Numbers.py
@@ -0,0 +1,21 @@
+try:
+    file = open("text.txt")
+except FileNotFoundError:
+    print("Path not correct or file not found!")
+
+symbols = ["-", ",", ".", "!", "?", "'"]
+text = file.readlines()
+
+result = open('output.txt', 'w')
+i = 1
+for line in text:
+    line = line.replace("\n", "")
+    letters, punctuation_marks = 0, 0
+    for char in line:
+        if char.isalpha():
+            letters += 1
+        elif char in symbols:
+            punctuation_marks += 1
+    result.write(f"Line {i}: {line} ({letters})({punctuation_marks})\n")
+
+    i += 1
