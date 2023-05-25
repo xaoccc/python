7ebbced28bb1781937abcf453058276b1d175ad9
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-25 21:49:28 +0300

    files handling

--- Python Advanced/File Handling/01. Even Lines.py
+++ Python Advanced/File Handling/01. Even Lines.py
@@ -1,1 +1,16 @@
-file = open('text.txt', 'r')
+try:
+    file = open('text.txt')
+except FileNotFoundError:
+    print("File not found or path is incorrect")
+
+text = file.readlines()
+symbols = ["-", ",", ".", "!", "?"]
+
+for i in range(len(text)):
+    if i % 2 == 0:
+        for char in text[i]:
+            if char in symbols:
+                text[i] = text[i].replace(char, "@")
+        result = text[i].split()
+        result.reverse()
+        print(" ".join(result))
