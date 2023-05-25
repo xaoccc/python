ac42f26f395ed5f791d7f50f25ce3336bdeed6ec
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-25 19:40:43 +0300

    Update 01. Numbers Dictionary.py

--- Python Advanced/Error Handling/01. Numbers Dictionary.py
+++ Python Advanced/Error Handling/01. Numbers Dictionary.py
@@ -5,11 +5,11 @@ while line != "Search":
     number_as_string = line
     try:
         number = int(input())
-    except:
+    except TypeError:
         print("The variable number must be an integer")
     try:
         numbers_dictionary[number_as_string] = number
-    except:
+    except KeyError:
         print("Number does not exist in dictionary")
     line = input()
     
@@ -18,7 +18,7 @@ while line != "Remove":
     searched = line
     try:
         print(numbers_dictionary[searched])
-    except:
+    except KeyError:
         print("Number does not exist in dictionary")
     line = input()
     
@@ -27,7 +27,7 @@ while line != "End":
     searched = line
     try:
         del numbers_dictionary[searched]
-    except:
+    except KeyError:
         print("Number does not exist in dictionary")
     line = input()
 print(numbers_dictionary)
