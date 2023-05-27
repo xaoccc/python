0404f263b1d91c72d26ff612e3b5c2499b267a34
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-27 13:43:39 +0300

    jur

---
+++ Python Advanced/File Handling/Lab/File Reader.py
@@ -0,0 +1,11 @@
+try:
+    file = open("numbers.txt")
+    nums = file.readlines()
+    for i in range(len(nums)):
+        nums[i] = nums[i].replace("\n", "")
+    result = [int(i) for i in nums]
+    print(sum(result))
+    file.close()
+except FileNotFoundError:
+    print("File not found")
+
