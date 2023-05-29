c8e24d35fb07c2587efbbfe4771e217e0d3e7b9e
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-29 20:26:41 +0300

    numbers

--- Python Advanced/File Handling/Lab/02. File Reader.py
+++ Python Advanced/File Handling/Lab/02. File Reader.py
@@ -1,10 +1,15 @@
+# try:
+#     with open("numbers.txt", "r") as file:
+#         nums = file.readlines()
+#         for i in range(len(nums)):
+#             nums[i] = nums[i].replace("\n", "")
+#         result = [int(i) for i in nums]
+#         print(sum(result))
+# except FileNotFoundError:
+#     print("File not found")
+
 try:
     with open("numbers.txt", "r") as file:
-        nums = file.readlines()
-        for i in range(len(nums)):
-            nums[i] = nums[i].replace("\n", "")
-        result = [int(i) for i in nums]
-        print(sum(result))
+        print(sum([int(i) for i in file.read().split("\n")]))
 except FileNotFoundError:
     print("File not found")
-
