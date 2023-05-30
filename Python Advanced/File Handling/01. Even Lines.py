12f106c0706cc9b324d4a21a9a08315506df1256
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-05-30 18:37:52 +0300

    asd

--- Python Advanced/File Handling/01. Even Lines.py
+++ Python Advanced/File Handling/01. Even Lines.py
@@ -11,5 +11,5 @@ for i in range(len(text)):
         result = text[i].split()
         result.reverse()
         print(" ".join(result))
-        
+
 file.close()
--- Python Advanced/File Handling/02. Line Numbers.py
+++ Python Advanced/File Handling/02. Line Numbers.py
@@ -1,8 +1,6 @@
-
+from string import punctuation
 with open("text.txt", "r") as file:
     text = file.readlines()
-
-symbols = ["-", ",", ".", "!", "?", "'"]
 
 result = open('output.txt', 'w')
 i = 1
@@ -12,7 +10,7 @@ for line in text:
     for char in line:
         if char.isalpha():
             letters += 1
-        elif char in symbols:
+        elif char in punctuation:
             punctuation_marks += 1
     result.write(f"Line {i}: {line} ({letters})({punctuation_marks})\n")
 
