f8180ff64bfe6770636096c77e02a56f46329088
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-30 14:57:02 +0300

    Update 02. Scope Mess.py

--- Python OOP/First Steps in OOP - Lab/02. Scope Mess.py
+++ Python OOP/First Steps in OOP - Lab/02. Scope Mess.py
@@ -1,20 +1,30 @@
+
 x = "global"
 def outer():
     x = "local"
     def inner():
-        
+        # 5. We define a value of x which is also for outer()
+        nonlocal x
+        # 6. change the variable and print it -> inner: nonlocal
         x = "nonlocal"
         print("inner:", x)
     def change_global():
-        x = "global: changed!"
+        # 9. Change the variable with the value of x outside the function(global)
+        global x
+        # 10. Add some text to it and print it
+        x = x + ": changed!"
         print(x)
-        
+    
+    # 3. print the variable defined at the beginning of outer()    
     print("outer:", x)
-    x = "nonlocal"
+    # 4. Call the inner() funcion
     inner()
+    # 7. x should be changed here by the function inner(), so we print inner: nonlocal
     print("outer:", x)
+    # 8. Call the change_global() funcion 
     change_global()
 
-    
+# 1. print the global variable    
 print(x)
+# 2. Call the outer() function
 outer()
