1c5541a8311d63bdd07374861aecca2940f4b389
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-31 15:23:43 +0300

    Update matrix-template.py

--- Python Advanced/Exams/matrix-template.py
+++ Python Advanced/Exams/matrix-template.py
@@ -1,13 +1,31 @@
+
 directions = {
     "up": (-1, 0),
     "down": (1, 0),
-    "left": (-1, 0),
-    "right": (1, 0)
+    "left": (0, -1),
+    "right": (0, 1)
 }
 matrix = []
-for i in range(int(input)):
-    matrix.append(list(input()))
+# Create the matrix
+for i in range(int(input())):
+    matrix.append(input().split())
     if "P" in matrix[i]:
         position = [i, matrix[i].index("P")]
-        matrix[position[0]][position[1]] = "unmarked"
+        matrix[position[0]][position[1]] = "unmarked"        
+       
+while True:
+    move = input() 
+    position = [position[0] + directions[move][0], position[1] + directions[move][1]]
+    if position[0] < 0 or position[0] >= len(matrix) or position[1] < 0 or position[1] >= len(matrix):
+        # logic for out of the map
+    elif:
+        # Some other logic
+    else:
+        # Some more logic
+
+matrix[position[0]][position[1]] = "P"   
+
+# Print the matrix + some other result
+for row in matrix:
+    print("".join(row))
      
