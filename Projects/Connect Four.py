cec99117285b3b6165428dc75699d76e3214ec1b
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-05-31 17:13:42 +0300

    Update Connect Four.py

--- Projects/Connect Four.py
+++ Projects/Connect Four.py
@@ -1,21 +1,12 @@
 cols, rows = [int(input()), int(input())]
 playground = [[0 for j in range(cols)] for i in range(rows)]
+visited = []
 player_one, player_two = 0, 0
 
-def find_area(row, col, playground, player):
+def out_of_playground(row, col):
     if row >= rows or col >= cols or row < 0 or col < 0:
-        return 0
-
-    if playground[row][col] != player:
-        return 0
-
-    playground[row][col] = player
-    area = 1
-    area += find_area(row + 1, col, playground, player)
-    area += find_area(row - 1, col, playground, player)
-    area += find_area(row, col + 1, playground, player)
-    area += find_area(row, col - 1, playground, player)
-    return area
+        return True
+    return False
 
 i = 0
 while True:
@@ -23,12 +14,15 @@ while True:
     coords = [int(i) for i in input().split()]
     row = coords[0]
     col = coords[1]
-    if row >= 0 and row < rows and col >= 0 and col < cols:
+    if out_of_playground(row, col) or coords in visited:
+        print("Invalid coordinates!")
+        continue
+    else:
+        visited.append(coords)
         if i % 2 != 0:
-            playground[row][col] = 1
-            print(find_area(row, col, playground, playground[row][col]))
-        elif i % 2 == 0:
-            playground[row][col] = 2
-            print(find_area(row, col, playground, playground[row][col]))
+            playground[row][col] += 1
+        else:
+            playground[row][col] += 2        
+    
     for row in playground:
         print(*row)
