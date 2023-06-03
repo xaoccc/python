421c679811052a64cee061db8823fb81035ff7bf
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-06-03 09:49:12 +0300

    Implemented mark_red() function

--- Python Algorithms/RECURSION TRAINING/mark_all_connected_elements.py
+++ Python Algorithms/RECURSION TRAINING/mark_all_connected_elements.py
@@ -1,17 +1,32 @@
+
 def mark_red(matrix, row, col):
     
-    if row < 0 or row > rows or col < 0 or col > cols:
+    if row < 0 or row >= rows or col < 0 or col >= cols:
         return
     
-    if matrix[row][col] != "current_box_color":
+    if matrix[row][col] != "y":
         return
     
-    if matrix[row][col] == "current_box_color":
+    if matrix[row][col] == "y":
         matrix[row][col] = "red"
 
-    mark(matrix, row - 1, col)
-    mark(matrix, row, col - 1)
-    mark(matrix, row + 1, col)
-    mark(matrix, row, col + 1)
+    mark_red(matrix, row - 1, col)
+    mark_red(matrix, row, col - 1)
+    mark_red(matrix, row + 1, col)
+    mark_red(matrix, row, col + 1)
     
     return matrix
+
+
+rows = 6
+cols = 7
+matrix = []
+for i in range(rows):
+    matrix.append(input().split())
+
+
+current = ([int(i) for i in input().split()])
+mark_red(matrix, current[0], current[1])
+
+for row in matrix:
+    print(*row)
