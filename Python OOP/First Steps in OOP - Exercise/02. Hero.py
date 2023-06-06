495d5a4ae85b828142f2caff115d1b1eceecccd7
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-06 15:14:02 +0300

    Create 02. Hero.py

---
+++ Python OOP/First Steps in OOP - Exercise/02. Hero.py
@@ -0,0 +1,15 @@
+class Hero:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+    
+        
+    def defend(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            self.health = 0
+            return f"{self.name} was defeated"
+        
+        
+    def heal(self, amount):
+        self.health += amount
