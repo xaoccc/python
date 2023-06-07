b22317b5e41206b26e03a72b628c90e19770eeee
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-07 11:12:23 +0300

    Create 03. Circle.py

---
+++ Python OOP/Classes and Objects - Lab/03. Circle.py
@@ -0,0 +1,16 @@
+class Circle:
+    pi = 3.14 #class attribute
+    def __init__(self, radius: int):
+        self.radius = radius
+        
+        
+    def set_radius(self, new_radius):
+        self.radius = new_radius
+        
+        
+    def get_area(self):
+        return self.pi * self.radius * self.radius
+        
+    
+    def get_circumference(self):
+        return 2 * self.pi * self.radius
