1f620f89b21088a6cac92d33aaf02c1e552090be
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-07 15:24:17 +0300

    Create 05. Smartphone.py

---
+++ Python OOP/Classes and Objects - Lab/05. Smartphone.py
@@ -0,0 +1,23 @@
+class Smartphone:
+    def __init__(self, memory):
+        self.memory = memory
+        self.apps = []
+        self.is_on = False
+        
+        
+    def status(self):
+        return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
+    
+    
+    def power(self):
+        self.is_on = True
+    
+    
+    def install(self, app, app_memory):
+        if self.is_on and app_memory <= self.memory:
+            self.apps.append(app)
+            self.memory -= app_memory
+            return  f"Installing {app}"
+        if not self.is_on:
+            return f"Turn on your phone to install {app}"
+        return f"Not enough memory to install {app}"
