303ccf0ebfe81a05644bce45de0eab4f384ae97f
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-06-08 18:55:09 +0300

    Update login.py

---
+++ Projects/Shop/login.py
@@ -0,0 +1,11 @@
+from tkinter import Button
+from python.Projects.Shop.canvas import window
+
+
+def render_entry():
+    register_button = Button(
+        window,
+        text="Login",
+        bg="#6fa2f2",
+        fg="#1b1c1f",
+    )
