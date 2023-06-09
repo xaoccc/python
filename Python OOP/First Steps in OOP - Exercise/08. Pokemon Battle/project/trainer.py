fa87cd20222d1602e55f7fe457b982df25eb43d7
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-06-09 18:47:43 +0300

    100/100

--- Python OOP/First Steps in OOP - Exercise/08. Pokemon Battle/project/trainer.py
+++ Python OOP/First Steps in OOP - Exercise/08. Pokemon Battle/project/trainer.py
@@ -1,7 +1,7 @@
 from project.pokemon import Pokemon
 
-class Trainer:
 
+class Trainer:
     def __init__(self, name):
         self.name = name
         self.pokemons = []
@@ -12,8 +12,7 @@ class Trainer:
         self.pokemons.append(pokemon)
         return f"Caught {pokemon.name} with health {pokemon.health}"
         
-        
-    def release_pokemon(self, pokemon_name: string):
+    def release_pokemon(self, pokemon_name: str):
         for pok in self.pokemons:
             if pok.name == pokemon_name:
                 self.pokemons.remove(pok)
@@ -21,9 +20,19 @@ class Trainer:
         
         return "Pokemon is not caught"
         
-        
     def trainer_data(self):
-        output = f"Pokemon Trainer {self.name}\nPokemon count {len(pokemons)}"
-        for pok in pokemons:
-            output += f"\n- {pokemon.pokemon_details()}"
+        output = f"Pokemon Trainer {self.name}\nPokemon count {len(self.pokemons)}"
+        for pok in self.pokemons:
+            output += f"\n- {pok.pokemon_details()}"
         return output
+
+pokemon = Pokemon("Pikachu", 90)
+print(pokemon.pokemon_details())
+trainer = Trainer("Ash")
+print(trainer.add_pokemon(pokemon))
+second_pokemon = Pokemon("Charizard", 110)
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.trainer_data())
