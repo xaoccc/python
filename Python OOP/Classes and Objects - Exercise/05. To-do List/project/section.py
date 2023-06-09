2a5130661c48092f45c0382029517b0cfba89b84
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-06-09 19:39:54 +0300

    100/100

--- Python OOP/Classes and Objects - Exercise/05. To-do List/project/section.py
+++ Python OOP/Classes and Objects - Exercise/05. To-do List/project/section.py
@@ -1,4 +1,5 @@
-from task import Task
+from project.task import Task
+
 
 class Section:
     def __init__(self, name):
@@ -12,13 +13,12 @@ class Section:
             return f"Task {new_task.details()} is added to the section"
         return f"Task is already in the section {self.name}"
         
-        
     def complete_task(self, task_name: str):
-        if task_name in self.tasks:
-            Task.completed = True
-            return f"Completed task {task_name}"
+        for task in self.tasks:
+            if task.name == task_name:
+                task.completed = True
+                return f"Completed task {task_name}"
         return f"Could not find task with the name {task_name}"
-        
         
     def clean_section(self):
         for task in self.tasks:
@@ -26,10 +26,13 @@ class Section:
                 self.cleaned += 1
                 self.tasks.remove(task)
         return f"Cleared {self.cleaned} tasks."
-        
     
     def view_section(self):
         output = [f"Section {self.name}:"]
         for task in self.tasks:
             output.append(f"{task.details()}")
         return '\n'.join(output)
+
+
+
+
--- Python OOP/Classes and Objects - Exercise/05. To-do List/project/task.py
+++ Python OOP/Classes and Objects - Exercise/05. To-do List/project/task.py
@@ -5,13 +5,11 @@ class Task:
         self.comments = []
         self.completed = False
     
-    
     def change_name(self, new_name: str):
         if self.name != new_name:
             self.name = new_name
             return self.name
         return "Name cannot be the same."
-        
         
     def change_due_date(self, new_date: str):
         if self.due_date != new_date:
@@ -19,17 +17,20 @@ class Task:
             return self.due_date
         return "Date cannot be the same."
 
-
     def add_comment(self, comment: str):
         self.comments.append(comment)
         
-        
     def edit_comment(self, comment_number: int, new_comment: str):
-        if len(self.comments) > comment_number or comment_number >= 0:
+        if len(self.comments) < comment_number or comment_number <= 0:
             self.comments[comment_number] = new_comment
             return ', '.join(self.comments)
         return "Cannot find comment."
-        
+        # WTF????????
+        # if not 0 <= comment_number < len(self.comments):
+        #     return f"Cannot find comment."
+        #
+        # self.comments[comment_number] = new_comment
+        # return ", ".join(self.comments)
         
     def details(self):
         return f"Name: {self.name} - Due Date: {self.due_date}"
