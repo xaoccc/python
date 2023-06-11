43e7813376b500e1ee613e816ed611facebd97ca
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-06-11 15:25:39 +0300

    Update album.py

---
+++ Python OOP/Classes and Objects - Exercise/07. Spoopify/project/album.py
@@ -0,0 +1,17 @@
+from song import Song
+
+class Album:
+    def __init__(self, name: str, *songs):
+        self.name = name
+        self.songs = list(songs)
+        self.published = False
+
+    def add_song(self, song: Song):
+        if song.single:
+            return f"Cannot add {song.name}. It's a single"
+        if self.published:
+            return "Cannot add songs. Album is published."
+        if song in self.songs:
+            return "Song is already in the album."
+        self.songs.append(song)
+        return f"Song {song.name} has been added to the album {self.name}."
