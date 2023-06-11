2c11772106662072609c4d31c77eef17d5926f1b
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-06-11 15:30:20 +0300

    Update album.py

--- Python OOP/Classes and Objects - Exercise/07. Spoopify/project/album.py
+++ Python OOP/Classes and Objects - Exercise/07. Spoopify/project/album.py
@@ -15,3 +15,19 @@ class Album:
             return "Song is already in the album."
         self.songs.append(song)
         return f"Song {song.name} has been added to the album {self.name}."
+
+    def remove_song(self, song_name: str):
+        if song_name not in self.songs:
+            return "Song is not in the album."
+        if self.published:
+            return "Cannot remove songs. Album is published."
+        self.songs.remove(song_name)
+        return f"Removed song {song_name} from album {self.name}."
+
+    def publish(self):
+        if self.published:
+            return f"Album {self.name} is already published."
+        self.published = True
+        return f"Album {self.name} has been published."
+
+
