fa649366291602cf1a55e6d6e072b57ed47e9a06
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-12 12:09:02 +0300

    Update 03. Naughty or Nice 38-100.py

--- Python Advanced/Exams/Python Advanced Retake Exam - 15 December 2021/03. Naughty or Nice 38-100.py
+++ Python Advanced/Exams/Python Advanced Retake Exam - 15 December 2021/03. Naughty or Nice 38-100.py
@@ -51,22 +51,41 @@ def naughty_or_nice_list(fargs, *args, **kwargs):
 #         "Not found": []
 #     }
 #     kids_dict = {}
+#     kids_dict_reversed = {}
+    
 #     for kid in kids:
 #         if kid[0] not in kids_dict:
 #             kids_dict[kid[0]] = []
 #         kids_dict[kid[0]].append(kid[1])
+        
+#     for kid in kids:
+#         if kid[1] not in kids_dict_reversed:
+#             kids_dict_reversed[kid[1]] = []
+#         kids_dict_reversed[kid[1]].append(kid[0])
     
 #     if args:
 #         for i in args:
 #             current = [int(i) if i.isdigit() else i for i in i.split("-")]
-#             if len(kids_dict[current[0]]) == 1:
-#                 santa_list["Naughty"].append(kids_dict[current[0]][0])
+#             if current[0] in kids_dict:
+#                 if len(kids_dict[current[0]]) == 1:
+#                     santa_list[current[1]].append(kids_dict[current[0]][0])
+#                     for kid in kids:
+#                         if kid[0] == current[0] and kid[1] == kids_dict[current[0]][0]:
+#                             kids.remove(kid)
+                    
+                    
         
-#     # if kwargs:
-#     #     for key, value in kwargs.items():
-#     #         pass
+#     if kwargs:
+#         for key, value in kwargs.items():
+#             if key in kids_dict_reversed:
+#                 if len(kids_dict_reversed[key]) == 1:
+#                     santa_list[value].append(key)
+#                     for kid in kids:
+#                         if kids_dict_reversed[key][0] == kid[0] and kid[1] == key:
+#                             kids.remove(kid)
     
-    
-    
+#     if kids:        
+#         for kid in kids:
+#             santa_list["Not found"].append(kid[1])
     
 #     return santa_list
