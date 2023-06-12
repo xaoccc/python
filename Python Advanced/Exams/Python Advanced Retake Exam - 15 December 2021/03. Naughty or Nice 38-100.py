d015260c1707adfbac1172ab05d25f2b5a6ceb16
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-12 12:20:29 +0300

    Update 03. Naughty or Nice 38-100.py

--- Python Advanced/Exams/Python Advanced Retake Exam - 15 December 2021/03. Naughty or Nice 38-100.py
+++ Python Advanced/Exams/Python Advanced Retake Exam - 15 December 2021/03. Naughty or Nice 38-100.py
@@ -1,91 +1,47 @@
-import copy
-
-def naughty_or_nice_list(fargs, *args, **kwargs):
-    santa_list = {}
-    kids_filter = {}
-    all_kids_in_santa_list = []
+def naughty_or_nice_list(kids, *args, **kwargs):
+    santa_list = {
+        "Nice": [],
+        "Naughty": [],
+        "Not found": []
+    }
+    kids_dict = {}
+    kids_dict_reversed = {}
     result = ""
     
-    for kid in fargs:
-        if kid[0] not in santa_list:
-            santa_list[kid[0]] = []
-        santa_list[kid[0]].append(kid[1])
-
-    for i in args:
-        i = i.split("-")
-        kids_filter[int(i[0])] = i[1]
+    for kid in kids:
+        if kid[0] not in kids_dict:
+            kids_dict[kid[0]] = []
+        kids_dict[kid[0]].append(kid[1])
     
-    nice_naughty = {"Nice":[], "Naughty": [], "Not found":[]}
-    for kid in santa_list.values():
-        all_kids_in_santa_list += kid
     
-    for number, kids_names in copy.deepcopy(santa_list).items():
-        if len(kids_names) == 1:
-            if number in kids_filter:
-                nice_naughty[kids_filter[number]].append(kids_names[0])
-                santa_list.pop(number)        
+    if args:
+        for i in args:
+            current = [int(i) if i.isdigit() else i for i in i.split("-")]
+            if current[0] in kids_dict:
+                if len(kids_dict[current[0]]) == 1:
+                    santa_list[current[1]].append(kids_dict[current[0]][0])
+                    for kid in kids:
+                        if kid[0] == current[0] and kid[1] == kids_dict[current[0]][0]:
+                            kids.remove(kid)
+                    
+    for kid in kids:
+        if kid[1] not in kids_dict_reversed:
+            kids_dict_reversed[kid[1]] = []
+        kids_dict_reversed[kid[1]].append(kid[0])                
+        
+    if kwargs:
+        for key, value in kwargs.items():
+            if key in kids_dict_reversed:
+                if len(kids_dict_reversed[key]) == 1:
+                    santa_list[value].append(key)
+                    for kid in kids:
+                        if kids_dict_reversed[key][0] == kid[0] and kid[1] == key:
+                            kids.remove(kid)
     
-              
-    for number, kids_names in copy.deepcopy(santa_list).items():
-        for kid in kids_names:
-            if all_kids_in_santa_list.count(kid) == 1 and number in kids_filter:
-                nice_naughty[kwargs[kid]].append(kid)
-        if number in kids_filter:
-            santa_list.pop(number)
-            
-    if santa_list:
-        for number, kids_names in santa_list.items():
-            for kid in kids_names:
-                nice_naughty["Not found"].append(kid)
-
-    for key, names in nice_naughty.items():
-        if names:
-            result += f"{key}: {', '.join(names)}\n"
-
+    if kids:        
+        for kid in kids:
+            santa_list["Not found"].append(kid[1])
+    for key, value in santa_list.items():
+        if value:
+            result += f"{key}: {', '.join(value)}\n"
     return result
-
-# def naughty_or_nice_list(kids, *args, **kwargs):
-#     santa_list = {
-#         "Nice": [],
-#         "Naughty": [],
-#         "Not found": []
-#     }
-#     kids_dict = {}
-#     kids_dict_reversed = {}
-    
-#     for kid in kids:
-#         if kid[0] not in kids_dict:
-#             kids_dict[kid[0]] = []
-#         kids_dict[kid[0]].append(kid[1])
-        
-#     for kid in kids:
-#         if kid[1] not in kids_dict_reversed:
-#             kids_dict_reversed[kid[1]] = []
-#         kids_dict_reversed[kid[1]].append(kid[0])
-    
-#     if args:
-#         for i in args:
-#             current = [int(i) if i.isdigit() else i for i in i.split("-")]
-#             if current[0] in kids_dict:
-#                 if len(kids_dict[current[0]]) == 1:
-#                     santa_list[current[1]].append(kids_dict[current[0]][0])
-#                     for kid in kids:
-#                         if kid[0] == current[0] and kid[1] == kids_dict[current[0]][0]:
-#                             kids.remove(kid)
-                    
-                    
-        
-#     if kwargs:
-#         for key, value in kwargs.items():
-#             if key in kids_dict_reversed:
-#                 if len(kids_dict_reversed[key]) == 1:
-#                     santa_list[value].append(key)
-#                     for kid in kids:
-#                         if kids_dict_reversed[key][0] == kid[0] and kid[1] == key:
-#                             kids.remove(kid)
-    
-#     if kids:        
-#         for kid in kids:
-#             santa_list["Not found"].append(kid[1])
-    
-#     return santa_list
