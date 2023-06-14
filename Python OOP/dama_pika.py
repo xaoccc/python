c0ed2ff57bc55239bef91f25476b02df5e2173a7
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-14 14:32:38 +0300

    Update dama_pika.py

--- Python OOP/dama_pika.py
+++ Python OOP/dama_pika.py
@@ -8,10 +8,36 @@ def ritual(func):
         func(*args, **kwargs)
     return wrapper
 
-# Pass data into the decorator, so that dama_pika(name) == func  
+# Pass data into the decorator, so that the result of dama_pika(name) == func  
 @ritual
 def dama_pika(name):
     print(f"{name}")
 
 # Call the decorated function    
 dama_pika("Dama Pika!")
+
+
+
+
+def ritual(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+        # If we dont return here,  dama_pika(name) will also not return any value, just print 3 times Dama Pika!!!!
+        # In this case, we return the argument given  - dama_pika("Dama Pika!") and print it as well
+        return func(*args, **kwargs)
+    return wrapper
+
+@ritual
+def dama_pika(name):
+    print("Dama Pika!!!!")
+    return f"{name}"
+    
+test = dama_pika("Dama Pika!")
+print(test)
+
+# Output:
+# Dama Pika!!!!
+# Dama Pika!!!!
+# Dama Pika!!!!
+# Dama Pika!
