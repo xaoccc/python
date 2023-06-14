27c826f21717423da03c87233d9cbfcec147c6fb
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-14 15:57:35 +0300

    Update 03. Grocery List91-100.py

--- Python Advanced/Exams/Python Advanced Exam - 18 February 2023/03. Grocery List91-100.py
+++ Python Advanced/Exams/Python Advanced Exam - 18 February 2023/03. Grocery List91-100.py
@@ -1,16 +1,15 @@
-def shop_from_grocery_list(budget, grocery_list, *args):
-    i = 0
-    while i < len(args) and budget >= 0:
-        product, price = args[i][0], args[i][1]
-        
-        if grocery_list:
-            budget -= price
-            if product in grocery_list and budget >= 0:
-                grocery_list.remove(product)
-
-        i += 1 
-        if not grocery_list:
+def shop_from_grocery_list(budget, groceries_list, *products):
+    bought_products = []
+    
+    for product in products:
+        if product[0] in groceries_list and product[0] not in bought_products:
+            if product[1] <= budget:
+                budget -= product[1]
+                groceries_list.remove(product[0])
+                bought_products.append(product[0])
+            else:
+                return f"You did not buy all the products. Missing products: {', '.join(groceries_list)}."
+        if not groceries_list:
             return f"Shopping is successful. Remaining budget: {budget:.2f}."
-           
-    if budget < 0 or grocery_list:
-        return f"You did not buy all the products. Missing products: {', '.join(grocery_list)}."
+        
+    return f"You did not buy all the products. Missing products: {', '.join(groceries_list)}."
