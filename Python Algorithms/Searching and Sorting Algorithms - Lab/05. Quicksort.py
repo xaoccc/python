7e88e89d4d8aa62f53442d9cec5598f33d0bf440
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-16 11:37:21 +0300

    Update 05. Quicksort.py

--- Python Algorithms/Searching and Sorting Algorithms - Lab/05. Quicksort.py
+++ Python Algorithms/Searching and Sorting Algorithms - Lab/05. Quicksort.py
@@ -2,10 +2,14 @@
 def quicksort(start, end, nums):
     if start >= end:
         return
+    # We take the first number from the list
     pivot = start
     left = start + 1
     right = end
+
+    # Here we sort the list in two parts - smaller or equal the pivot(first half) and bigger than the pivot (secon half) 
     while left <= right:
+        # We swap the numbers, if we find smaller number in the second half:
         if nums[left] > nums[pivot] > nums[right]:
             nums[left], nums[right] = nums[right], nums[left]
 
@@ -14,8 +18,13 @@ def quicksort(start, end, nums):
 
         if nums[right] >= nums[pivot]:
             right -= 1
+    # We move the current pivot after the list of smaller/equal nums, 
+    # so in the worst case, the pivot will be equal to the biggest number in this sub-list, which is ok
     nums[pivot], nums[right] = nums[right], nums[pivot]
+
+    # Do the same for the first half:
     quicksort(start, right - 1, nums)
+    # Then for the second half
     quicksort(left, end, nums)
     return nums
 
