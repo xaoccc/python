ddb5ac710eab9487bad762d4b2dc50a6bc2d3c11
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-23 14:02:08 +0300

    Update 2469. Convert the Temperature.py

--- Leetcode/2469. Convert the Temperature.py
+++ Leetcode/2469. Convert the Temperature.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def convertTemperature(self, celsius):
-        """
-        :type celsius: float
-        :rtype: List[float]
-        """
         kf = []
         kf.extend((celsius + 273.15, celsius * 1.80 + 32))
         return kf
