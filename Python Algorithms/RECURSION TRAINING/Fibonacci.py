a25af827eeffc525a40d0ebdc63d447d34e4234c
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-27 15:47:02 +0300

    Update Fibonacci.py

--- Python Algorithms/RECURSION TRAINING/Fibonacci.py
+++ Python Algorithms/RECURSION TRAINING/Fibonacci.py
@@ -3,11 +3,11 @@ result = [0, 1, 1]
 
 def fib(i):
     global result
-    if i < 1:
+    if i < 3:
         return 1
     result.append(fib(i - 2) + fib(i - 1))
     current_num = result[-1]
     return current_num
 
 fib(n)
-print(result[:3] + result[-n:])
+print(result[:3] + result[-n+2:])
