b93437c44e32e3a015bca6a800822fd9b9fef2eb
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-06-29 18:59:33 +0300

    Update 02. Mammal.py

--- Python OOP/Encapsulation - Lab/02. Mammal.py
+++ Python OOP/Encapsulation - Lab/02. Mammal.py
@@ -9,7 +9,7 @@ class Mammal:
         return f"{self.name} makes {self.sound}"
     
     def get_kingdom(self): 
-        return Mammal.__kingdom
+        return self.__kingdom
         
     def info(self):
         return f"{self.name} is of type {self.type}"
