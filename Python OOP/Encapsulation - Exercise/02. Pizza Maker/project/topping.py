71294783e0c363c852018488dc651d0631202e2e
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-06-30 21:39:10 +0300

    Update topping.py

--- Python OOP/Encapsulation - Exercise/02. Pizza Maker/project/topping.py
+++ Python OOP/Encapsulation - Exercise/02. Pizza Maker/project/topping.py
@@ -13,7 +13,7 @@ class Topping:
     
     @topping_type.setter
     def topping_type(self, value):
-        if not value:
+        if value == "":
             raise ValueError("The topping type cannot be an empty string")
         self.__topping_type = value
             
