1a7aa55fc1e4e35bb53047cd50dc1f389ec8994a
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-06-30 15:46:18 +0300

    Create 01. Calculator.py

---
+++ Python OOP/Static and Class Methods - Lab/01. Calculator.py
@@ -0,0 +1,22 @@
+class Calculator:
+    @staticmethod
+    def add(*args):
+        return sum(args)
+    @staticmethod
+    def multiply(*args):
+        result = 1
+        for i in args:
+            result *= i
+        return result
+    @staticmethod
+    def subtract(*args):
+        result = args[0]
+        for i in range(1, len(args)):
+            result -= args[i]
+        return result
+    @staticmethod
+    def divide(*args):
+        result = args[0]
+        for i in range(1, len(args)):
+            result /= args[i]
+        return result
