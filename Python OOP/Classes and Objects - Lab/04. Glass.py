a6e4d88abbf625f95ed3cfab2c9a96a8858f9c38
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-01 17:15:43 +0300

    Update 04. Glass.py

--- Python OOP/Classes and Objects - Lab/04. Glass.py
+++ Python OOP/Classes and Objects - Lab/04. Glass.py
@@ -1,8 +1,8 @@
 class Glass:
     capacity = 250
+    
     def __init__(self):
         self.content = 0
-        
         
     def fill(self, ml):
         if self.capacity - self.content >= ml:
@@ -10,11 +10,9 @@ class Glass:
             return f"Glass filled with {ml} ml"
         return f"Cannot add {ml} ml"
     
-    
     def empty(self):
         self.content = 0
         return "Glass is now empty"
-        
     
     def info(self):
         return f"{self.capacity - self.content} ml left"
