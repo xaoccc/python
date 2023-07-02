5cca35c5d857ab2d803d6130dc7956295cfc20b1
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-02 20:41:17 +0300

    Update 01. Calculator.py

--- Python OOP/Static and Class Methods - Lab/01. Calculator.py
+++ Python OOP/Static and Class Methods - Lab/01. Calculator.py
@@ -2,18 +2,21 @@ class Calculator:
     @staticmethod
     def add(*args):
         return sum(args)
+
     @staticmethod
     def multiply(*args):
         result = 1
         for i in args:
             result *= i
         return result
+
     @staticmethod
     def subtract(*args):
         result = args[0]
         for i in range(1, len(args)):
             result -= args[i]
         return result
+
     @staticmethod
     def divide(*args):
         result = args[0]
