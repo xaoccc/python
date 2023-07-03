59dbd22da63fdb87175b03ee57ca6a92540f2028
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-03 17:06:31 +0300

    Update and rename 01. Photo Album.py to 01. Photo Album71-100.py

--- Python OOP/Static and Class Methods - Exercise/01. Photo Album71-100.py
+++ Python OOP/Static and Class Methods - Exercise/01. Photo Album71-100.py
@@ -4,7 +4,6 @@ class PhotoAlbum:
     def __init__(self, pages):
         self.pages = pages
         self.photos = [[] for i in range(self.pages)]
-
         
     @classmethod
     def from_photos_count(cls, photos_count):
@@ -15,3 +14,12 @@ class PhotoAlbum:
             if len(self.photos[i]) < 4:
                 self.photos[i].append(label)
                 return f"{label} photo added successfully on page {i + 1} slot {self.photos[i].index(label) + 1}."
+            
+    def display(self):
+        result = "-----------\n"
+        for i in range(self.pages):
+            for j in range(len(self.photos[i])):
+                result += "[] "
+            result = result.strip()
+            result += "\n-----------\n"
+        return result
