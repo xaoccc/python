7d1970446e9baf0677d33f0e5dae61c8e3c7d0e3
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-03 17:26:40 +0300

    Update 01. Photo Album71-100.py

--- Python OOP/Static and Class Methods - Exercise/01. Photo Album71-100.py
+++ Python OOP/Static and Class Methods - Exercise/01. Photo Album71-100.py
@@ -10,16 +10,19 @@ class PhotoAlbum:
         return cls(ceil(photos_count / 4))
     
     def add_photo(self, label):
+        if len(self.photos[-1]) == 4:
+            return "No more free slots"
         for i in range(len(self.photos)):
             if len(self.photos[i]) < 4:
                 self.photos[i].append(label)
                 return f"{label} photo added successfully on page {i + 1} slot {self.photos[i].index(label) + 1}."
             
     def display(self):
-        result = "-----------\n"
+        result = "\n-----------\n"
         for i in range(self.pages):
             for j in range(len(self.photos[i])):
                 result += "[] "
-            result = result.strip()
+            if len(self.photos[i]) != 0:
+                result = result.strip()
             result += "\n-----------\n"
         return result
