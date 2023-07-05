f14c4320177e74a86cb53d6d173a9648df2491ed
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-05 15:55:49 +0300

    Add files via upload

---
+++ Python OOP/Polymorphism and Abstraction - Exercise/project/food.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class Food(ABC):
+    def __init__(self, quantity):
+        self.quantity = quantity
+        
+
+class Vegetable(Food):
+    pass
+
+class Fruit(Food):
+    pass
+
+class Meat(Food):
+    pass
+
+class Seed(Food):
+    pass
