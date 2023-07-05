c253e9f8ee87a9dbf2f083da0d6442f43da0fa89
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-05 10:58:25 +0300

    Update Shapes.py

--- Python OOP/Polymorphism and Abstraction - Lab/Shapes.py
+++ Python OOP/Polymorphism and Abstraction - Lab/Shapes.py
@@ -27,11 +27,33 @@ class Circle(Shape):
     def calculate_area(self):
         return math.pi * (self.radius ** 2)
 
+class Trapezoid(Shape):
+    def __init__(self, base_side, top_side, height):
+        self.base_side = base_side
+        self.top_side = top_side
+        self.height = height
+     
+    def calculate_area(self):
+        return (self.base_side + self.top_side) * self.height / 2 
+        
+        
+class Octagon(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def calculate_area(self):
+        return self.side ** 2 * (1 + math.sqrt(2)) * 2
+        
 # Usage
 rectangle = Rectangle(5, 10)
 triangle = Triangle(4, 6)
 circle = Circle(3)
+trapezoid = Trapezoid(13, 9, 4)
+octagon = Octagon(6)
 
+# We can just specify the shape and call the SAME method:
 print(rectangle.calculate_area())  # Output: 50
 print(triangle.calculate_area())  # Output: 12.0
 print(circle.calculate_area())  # Output: 28.274333882308138
+print(trapezoid.calculate_area())  # Output: 44.0   
+print(octagon.calculate_area())  # Output: 173.82337649086284
