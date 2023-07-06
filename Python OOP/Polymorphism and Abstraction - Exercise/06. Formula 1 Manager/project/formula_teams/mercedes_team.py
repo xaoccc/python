a4e84470073ad173b9af2a5258885947cdda0659
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-06 12:23:21 +0300

    Update mercedes_team.py

--- Python OOP/Polymorphism and Abstraction - Exercise/06. Formula 1 Manager/project/formula_teams/mercedes_team.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/06. Formula 1 Manager/project/formula_teams/mercedes_team.py
@@ -3,6 +3,13 @@ from project.formula_teams.formula_team import FormulaTeam
 
 class MercedesTeam(FormulaTeam):
     def calculate_revenue_after_race(self, race_pos):
-        profits = {1: 1000000, 3: 500000, 5: 100000, 7: 50000}
-        self.budget += profits[race_pos] - 200000
-        return f"The revenue after the race is { profits[race_pos] - 200000 }$. Current budget { self.budget }$"
+        revenue, expences = 0, 200000
+        sponsors_profits = {"Petronas": {1: 1000000, 3: 500000}, "TeamViewer": {5: 100000, 7: 50000}}
+        for sponspor, profit in sponsors_profits.items():
+            for position, money in profit.items():
+                if race_pos <= position:
+                    revenue += money 
+                    break
+        revenue -= expences
+        self.budget += revenue
+        return f"The revenue after the race is { revenue }$. Current budget { self.budget }$"
