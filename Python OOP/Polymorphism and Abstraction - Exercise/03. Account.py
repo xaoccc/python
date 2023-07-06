b47471a77cdd549c0ff2146cdef90a51887d964f
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-06 16:36:28 +0300

    Create 03. Account.py

---
+++ Python OOP/Polymorphism and Abstraction - Exercise/03. Account.py
@@ -0,0 +1,41 @@
+
+class Account:
+    def __init__(self, owner, amount=0):
+        self.owner = owner
+        self.amount = amount
+        self._transactions = []
+        
+
+    def handle_transaction(self, transaction_amount):
+        if self.amount + transaction_amount < 0:
+            raise ValueError("sorry cannot go in debt!")
+        self.amount += transaction_amount
+        self._transactions.append(transaction_amount)
+        
+    def add_transaction(self, amount):
+        if type(amount) != int:
+            raise ValueError("please use int for amount")
+        if self.amount + amount < 0:
+            raise ValueError("sorry cannot go in debt!")
+        self.amount += amount
+        self._transactions.append(amount)
+        return f"New balance: {self.amount}"
+    
+    def __str__(self):
+        return f"Account of {self.owner} with starting amount: {self.amount}"
+        
+    @property
+    def balance(self):
+        return self.amount
+        
+    def __repr__(self):
+        return f"Account({self.owner}, {self.amount})"
+        
+    def __len__(self):
+        return len(self._transactions)
+        
+    def __getitem__(self, i):
+        return self._transactions[i]
+        
+    def __reversed__(self):
+        return reverse(self._transactions.reversed)
