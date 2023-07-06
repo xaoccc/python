38f9472837254b6caf2e9fab42d8230605ab3cb5
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-06 17:06:47 +0300

    Update 03. Account.py

--- Python OOP/Polymorphism and Abstraction - Exercise/03. Account.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/03. Account.py
@@ -1,16 +1,15 @@
-
 class Account:
     def __init__(self, owner, amount=0):
         self.owner = owner
         self.amount = amount
         self._transactions = []
-        
 
     def handle_transaction(self, transaction_amount):
         if self.amount + transaction_amount < 0:
             raise ValueError("sorry cannot go in debt!")
         self.amount += transaction_amount
         self._transactions.append(transaction_amount)
+        return f"New balance: {self.amount}"
         
     def add_transaction(self, amount):
         if type(amount) != int:
@@ -38,4 +37,28 @@ class Account:
         return self._transactions[i]
         
     def __reversed__(self):
-        return reverse(self._transactions.reversed)
+        return reversed(self._transactions)
+        
+    def __lt__(self, other):
+        return self.amount < other.amount
+        
+    def __le__(self, other):
+        return self.amount <= other.amount 
+        
+    def __eq__(self, other):
+        return self.amount == other.amount
+        
+    def __ge__(self, other):
+        return self.amount >= other.amount
+    
+    def __gt__(self, other):
+        return self.amount > other.amount
+        
+    def __ne__(self, other):
+        return self.amount != other.amount
+        
+    def __add__(self, other):
+        combined_transactions = self._transactions + other._transactions
+        new_acc = Account(f"{self.owner}&{other.owner}", self.amount + other.amount)
+        new_acc._transactions = combined_transactions
+        return new_acc
