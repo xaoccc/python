236acebf5e6180ecac02af2843f6026efab15f28
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-06 12:22:38 +0300

    Update f1_season_app.py

--- Python OOP/Polymorphism and Abstraction - Exercise/06. Formula 1 Manager/project/f1_season_app.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/06. Formula 1 Manager/project/f1_season_app.py
@@ -12,14 +12,16 @@ class F1SeasonApp:
             self.red_bull_team = RedBullTeam(budget)
             return f"{team_name} has joined the new F1 season."
 
-        if team_name == "Mercedes":
+        elif team_name == "Mercedes":
             self.mercedes_team = MercedesTeam(budget)
             return f"{team_name} has joined the new F1 season."
 
-        return ValueError("Invalid team name!")
+        else:
+            raise ValueError("Invalid team name!")
+         
 
     def new_race_results(self, race_name, red_bull_pos, mercedes_pos):
         if self.red_bull_team is None or self.mercedes_team is None:
             raise Exception("Not all teams have registered for the season.")
-
-        return f"Red Bull: { self.red_bull_team.calculate_revenue_after_race(red_bull_pos) }. Mercedes: { self.mercedes_team.calculate_revenue_after_race(mercedes_pos) }. {min([red_bull_pos, mercedes_pos])} is ahead at the { race_name } race."
+        winner = "Mercedes" if red_bull_pos > mercedes_pos else "Red Bull"
+        return f"Red Bull: { self.red_bull_team.calculate_revenue_after_race(red_bull_pos) }. Mercedes: { self.mercedes_team.calculate_revenue_after_race(mercedes_pos) }. {winner} is ahead at the { race_name } race."
