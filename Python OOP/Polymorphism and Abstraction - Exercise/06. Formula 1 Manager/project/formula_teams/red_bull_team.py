91399904d346a53af35d3c3085e5f1c125470699
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-06 10:29:41 +0300

    Update red_bull_team.py

--- Python OOP/Polymorphism and Abstraction - Exercise/06. Formula 1 Manager/project/formula_teams/red_bull_team.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/06. Formula 1 Manager/project/formula_teams/red_bull_team.py
@@ -1,7 +1,8 @@
-from project.f1_season_app import F1SeasonApp
 from project.formula_teams.formula_team import FormulaTeam
 
 
 class RedBullTeam(FormulaTeam):
     def calculate_revenue_after_race(self, race_pos):
-        pass
+        profits = {1: 1500000, 2: 800000, 8: 20000, 10: 10000}
+        self.budget += profits[race_pos] - 250000
+        return f"The revenue after the race is { profits[race_pos] - 250000 }$. Current budget { self.budget }$"
