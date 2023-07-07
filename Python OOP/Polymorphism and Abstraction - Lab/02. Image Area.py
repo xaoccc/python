be53be097142a5440d84e9783be4c753a6b136d7
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-07 11:42:17 +0300

    Update 02. Image Area.py

--- Python OOP/Polymorphism and Abstraction - Lab/02. Image Area.py
+++ Python OOP/Polymorphism and Abstraction - Lab/02. Image Area.py
@@ -1,21 +1,16 @@
 class ImageArea:
     def __init__(self, width, height):
         self.width = width
-        self.height = height
-        self.area = width * height        
+        self.height = height     
 
     def get_area(self):
         return self.width * self.height
     
     def __lt__(self, other):
-        return self.area < other.area
+        return self.get_area() < other.get_area()
 
     def __le__(self, other):
-        return self.area <= other.area
+        return self.get_area() <= other.get_area()
 
     def __eq__(self, other):
-        return self.area == other.area
-
-
-
-
+        return self.get_area() == other.get_area()
