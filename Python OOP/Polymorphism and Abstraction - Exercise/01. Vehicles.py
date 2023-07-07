56183509d50c12f278f74fe98348f6af7fcf81ba
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-07 19:00:22 +0300

    Update 01. Vehicles.py

--- Python OOP/Polymorphism and Abstraction - Exercise/01. Vehicles.py
+++ Python OOP/Polymorphism and Abstraction - Exercise/01. Vehicles.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class Vehicle(ABC):
     def __init__(self, fuel_quantity, fuel_consumption):
         self.fuel_quantity = fuel_quantity
@@ -12,7 +13,8 @@ class Vehicle(ABC):
     @abstractmethod
     def refuel(self, fuel):
         pass
-        
+
+
 class Car(Vehicle):
     def drive(self, distance):
         if self.fuel_quantity >= distance * (self.fuel_consumption + 0.9):
@@ -20,7 +22,8 @@ class Car(Vehicle):
     
     def refuel(self, fuel):
         self.fuel_quantity += fuel
-        
+
+
 class Truck(Vehicle):
     def drive(self, distance):
         if self.fuel_quantity >= distance * (self.fuel_consumption + 1.6):
