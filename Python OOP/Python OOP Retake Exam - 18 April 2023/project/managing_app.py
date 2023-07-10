6095000796005b0c0b71042ec26d2ff0513fb0aa
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-10 22:57:11 +0300

    128/150

--- Python OOP/Python OOP Retake Exam - 18 April 2023/project/managing_app.py
+++ Python OOP/Python OOP Retake Exam - 18 April 2023/project/managing_app.py
@@ -88,30 +88,5 @@ class ManagingApp:
             result += f"\n{user}"
         return result
 
-app = ManagingApp()
 
-print(app.register_user( 'Tisha', 'Reenie', '7246506' ))
-print(app.register_user( 'Bernard', 'Remy', 'CDYHVSR68661'))
-print(app.register_user( 'Ivan', 'Pesho', '666'))
-print(app.register_user( 'Mack', 'Cindi', '7246506'))
-print(app.upload_vehicle('PassengerCar', 'Chevrolet', 'Volt', 'CWP8032'))
-print(app.upload_vehicle( 'PassengerCar', 'Volkswagen', 'e-Up!', 'COUN199728'))
-print(app.upload_vehicle('PassengerCar', 'Mercedes-Benz', 'EQS', '5UNM315'))
-print(app.upload_vehicle('CargoVan', 'Ford', 'e-Transit', '726QOA'))
-print(app.upload_vehicle('CargoVan', 'BrightDrop', 'Zevo400', 'SC39690'))
-print(app.upload_vehicle('EcoTruck', 'Mercedes-Benz', 'eActros', 'SC39690'))
-print(app.upload_vehicle('PassengerCar', 'Tesla', 'CyberTruck', '726QOA'))
-print(app.allow_route('SOF', 'PLD', 144))
-print(app.allow_route('BUR', 'VAR', 87))
-print(app.allow_route('BUR', 'VAR', 87))
-print(app.allow_route('SOF', 'PLD', 184))
-print(app.allow_route('BUR', 'VAR', 86.999))
-print(app.make_trip('CDYHVSR68661', '5UNM315', 3, False))
-print(app.make_trip('7246506', 'CWP8032', 1, True))
-print(app.make_trip('SC39690', 'COUN199728', 1, True))
-print(app.make_trip('CDYHVSR68661', 'CWP8032', 3, True))
-print(app.make_trip('CDYHVSR68661', '666', 4, True))
-
-print(app.repair_vehicles(20))
-print(app.users_report())
 
