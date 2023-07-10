856b94dc349942e432df97afb3cda698241e43c7
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-10 22:03:08 +0300

    abc

--- Python OOP/Python OOP Retake Exam - 18 April 2023/project/vehicles/passenger_car.py
+++ Python OOP/Python OOP Retake Exam - 18 April 2023/project/vehicles/passenger_car.py
@@ -1,9 +1,9 @@
 from project.vehicles.base_vehicle import BaseVehicle
 
+
 class PassengerCar(BaseVehicle):
     def __init__(self, brand, model, license_plate_number, max_mileage=450.00):
         super().__init__(brand, model, license_plate_number, max_mileage)
 
-    
     def drive(self, mileage: float):
         self.battery_level -= int((self.battery_level * (mileage / self.max_mileage)))
