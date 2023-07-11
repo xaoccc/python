8ae7fa6b5fa7c592e6a9dcbf2c0fefd47e065959
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-11 13:54:18 +0300

    Add files via upload

---
+++ Python OOP/Python OOP Exam - 8 April 2023/project/services/secondary_service.py
@@ -0,0 +1,11 @@
+from project.robots.female_robot import FemaleRobot
+from project.robots.male_robot import MaleRobot
+from project.services.base_service import BaseService
+
+
+class SecondaryService(BaseService):
+    def __init__(self, name, capacity=15):
+        super.__init__(name, capacity)
+    
+    def details(self):
+        return f"{self.name} Secondary Service:\nRobots: {' '.join([robot.name for robot in self.robots]) if self.robots else 'none'}"
