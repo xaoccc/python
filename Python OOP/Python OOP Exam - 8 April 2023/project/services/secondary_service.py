22d66ef1d61511c33688699f13a3bf8236ed5d0d
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-11 14:08:18 +0300

    Update secondary_service.py

--- Python OOP/Python OOP Exam - 8 April 2023/project/services/secondary_service.py
+++ Python OOP/Python OOP Exam - 8 April 2023/project/services/secondary_service.py
@@ -5,7 +5,7 @@ from project.services.base_service import BaseService
 
 class SecondaryService(BaseService):
     def __init__(self, name, capacity=15):
-        super.__init__(name, capacity)
+        super().__init__(name, capacity)
     
     def details(self):
         return f"{self.name} Secondary Service:\nRobots: {' '.join([robot.name for robot in self.robots]) if self.robots else 'none'}"
