e9d113d2d62c28b1d376fb8b590d4f848aa65bdb
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-11 14:08:10 +0300

    Update main_service.py

--- Python OOP/Python OOP Exam - 8 April 2023/project/services/main_service.py
+++ Python OOP/Python OOP Exam - 8 April 2023/project/services/main_service.py
@@ -5,7 +5,7 @@ from project.services.base_service import BaseService
 
 class MainService(BaseService):
     def __init__(self, name, capacity=30):
-        super.__init__(name, capacity)
+        super().__init__(name, capacity)
         
     def details(self):
         return f"{self.name} Main Service:\nRobots: {' '.join([robot.name for robot in self.robots]) if self.robots else 'none'}" 
