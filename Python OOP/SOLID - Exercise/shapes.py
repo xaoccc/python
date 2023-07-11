815eed4a59674cbbae843595d40c0408b79c8c2e
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-11 22:44:31 +0300

    Update shapes.py

--- Python OOP/SOLID - Exercise/shapes.py
+++ Python OOP/SOLID - Exercise/shapes.py
@@ -1,8 +1,26 @@
-class Rectangle:
+from abc import ABC, abstractmethod
 
+
+class Shape(ABC):
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
+    @abstractmethod
+    def get_area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def get_area(self):
+        return self.width * self.height
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        super().__init__(base, height)
+
+    def get_area(self):
+        return self.base * self.height
 
 class AreaCalculator:
 
