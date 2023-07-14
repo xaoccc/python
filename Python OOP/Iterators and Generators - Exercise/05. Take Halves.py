25def68b9b40ca64262e159859bc8e7630763056
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-14 13:25:32 +0300

    Update 05. Take Halves.py

--- Python OOP/Iterators and Generators - Exercise/05. Take Halves.py
+++ Python OOP/Iterators and Generators - Exercise/05. Take Halves.py
@@ -1,6 +1,6 @@
 def solution():
     def integers():
-        i = 0
+        i = 1
         while True:
             yield i
             i += 1
@@ -9,18 +9,12 @@ def solution():
         for i in integers():
             yield i / 2
 
-    # TODO: Implement
     def take(n, seq):
-        m = 0
-        while m <  n:
-            yield seq[m]
-            m += 1
-
-    # TODO: Implement
-    return take, halves, integers
+        result = []
+        for i in range(n):
+            result.append(next(seq))
+        return result
 
 
-take = solution()[0]
-halves = solution()[1]
-print(take(5, halves()))
+    return (take, halves, integers)
 
