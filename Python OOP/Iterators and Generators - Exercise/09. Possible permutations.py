fb829a31dba159d495c41b6e07a119f4d0e92595
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-15 15:41:22 +0300

    Update 09. Possible permutations.py

--- Python OOP/Iterators and Generators - Exercise/09. Possible permutations.py
+++ Python OOP/Iterators and Generators - Exercise/09. Possible permutations.py
@@ -1,6 +1,7 @@
+from itertools import permutations
+
+
 def possible_permutations(nums):
-    
+    for i in list(permutations(nums)):
+        yield list(i)
 
-
-
-[print(n) for n in possible_permutations([1, 2, 3])]
