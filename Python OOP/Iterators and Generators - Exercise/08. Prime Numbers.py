9deb9b4b652d221b9133d3a1b3086ec6043b5b22
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-15 15:17:42 +0300

    Create 08. Prime Numbers.py

---
+++ Python OOP/Iterators and Generators - Exercise/08. Prime Numbers.py
@@ -0,0 +1,12 @@
+def get_primes(nums):
+    for num in nums:
+        if num <= 1:
+            continue
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            yield num
+
+
+print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
