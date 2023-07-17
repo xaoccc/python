614a3176e6fc29f95d6504de06d215b5d91d5550
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-17 15:16:57 +0300

    Update 02. Vowel Filter.py

--- Python OOP/Decorators - Lab/02. Vowel Filter.py
+++ Python OOP/Decorators - Lab/02. Vowel Filter.py
@@ -1,5 +1,4 @@
 def vowel_filter(function):
     def wrapper():
-        vowels = ["a", "e", "o", "i", "u", "y"]
-        return [i for i in function() if i in vowels]
+        return [i for i in function() if i.lower() in "aeiouy"]
     return wrapper
