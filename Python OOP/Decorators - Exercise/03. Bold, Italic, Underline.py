75dd9e03d0d81e73c0d81cc680e0f02d7d7feee5
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-18 18:36:51 +0300

    Update 03. Bold, Italic, Underline.py

--- Python OOP/Decorators - Exercise/03. Bold, Italic, Underline.py
+++ Python OOP/Decorators - Exercise/03. Bold, Italic, Underline.py
@@ -2,12 +2,14 @@ def make_bold(function):
     def wrapper(*text):
         return f"<b>{function(*text)}</b>"
     return wrapper
+
     
 def make_italic(function):
     def wrapper(*text):
         return f"<i>{function(*text)}</i>"
     return wrapper
-    
+
+
 def make_underline(function):
     def wrapper(*text):
         return f"<u>{function(*text)}</u>"
