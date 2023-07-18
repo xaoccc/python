dfaff67b56b6161255eba5812efa3ae0657bebd0
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-18 11:08:58 +0300

    Update 05. Cache.py

--- Python OOP/Decorators - Exercise/05. Cache.py
+++ Python OOP/Decorators - Exercise/05. Cache.py
@@ -1,30 +1,14 @@
-def cache():
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
-
-
-# TODO: Implement
-
-@cache()
+def cache(func):
+    def wrapper(*args, **kwargs):
+        wrapper.log[args[0]] = func(*args, **kwargs)
+        return func(*args, **kwargs)
+    wrapper.log = {}
+    return wrapper
+    
+@cache
 def fibonacci(n):
-    log1 = {}
-    if n not in log1:
-        if n < 2:
-            log1[n] = n
-            return n
-        else:
-            log1[n] = fibonacci(n - 1) + fibonacci(n - 2)
-            return fibonacci(n - 1) + fibonacci(n - 2)
-
-    def log(log1):
-        return log1
-
-
-
-
-print(fibonacci(3))
-print(fibonacci.log)
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
