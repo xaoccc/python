5c311bbfef178b3fcd71d1a58eb43b54d7f289f3
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-19 15:23:33 +0300

    Update 03. List.py

--- Python OOP/Testing - Lab/03. List.py
+++ Python OOP/Testing - Lab/03. List.py
@@ -1,6 +1,5 @@
 import unittest
 
-
 class IntegerList:
     def __init__(self, *args):
         self.integers = []
@@ -18,42 +17,105 @@ class IntegerList:
         try:
             self.integers.pop(idx)
         except:
-            raise IndexError()
+            raise IndexError("Index is out of range")
             
     def get(self, idx):
         try:
             return self.integers[idx]
         except:
-            raise IndexError()
+            raise IndexError("Index is out of range")
             
     def insert(self, idx, num):
-        try: 
-            self.integers.insert(idx, num)
-        except:
-            if not isinstance(num, int):
-                raise ValueError("Element is not Integer")  
-            raise IndexError()
+        if idx < 0 or idx > len(self.integers):
+            raise IndexError("Index is out of range")
+            
+        if not isinstance(num, int):
+                raise ValueError("Element is not Integer")
+                
+        self.integers.insert(idx, num)
+
+             
+            
             
     def get_biggest(self):
         return max(self.integers)
         
     def get_index(self, num):
-        try:
-            return self.integers.index(num)
-        except:
-            raise IndexError()
+        return self.integers.index(num)
+
 
 
         
 
 class ListTests(unittest.TestCase):
     def setUp(self):
-        self.my_list = IntegerList(1,2,3)
+        self.my_list = IntegerList(1,2,"3",3)
         
     def test_constructor(self):
         result = self.my_list.integers
         expected_result = [1,2,3]
         self.assertEqual(result, expected_result)
+        
+    def test_add_valid(self):
+        self.my_list.add(7)
+        result = self.my_list.integers
+        expected_result = [1,2,3,7]
+        self.assertEqual(result, expected_result)
+        
+    def test_add_invalid(self):
+        with self.assertRaises(ValueError) as context:
+            self.my_list.add("7")
+        self.assertEqual("Element is not Integer", str(context.exception))
+        
+    def test_remove_valid_index(self):
+        self.my_list.remove_index(1)
+        result = self.my_list.integers
+        expected_result = [1,3]
+        self.assertEqual(result, expected_result)
+        
+    def test_remove_invalid_index(self):
+        with self.assertRaises(IndexError) as context:
+            self.my_list.remove_index(10)
+        self.assertEqual("Index is out of range", str(context.exception))
+        
+    def test_get_valid_index(self):
+        self.my_list.get(2)
+        result = self.my_list.integers[2]
+        expected_result = 3
+        self.assertEqual(result, expected_result)
+        
+    def test_get_invalid_index(self):
+        with self.assertRaises(IndexError) as context:
+            self.my_list.get(20)
+        self.assertEqual("Index is out of range", str(context.exception))
+        
+    def test_insert_valid_type_valid_index(self):
+        self.my_list.insert(3, 6)
+        result = self.my_list.integers
+        expected_result = [1,2,3,6]
+        self.assertEqual(result, expected_result)
+        
+    def test_insert_invalid_index(self):
+        with self.assertRaises(IndexError) as context:
+            self.my_list.insert(4, 6)
+        self.assertEqual("Index is out of range", str(context.exception))
+        
+    def test_insert_invalid_type(self):
+        with self.assertRaises(ValueError) as context:
+            self.my_list.insert(3, "6")
+        self.assertEqual("Element is not Integer", str(context.exception))
+        
+    def test_get_biggest(self):
+        result = self.my_list.get_biggest()
+        expected_result = 3
+        self.assertEqual(result, expected_result)
+        
+    def test_get_index_valid_index(self):
+        result = self.my_list.get_index(3)
+        expected_result = 2
+        self.assertEqual(result, expected_result)
+        
+
         
         
 if __name__ == "__main__":
