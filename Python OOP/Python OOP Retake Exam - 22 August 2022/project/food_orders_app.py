31e354bbff2b23919245cb0a6158eee1e4728fa3
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-22 10:07:53 +0300

    Update food_orders_app.py

--- Python OOP/Python OOP Retake Exam - 22 August 2022/project/food_orders_app.py
+++ Python OOP/Python OOP Retake Exam - 22 August 2022/project/food_orders_app.py
@@ -1,5 +1,8 @@
 from project.client import Client
 from project.meals.meal import Meal
+from project.meals.starter import Starter
+from project.meals.main_dish import MainDish
+from project.meals.dessert import Dessert
 
 class FoodOrdersApp:
     def __init__(self):
@@ -12,3 +15,19 @@ class FoodOrdersApp:
                 raise Exception("The client has already been registered!")
 
         self.clients.append(Client(client_phone_number))
+
+    def add_meals_to_menu(self, *meals: Meal):
+        acceptable_meals = {"Starter": Starter, "MainDish": MainDish, "Dessert": Dessert}
+        for meal in meals:
+            if meal.__class__.__name__ not in acceptable_meals:
+                continue
+            self.menu.append(meal)
+
+    def show_menu(self):
+        if len(self.menu) < 5:
+            raise Exception("The menu is not ready!")
+        result = []
+        for meal in self.menu:
+            result.append(meal.details())
+        return "\n".join(result)
+
