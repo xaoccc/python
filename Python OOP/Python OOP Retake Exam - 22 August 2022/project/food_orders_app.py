703231007330cbc0356b322d2198170cf0ef256e
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-22 09:57:56 +0300

    Update food_orders_app.py

--- Python OOP/Python OOP Retake Exam - 22 August 2022/project/food_orders_app.py
+++ Python OOP/Python OOP Retake Exam - 22 August 2022/project/food_orders_app.py
@@ -1,4 +1,14 @@
+from project.client import Client
+from project.meals.meal import Meal
+
 class FoodOrdersApp:
     def __init__(self):
         self.menu = []
         self.clients = []
+
+    def register_client(self, client_phone_number: str):
+        for client in self.clients:
+            if client.phone_number == client_phone_number:
+                raise Exception("The client has already been registered!")
+
+        self.clients.append(Client(client_phone_number))
