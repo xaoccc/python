a9aa617f07d261d46b86961a0294ef5905afb581
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-23 20:12:25 +0300

    Update horse.py

--- Python OOP/Python OOP Exam - 14 August 2022/project/horse_specification/horse.py
+++ Python OOP/Python OOP Exam - 14 August 2022/project/horse_specification/horse.py
@@ -1,4 +1,28 @@
 from abc import ABC, abstractmethod
 
 class Horse(ABC):
-    def __init(self):
+    def __init__(self, name: str, speed: int):
+        self.name = name
+        self.speed = speed
+        self.maximum_speed = 0
+        self.is_taken = False
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) < 4:
+            raise ValueError(f"Horse name {value} is less than 4 symbols!")
+        self.__name = value
+
+    @property
+    def speed(self):
+        return self.__speed
+
+    @speed.setter
+    def speed(self, value):
+        if value > self.maximum_speed:
+            raise ValueError("Horse speed is too high!")
+        self.__speed = value
