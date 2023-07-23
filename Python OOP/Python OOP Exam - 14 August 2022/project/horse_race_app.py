cfc2e02e74b91b620f16b1f9b7179aec3ab734b9
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-23 21:40:16 +0300

    Update horse_race_app.py

--- Python OOP/Python OOP Exam - 14 August 2022/project/horse_race_app.py
+++ Python OOP/Python OOP Exam - 14 August 2022/project/horse_race_app.py
@@ -19,3 +19,17 @@ class HorseRaceApp:
         if horse_type in self.horse_breeds:
             self.horses.append(self.horse_breeds[horse_type](horse_name, horse_speed))
             return f"{horse_type} horse {horse_name} is added."
+
+    def add_jockey(self, jockey_name: str, age: int):
+        for jockey in self.jockeys:
+            if jockey.name == jockey_name:
+                raise Exception(f"Jockey {jockey_name} has been already added!")
+        self.jockeys.append(Jockey(jockey_name, age))
+        return f"Jockey {jockey_name} is added."
+
+    def create_horse_race(self, race_type: str):
+        for race in self.horse_races:
+            if race.type == race_type:
+                raise Exception(f"Race {race_type} has been already created!")
+        self.horse_races.append(HorseRace(race_type))
+        return f"Race {race_type} is created."
