2879bb907db3a1874a36c867db96c479783c2dd2
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-23 20:32:13 +0300

    Update horse_race.py

---
+++ Python OOP/Python OOP Exam - 14 August 2022/project/horse_race.py
@@ -0,0 +1,16 @@
+class HorseRace:
+    valid_race_types = ["Winter", "Spring", "Autumn", "Summer"]
+    def __init__(self, race_type: str):
+        self.race_type = race_type
+        self.jockeys = []
+
+    @property
+    def race_type(self):
+        return self.__race_type
+
+    @race_type.setter
+    def race_type(self, value):
+        if value not in self.valid_race_types:
+            raise ValueError("Race type does not exist!")
+        self.__race_type = value
+
