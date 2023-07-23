aeee058ed9e0b341867a248275cfabed77ad184c
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-23 20:34:20 +0300

    Update horse_race_app.py

---
+++ Python OOP/Python OOP Exam - 14 August 2022/project/horse_race_app.py
@@ -0,0 +1,10 @@
+from project.jockey import Jockey
+from project.horse_specification.horse import Horse
+from project.horse_specification.appaloosa import Appaloosa
+from project.horse_specification.thoroughbred import Thoroughbred
+from project.horse_race import HorseRace
+
+
+class HorseRaceApp:
+    def __init__(self):
+        self.
