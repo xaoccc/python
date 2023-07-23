70388487c68c52bf66966956432a043ca851d624
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-23 20:35:39 +0300

    Update horse_race_app.py

--- Python OOP/Python OOP Exam - 14 August 2022/project/horse_race_app.py
+++ Python OOP/Python OOP Exam - 14 August 2022/project/horse_race_app.py
@@ -7,4 +7,9 @@ from project.horse_race import HorseRace
 
 class HorseRaceApp:
     def __init__(self):
-        self.
+        self.horses = []
+        self.jockeys = []
+        self.horse_races = []
+
+    def add_horse(self, horse_type: str, horse_name: str, horse_speed: int):
+        
