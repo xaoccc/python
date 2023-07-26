81367d4d93eabc5cb9bfaf935bd6ef25280b1d0f
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-26 22:38:36 +0300

    Update astronaut.py

---
+++ Python OOP/Exams/Python OOP Retake Exam - 23 August 2021/project/astronaut/astronaut.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+class Astronaut(ABC):
+    def __init__(self, name, oxygen):
+        self.name = name
+        self.oxygen = oxygen
+        self.backpack = []
+        
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, value):
+        if value.strip() == "":
+            raise ValueError("Astronaut name cannot be empty string or whitespace!")
+        self.__name = value
+
+    @abstractmethod
+    def breathe(self):
+        pass
