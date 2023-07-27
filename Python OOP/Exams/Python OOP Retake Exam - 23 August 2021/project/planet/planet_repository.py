daa157b229a4db1a657282b48914f08628411b9f
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-07-27 14:29:38 +0300

    Update planet_repository.py

--- Python OOP/Exams/Python OOP Retake Exam - 23 August 2021/project/planet/planet_repository.py
+++ Python OOP/Exams/Python OOP Retake Exam - 23 August 2021/project/planet/planet_repository.py
@@ -14,4 +14,4 @@ class PlanetRepository:
     def find_by_name(self, name: str):
         for planet in self.planets:
             if planet.name == name:
-                return planet.name
+                return planet
