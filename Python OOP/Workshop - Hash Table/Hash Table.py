437517c834b33a131ba3f636407a5e75d0a0ae5b
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-28 23:42:41 +0300

    Update Hash Table.py

--- Python OOP/Workshop - Hash Table/Hash Table.py
+++ Python OOP/Workshop - Hash Table/Hash Table.py
@@ -14,9 +14,13 @@ class HashTable:
                 return current_index % self.__size
             current_index += 1
 
+    def add(self, key, value):
+        self[key] = value
+
 
     def __len__(self):
         return self.__length
+
     def __setitem__(self, key, value):
         if len(self) == self.__size:
             self.__resize()
@@ -25,6 +29,16 @@ class HashTable:
         self.__keys[index] = key
         self.__values[index] = value
         self.__length += 1
+
+    def __getitem__(self, key):
+        try:
+            index = self.__keys.index(key)
+            return self.__values[index]
+        except ValueError:
+            raise KeyError("No such key in hash table!")
+
+    def get(self, key):
+        return self.__getitem__(key)
 
     def __resize(self):
         self.__keys +=  [None] * self.__size
@@ -52,6 +66,9 @@ table["has_cat"] = False
 print(table._HashTable__keys)
 print(table._HashTable__values)
 table["has_bear"] = False
+table.add("car","Golf")
 
 print(table)
-print(table["age"])
+print(table.get("age"))
+print(table["name"])
+
