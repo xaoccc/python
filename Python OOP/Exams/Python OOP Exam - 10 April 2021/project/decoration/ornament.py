51a897927363839c8bd54622019bc8a3cd325dda
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-07-29 13:37:16 +0300

    Update ornament.py

--- Python OOP/Exams/Python OOP Exam - 10 April 2021/project/decoration/ornament.py
+++ Python OOP/Exams/Python OOP Exam - 10 April 2021/project/decoration/ornament.py
@@ -1,1 +1,6 @@
 from project.decoration.base_decoration import BaseDecoration
+
+
+class Ornament(BaseDecoration):
+    def __init__(self, comfort=1, price=5):
+        super().__init__(comfort, price)
