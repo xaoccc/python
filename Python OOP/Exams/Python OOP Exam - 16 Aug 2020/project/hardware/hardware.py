7b72abd93cc1b20a02f157b093a93237baa6a4d9
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-03 10:55:25 +0300

    Update hardware.py

---
+++ Python OOP/Exams/Python OOP Exam - 16 Aug 2020/project/hardware/hardware.py
@@ -0,0 +1,22 @@
+from project.software.software import Software
+from project.software.light_software import LightSoftware
+from project.software.express_software import ExpressSoftware
+
+class Hardware:
+    def __init__(self, name, hardware_type, capacity, memory):
+        self.name = name
+        self.hardware_type = hardware_type
+        self.capacity = capacity
+        self.memory = memory
+        self.software_components = []
+
+    def install(self, software):
+        if self.capacity < software.capacity_consumption or self.memory < software.memory_consumption:
+            raise Exception("Software cannot be installed")
+        self.software_components.append(software)
+
+    def uninstall(self, software):
+        if software in self.software_components:
+            self.software_components.remove(software)
+
+
