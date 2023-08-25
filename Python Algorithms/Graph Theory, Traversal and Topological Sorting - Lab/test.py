0d4b3c6e7823ff8e116fe18f696c024f211d9e49
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-08-25 11:01:21 +0300

    ASa

---
+++ Python Algorithms/Graph Theory, Traversal and Topological Sorting - Lab/test.py
@@ -0,0 +1,27 @@
+#This code does 2 things: finds all nodes and finds all nodes components (connected nodes) if connected
+#It works only with ordered graphs
+
+nodes = int(input())
+graph = {}
+visited = set()
+
+for n in range(nodes):
+    graph[n] = [int(i) for i in input().split()]
+
+def dfs(node, graph, visited, current_graph):
+    if node in visited:
+        return
+
+    visited.add(node)
+    dfs(node, graph, visited, current_graph)
+
+    for child in graph[node]:
+        dfs(child, graph, visited, current_graph)
+    current_graph.append(node)
+    # print(node)
+
+for node in graph.keys():
+    current_graph = []
+    dfs(node, graph, visited, current_graph)
+    if current_graph:
+        print(current_graph)
