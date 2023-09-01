9253368f3dd158849364e9596049272809b305cc
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-09-01 09:45:38 +0300

    Update calculator.py

--- Projects/Simple_Console_Calculator/calculator.py
+++ Projects/Simple_Console_Calculator/calculator.py
@@ -1,14 +1,36 @@
 import math
 
-num1_input = float(input("Enter a number:"))
+num1_input = input("Enter a number:")
+while True:
+    try:
+        float(num1_input)
+        break
+    except:
+        print("Invalid input! You must enter a number!")
+        num1_input = input("Enter a number:")
+
+num1_input = float(num1_input)
 result = num1_input
+
 while True:
 
     command_input = input("Enter a command:")
+    while command_input not in ["=", "+", "-", "*", "/", "^", "m", "log", "f"]:
+        print("Invalid input!")
+        command_input = input("Enter a command:")
     if command_input == "=":
         break
-    
-    num2_input = float(input("Enter a number:"))
+
+    num2_input = input("Enter a number:")
+    while True:
+        try:
+            float(num2_input)
+            break
+        except:
+            print("Invalid input! You must enter a number!")
+            num2_input = input("Enter a number:")
+
+    num2_input = float(num2_input)
 
     if command_input == "+":
         result += num2_input
