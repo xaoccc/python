942d77697b020cf226846b6faf5597a2338687d0
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-09-02 13:31:32 +0300

    Update app.py

---
+++ Projects/Babystitters/app.py
@@ -0,0 +1,24 @@
+class BabysitterApp:
+    def __init__(self):
+        parents = []
+        children = []
+        neighbourhoods = []
+
+    def add_parent(self):
+        pass
+
+    def add_child(self):
+        pass
+
+
+    def add_neighbourhood(self):
+        pass
+
+
+    def add_child_to_parent(self):
+        #check if parent and child are in the same neghbourhood, if not - error!
+        pass
+
+
+    def search_for_babysitter(self):
+        pass
