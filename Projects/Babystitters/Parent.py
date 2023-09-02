57431aa606df0ae641e3801d8da9e26fabcbf2b1
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-09-02 13:13:07 +0300

    Update Parent.py

--- Projects/Babystitters/Parent.py
+++ Projects/Babystitters/Parent.py
@@ -23,7 +23,7 @@ class Parent(ABC):
 
     @property
     def last_name(self):
-        return self.__first_name
+        return self.__last_name
 
     @last_name.setter
     def last_name(self, value):
@@ -32,10 +32,55 @@ class Parent(ABC):
         self.__last_name = value
 
 
+    @property
+    def id_number(self):
+        return self.__id_number
+
+    @id_number.setter
+    def id_number(self, value):
+        if isinstance(value, (float, int)) or not value.isdigit() or len(value) != 10:
+            raise ValueError("Invalid ID! ID number cannot contain letters and must consist of exactly 10 digits!")
+        self.__id_number = value
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Invalid age! Age must be an integer!")
+        if not 17 < value < 91:
+            raise ValueError("Invalid age! Age cannot be less than 18 or more than 90!")
+        self.__age = value
+
+    @property
+    def gender(self):
+        return self.__gender
+
+    @gender.setter
+    def gender(self, value):
+        if value not in ["male" , "female"]:
+            raise ValueError("We are not that modern. Please choose between 'male' and 'female'.")
+        self.__gender = value
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Invalid rating! Rating must be an integer!")
+        if not -10000 <= value <= 10000:
+            raise ValueError("Invalid rating! Rating cannot be less than -10000 or more than 10000!")
+        self.__rating = value
 
 
 
 
-test_parent = Parent("Ivan", "Ivanov", "1122334455", 34, "male", "Obelya", 100)
 
-print(test_parent.first_name)
+
+test_parent = Parent("Ivan", "Ivanov", "1122334455", 34, "fmale", "Obelya", 100)
+
+print(test_parent.gender)
