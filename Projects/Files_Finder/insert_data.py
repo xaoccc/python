4f5fa7502bf553ee3ca42f19893d572cc322f778
Georgi Tashev <114498517+xaoccc@users.noreply.github.com>
2023-10-02 14:24:24 +0300

    Create insert_data.py

---
+++ Projects/Files_Finder/insert_data.py
@@ -0,0 +1,31 @@
+import pathlib
+import psycopg2
+
+con = psycopg2.connect(
+    host='localhost',
+    database='files_finder',
+    user='postgres',
+    password=''
+)
+
+cur = con.cursor()
+
+data = cur.fetchall()
+
+# Get all files and folder from specific path. Don't forget to change directory separator from \ to / !
+files = pathlib.Path('C:/Users/user/Documents')
+
+# Recursively get all files from directory inside files and filter it by extension (in this case mp3).
+#  If you want all files use '*' as filter parameter inside rglob.
+filtered_data = list(files.rglob('*.xlsx'))
+
+# loop through the data
+for f in filtered_data:
+
+    file_name = f.parts[-1][:-5]
+    file_path = '\\'.join(f.parts[:-1])
+    query = '''INSERT INTO music(file_name, file_path)
+            VALUES
+                (%s, %s, %s)
+            '''
+    cur.execute(query, (file_name, file_path))
