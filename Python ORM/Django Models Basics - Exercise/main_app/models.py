c243f841a552c6b638d33a129c81eda283345866
xaoccc <xaocccc@gmail.com>
2023-10-17 13:49:01 +0300

    Product

--- Python ORM/Django Models Basics - Exercise/main_app/models.py
+++ Python ORM/Django Models Basics - Exercise/main_app/models.py
@@ -13,5 +13,24 @@ class Blog(models.Model):
 
 class WeatherForecast(models.Model):
     date = models.DateField()
-    temperature =
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    precipitation = models.FloatField()
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    ingredients = models.TextField()
+    cook_time = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now=True)
+
+
 
