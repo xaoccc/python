4161ae1d67f2d44ebd4a58b3b4ede377769ec92f
xaoccc <xaocccc@gmail.com>
2023-10-17 14:15:52 +0300

    Profile

--- Python ORM/Django Models Basics - Exercise/main_app/models.py
+++ Python ORM/Django Models Basics - Exercise/main_app/models.py
@@ -33,4 +33,17 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now=True)
 
 
+class UserProfile(models.Model):
+    username = models.CharField(max_length=65, unique=True)
+    first_name = models.CharField(max_length=40, unique=True)
+    last_name = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique=True, default="students@softuni.bg")
+    bio = models.TextField(max_length=120)
+    profile_image_url = models.URLField()
+    created_at = models.DateTimeField(auto_now=True)
+
+
+
+
+
 
