32f668414db6e2279b38180f959731b0a5fabee9
xaoccc <xaocccc@gmail.com>
2023-10-19 14:17:36 +0300

    Customize the Admin

--- Python ORM/Migrations and Django Admin - Lab/main_app/admin.py
+++ Python ORM/Migrations and Django Admin - Lab/main_app/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", "created_on")
+    search_fields = ("name", "category", "supplier")
+    list_filter = ("category", "supplier")
+    fieldsets = (
+        ("General Information", {"fields": ("name", "description", "price", "barcode")}),
+        ("Categorization", {"fields": ("category", "supplier")})
+    )
+    date_hierarchy = "created_on"
 
