29adb0dc217c646730655aea4499816e48a77b75
xaoccc <xaocccc@gmail.com>
2023-10-20 10:02:57 +0300

    migrations 0001, 0002, 0003

--- Python ORM/Migrations and Django Admin - Exercise/main_app/models.py
+++ Python ORM/Migrations and Django Admin - Exercise/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Shoe(models.Model):
+    brand = models.CharField(max_length=25)
+    size = models.PositiveIntegerField()
+
+
+class UniqueBrands(models.Model):
+    brand_name = models.CharField(max_length=25, unique=True)
