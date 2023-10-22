388ec5e589901918e0a844ee6751b95fba771cff
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-10-22 10:31:28 +0300

    06. course

--- Python ORM/Migrations and Django Admin - Exercise/main_app/admin.py
+++ Python ORM/Migrations and Django Admin - Exercise/main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import EventRegistration, Movie, Student, Supplier
+from .models import EventRegistration, Movie, Student, Supplier, Course
 @admin.register(EventRegistration)
 class EventRegistrationAdmin(admin.ModelAdmin):
     list_display = ["event_name", "participant_name", "registration_date" ]
@@ -32,3 +32,14 @@ class SupplierAdmin(admin.ModelAdmin):
     fieldsets = [
         ["Information", {"fields": ["name", "contact_person", "email", "address"]}]
     ]
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title", "lecturer", "price", "start_date" ]
+    list_filter = ["is_published", "lecturer"]
+    search_fields = ["title", "lecturer"]
+    fieldsets = [
+        ["Course Information", {"fields": ["title", "lecturer", "price", "start_date", "is_published"]}],
+        ["Description" , {"fields": ["description"]}]
+    ]
+    readonly_fields = ["start_date"]
--- Python ORM/Migrations and Django Admin - Exercise/main_app/models.py
+++ Python ORM/Migrations and Django Admin - Exercise/main_app/models.py
@@ -48,4 +48,15 @@ class Supplier(models.Model):
     def __str__(self):
         return f"{self.name} - {self.phone}"
 
+class Course(models.Model):
+    title = models.CharField(max_length=90)
+    lecturer = models.CharField(max_length=90)
+    description = models.TextField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.lecturer}"
+
 
