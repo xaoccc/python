ee5a3f4f72a4cdebf809d654bf9889a2dc42ac88
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-10-24 18:51:16 +0300

    Update settings.py

--- Python ORM/Data Operations in Django with Queries - Exercise/orm_skeleton/settings.py
+++ Python ORM/Data Operations in Django with Queries - Exercise/orm_skeleton/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Django_Data_Ops",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
