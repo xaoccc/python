25eb02b3510eb4b1e328bb8b7e1c85c782be4cd2
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-10-24 22:22:39 +0300

    02. Artifact

--- Python ORM/Data Operations in Django with Queries - Exercise/caller.py
+++ Python ORM/Data Operations in Django with Queries - Exercise/caller.py
@@ -6,7 +6,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Pet
+from main_app.models import Pet, Artifact
 
 # Create queries within functions
 
@@ -19,7 +19,27 @@ def create_pet(name, species):
     return f"{name} is a very cute {species}!"
 
 
+# test_code
+# print(create_pet('Buddy', 'Dog'))
+# print(create_pet('Whiskers', 'Cat'))
+# print(create_pet('Rocky', 'Hamster'))
 
-print(create_pet('Buddy', 'Dog'))
-print(create_pet('Whiskers', 'Cat'))
-print(create_pet('Rocky', 'Hamster'))
+
+def create_artifact(name: str, origin: str, age: int, description: str, is_magical: bool):
+    artifact = Artifact (
+        name = name,
+        origin = origin,
+        age = age,
+        description = description,
+        is_magical = is_magical
+    )
+    artifact.save()
+    return f"The artifact {name} is {age} years old!"
+
+def delete_all_artifacts():
+    Artifact.objects.all().delete()
+
+# test code
+# print(create_artifact('Ancient Sword', 'Lost Kingdom', 500, 'A legendary sword with a rich history', True))
+# print(create_artifact('Crystal Amulet', 'Mystic Forest', 300, 'A magical amulet believed to bring good fortune', True))
+# delete_all_artifacts()
--- Python ORM/Data Operations in Django with Queries - Exercise/main_app/models.py
+++ Python ORM/Data Operations in Django with Queries - Exercise/main_app/models.py
@@ -4,3 +4,11 @@ class Pet(models.Model):
 
     name = models.CharField(max_length=40)
     species = models.CharField(max_length=40)
+
+class Artifact(models.Model):
+    name = models.CharField(max_length=70)
+    origin = models.CharField(max_length=70)
+    age = models.PositiveIntegerField()
+    description = models.TextField()
+    is_magical = models.BooleanField(default=False)
+
