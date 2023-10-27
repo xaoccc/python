08486648cb680d34c354fde7e3c64be9605714b4
xaoccc <xaocccc@gmail.com>
2023-10-27 13:56:00 +0300

    Laptops

--- Python ORM/Working with Queries in Django - Exercise/main_app/models.py
+++ Python ORM/Working with Queries in Django - Exercise/main_app/models.py
@@ -70,3 +70,26 @@ class ArtworkGallery(models.Model):
 
     def __str__(self):
         return f"{self.art_name} is the highest rated art with {self.rating} rating!"
+
+
+class Laptop(models.Model):
+    LAPTOP_BRANDS = (
+        ("Asus", "Asus"),
+        ("Acer", "Acer"),
+        ("Apple", "Apple"),
+        ("Lenovo", "Lenovo"),
+        ("Dell", "Dell")
+    )
+    OPERATING_SYSTEMS = (
+        ("Windows", "Windows"),
+        ("MacOS", "MacOS"),
+        ("Linux", "Linux"),
+        ("Chrome OS", "Chrome OS")
+    )
+
+    brand = models.CharField(max_length=10, choices=LAPTOP_BRANDS)
+    processor = models.CharField(max_length=100)
+    memory = models.PositiveIntegerField(help_text="Memory in GB")
+    storage = models.PositiveIntegerField(help_text="Storage in GB")
+    operation_system = models.CharField(max_length=20, choices=OPERATING_SYSTEMS)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
