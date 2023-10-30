1c9132eb1fbedee5ad11d6a5a9e8d35b23b8d0e7
xaoccc <xaocccc@gmail.com>
2023-10-30 10:35:34 +0200

    01. The Lecturer

--- Python ORM/Django Models Relations - Lab/caller.py
+++ Python ORM/Django Models Relations - Lab/caller.py
@@ -8,3 +8,24 @@ django.setup()
 # Import your models
 # Create and check models
 # Run and print your queries
+from main_app.models import Lecturer, Subject
+
+# 01. The Lecturer
+# Test code
+# lecturer1 = Lecturer.objects.create(first_name="John", last_name="Doe")
+# lecturer2 = Lecturer.objects.create(first_name="Jane", last_name="Smith")
+# Subject.objects.create(name="Mathematics", code="MATH101", lecturer=lecturer1)
+#
+# Subject.objects.create(name="History", code="HIST101", lecturer=lecturer2)
+# Subject.objects.create(name="Physics", code="PHYS101", lecturer=lecturer1)
+# math_subject = Subject.objects.get(name="Mathematics")
+# math_lecturer = math_subject.lecturer
+# print(f"The lecturer for Mathematics is {math_lecturer}.")
+#
+# history_subject = Subject.objects.get(name="History")
+# history_lecturer = history_subject.lecturer
+# print(f"The lecturer for History is {history_lecturer}.")
+#
+# physics_subject = Subject.objects.get(name="Physics")
+# physics_lecturer = physics_subject.lecturer
+# print(f"The lecturer for Physics is {physics_lecturer}.")
--- Python ORM/Django Models Relations - Lab/main_app/models.py
+++ Python ORM/Django Models Relations - Lab/main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here
+
+class Lecturer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+    lecturer = models.ForeignKey(
+        Lecturer,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
