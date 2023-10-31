deb1ebc617661d826444491ce516ca93702c5e76
xaoccc <xaocccc@gmail.com>
2023-10-31 16:47:29 +0200

    2.Music App

--- Python ORM/Django Models Relations - Exercise/caller.py
+++ Python ORM/Django Models Relations - Exercise/caller.py
@@ -1,11 +1,12 @@
 import os
 import django
+from django.core.exceptions import ObjectDoesNotExist
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Author, Book
+from main_app.models import Author, Book, Artist, Song
 
 # Import your models here
 
@@ -26,3 +27,39 @@ def delete_all_authors_without_books():
     for author in authors:
         if not Book.objects.filter(author=author).values_list("title", flat=True):
             author.delete()
+
+
+def add_song_to_artist(artist_name: str, song_title: str):
+    # Because get() raises and exception if number of returned objects are not exactly 1, we might want to catch it:
+    try:
+        artist = Artist.objects.get(name=artist_name)
+    except ObjectDoesNotExist:
+        print(f"Artist '{artist_name}' does not exist.")
+        return
+
+    try:
+        song, created = Song.objects.get_or_create(title=song_title)
+        artist.songs.add(song)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def get_songs_by_artist(artist_name: str):
+    return Artist.objects.filter(name=artist_name).value("songs")
+
+def remove_song_from_artist(artist_name: str, song_title: str):
+    try:
+        artist = Artist.objects.get(name=artist_name)
+    except ObjectDoesNotExist:
+        print(f"Artist '{artist_name}' does not exist.")
+
+    try:
+        song = Song.objects.get(title=song_title)
+    except ObjectDoesNotExist:
+        print(f"Song '{song_title}' does not exist.")
+
+    try:
+        artist.songs.remove(song)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
