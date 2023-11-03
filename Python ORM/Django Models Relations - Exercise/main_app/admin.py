eda886fbf1427580ebd18dafcc0fff4e4b30e92b
xaoccc <xaocccc@gmail.com>
2023-11-03 11:43:31 +0200

    06. CarAdmin

--- Python ORM/Django Models Relations - Exercise/main_app/admin.py
+++ Python ORM/Django Models Relations - Exercise/main_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Car
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ["model", "year", "owner", "car_details"]
+
+    @staticmethod
+    def car_details(obj: object):
+        try:
+            owner_name = obj.owner.name
+        except AttributeError:
+            owner_name = "No owner"
+
+        try:
+            registration_number = obj.registration.registration_number
+        except AttributeError:
+            registration_number = "No registration number"
+
+        return f"Owner: {owner_name}, Registration: {registration_number}"
+
+    car_details.short_description = "Car Details"
