07221eb5312dea1bc0ce085602263d188866c19b
xaoccc <xaocccc@gmail.com>
2023-11-03 13:01:16 +0200

    01. Zoo Animals

---
+++ Python ORM/Models Inheritance and Customization - Lab/main_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    sound = models.CharField(max_length=100)
+
+class Mammal(Animal):
+    fur_color = models.CharField(max_length=50)
+
+
+class Bird(Animal):
+    wing_span = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Reptile(Animal):
+    scale_type = models.CharField(max_length=50)
