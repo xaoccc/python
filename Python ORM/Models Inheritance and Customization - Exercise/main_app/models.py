c72b9ce3a2e3771fc94bcc3418e10df030cab4f6
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-11-06 21:51:28 +0200

    Update models.py

--- Python ORM/Models Inheritance and Customization - Exercise/main_app/models.py
+++ Python ORM/Models Inheritance and Customization - Exercise/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# ModelName (SomeOtherModel):
+# no Meta - create 1 to 1 relation between ModelName and SomeOtherModel
+
+# ModelName (models.Model):
+# class Meta:
+# abstract = True - each model which inherits ModelName will have its fields, but no table ModelName will be created
+# proxy = True - table ModelName will be created, models which inherit ModelName cannot add fields, but only methods
