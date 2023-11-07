ea49cd70fb672985d3c9237bf41dc2c77b0068db
xaoccc <xaocccc@gmail.com>
2023-11-07 17:39:33 +0200

    5. Hotel Rooms

--- Python ORM/Models Inheritance and Customization - Exercise/caller.py
+++ Python ORM/Models Inheritance and Customization - Exercise/caller.py
@@ -6,11 +6,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import CreditCard
+# from main_app.models import CreditCard
 
-credit_card1 = CreditCard.objects.create(card_owner="Krasimir", card_number="1234567890123450")
-
-credit_card1.save()
 
 
 
--- Python ORM/Models Inheritance and Customization - Exercise/main_app/models.py
+++ Python ORM/Models Inheritance and Customization - Exercise/main_app/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.core.exceptions import ValidationError
 from django.db import models
 
@@ -121,6 +123,62 @@ class CreditCard(models.Model):
     card_owner = models.CharField(max_length=100)
     card_number = MaskedCreditCardField(max_length=20)
     # max_length could be overridden and not given here
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+
+
+class Room(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    number = models.CharField(max_length=100, unique=True)
+    capacity = models.PositiveIntegerField()
+    total_guests = models.PositiveIntegerField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+
+    # validate
+
+    def clean(self):
+        if self.total_guests > self.capacity:
+            raise ValidationError("Total guests are more than the capacity of the room")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+        return f"Room {self.number} created successfully"
+
+
+class BaseReservation(models.Model):
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    class Meta:
+        abstract = True
+
+    def reservation_period(self):
+        return (self.end_date - self.start_date).days
+
+    def calculate_total_cost(self):
+        total_cost = self.reservation_period() * self.room.price_per_night
+        return f"{total_cost:1f}"
+
+    @property
+    def is_available(self):
+        if self.start_date > self.end_date:
+            raise ValidationError("Start date cannot be after or in the same end date")
+
+
+
+
+class RegularReservation(BaseReservation):
+    pass
+
+
+class SpecialReservation(BaseReservation):
+    pass
+
 
 
 
