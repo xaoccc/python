e548e596496891e7c562c5db473d002b81fb043f
xaoccc <xaocccc@gmail.com>
2023-11-07 12:17:53 +0200

    2.Chat App

--- Python ORM/Models Inheritance and Customization - Exercise/main_app/models.py
+++ Python ORM/Models Inheritance and Customization - Exercise/main_app/models.py
@@ -60,4 +60,27 @@ class FelbladeDemonHunter(DemonHunter):
 
 
 class UserProfile(models.Model):
-    username = models.CharField(max_length=70)
+    username = models.CharField(max_length=70, unique=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True, blank=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(UserProfile, related_name="sent_messages", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(UserProfile, related_name="received_messages", on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def mark_as_read(self):
+        self.is_read = True
+
+    def mark_as_unread(self):
+        self.is_read = False
+
+    def reply_to_message(self, reply_content, receiver):
+        return Message(sender=self.receiver, receiver=receiver, content=reply_content)
+
+    def forward_message(self, sender, receiver):
+        return Message(sender=sender, receiver=receiver, content=self.content)
+
