1d23ea459823c8d6db05df31bfbbc43867a02cac
xaoccc <xaocccc@gmail.com>
2023-11-07 11:51:10 +0200

    1.Character Classes

--- Python ORM/Models Inheritance and Customization - Exercise/main_app/models.py
+++ Python ORM/Models Inheritance and Customization - Exercise/main_app/models.py
@@ -8,3 +8,56 @@ from django.db import models
 # class Meta:
 # abstract = True - each model which inherits ModelName will have its fields, but no table ModelName will be created
 # proxy = True - table ModelName will be created, models which inherit ModelName cannot add fields, but only methods
+
+class BaseCharacter(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Mage(BaseCharacter):
+    elemental_power = models.CharField(max_length=100)
+    spellbook_type = models.CharField(max_length=100)
+
+
+class Assassin(BaseCharacter):
+    weapon_type = models.CharField(max_length=100)
+    assassination_technique = models.CharField(max_length=100)
+
+
+class DemonHunter(BaseCharacter):
+    weapon_type = models.CharField(max_length=100)
+    demon_slaying_ability = models.CharField(max_length=100)
+
+
+class TimeMage(Mage):
+    time_magic_mastery = models.CharField(max_length=100)
+    temporal_shift_ability = models.CharField(max_length=100)
+
+
+class Necromancer(Mage):
+    raise_dead_ability = models.CharField(max_length=100)
+
+
+class ViperAssassin(Assassin):
+    venomous_strikes_mastery = models.CharField(max_length=100)
+    venomous_bite_ability = models.CharField(max_length=100)
+
+
+class ShadowbladeAssassin(Assassin):
+    shadowstep_ability = models.CharField(max_length=100)
+
+
+class VengeanceDemonHunter(DemonHunter):
+    vengeance_mastery = models.CharField(max_length=100)
+    retribution_ability = models.CharField(max_length=100)
+
+
+class FelbladeDemonHunter(DemonHunter):
+    felblade_ability = models.CharField(max_length=100)
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70)
