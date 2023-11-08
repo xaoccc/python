d8ddd35380e35138e692c4007ce66e43820ba529
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-11-08 21:21:50 +0200

    Media

--- Python ORM/Advanced Django Model Techniques - Exercise/main_app/models.py
+++ Python ORM/Advanced Django Model Techniques - Exercise/main_app/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MinLengthValidator
 from django.db import models
 from main_app.validators import validate_name, validate_phone
 
@@ -9,3 +9,48 @@ class Customer(models.Model):
     email = models.EmailField()
     phone_number = models.CharField(max_length=13, validators=[validate_phone])
     website_url = models.URLField()
+
+class BaseMedia(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    genre = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+        ordering=["-created_at", "title"]
+
+class Book(BaseMedia):
+    author = models.CharField(
+        max_length=100,
+        validators=[MinLengthValidator(5, "Author must be at least 5 characters long")]
+    )
+    isbn = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[MinLengthValidator(6, "ISBN must be at least 6 characters long")]
+    )
+
+    class Meta(BaseMedia.Meta):
+        verbose_name = "Model Book"
+        verbose_name_plural = "Models of type - Book"
+
+class Movie(BaseMedia):
+    director = models.CharField(
+        max_length=100,
+        validators=[MinLengthValidator(8, "Director must be at least 8 characters long")]
+    )
+
+    class Meta(BaseMedia.Meta):
+        verbose_name = "Model Movie"
+        verbose_name_plural = "Models of type - Movie"
+
+class Music(BaseMedia):
+    artist = models.CharField(
+        max_length=100,
+        validators=[MinLengthValidator(9, "Artist must be at least 9 characters long")]
+    )
+
+    class Meta(BaseMedia.Meta):
+        verbose_name = "Model Music"
+        verbose_name_plural = "Models of type - Music"
