9fdfc0309aa557417338af9c03e80e0d2c8d07a9
xaoccc <xaocccc@gmail.com>
2023-11-08 17:40:36 +0200

    05. Menu Review

--- Python ORM/Advanced Django Model Techniques - Lab/main_app/models.py
+++ Python ORM/Advanced Django Model Techniques - Lab/main_app/models.py
@@ -38,7 +38,7 @@ class RestaurantReview(models.Model):
     reviewer_name = models.CharField(max_length=100)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     review_content = models.TextField()
-    rating = models.PositiveIntegerField(validators=[MaxValueValidator(limit_value=5)])
+    rating = models.PositiveIntegerField(validators=[MaxValueValidator(5)])
 
     class Meta:
         abstract = True
@@ -59,6 +59,20 @@ class FoodCriticRestaurantReview(RestaurantReview):
         verbose_name = "Food Critic Review"
         verbose_name_plural = "Food Critic Reviews"
         unique_together = ["reviewer_name", "restaurant"]
+
+
+class MenuReview(models.Model):
+    reviewer_name = models.CharField(max_length=100)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    review_content = models.TextField()
+    rating = models.PositiveIntegerField(validators=[MaxValueValidator(5)])
+
+    class Meta:
+        ordering = ["-rating"]
+        verbose_name = "Menu Review"
+        verbose_name_plural = "Menu Reviews"
+        unique_together = ["reviewer_name", "menu"]
+        indexes = [models.Index(fields=["menu"], name="main_app_menu_review_menu_id")]
 
 
 
