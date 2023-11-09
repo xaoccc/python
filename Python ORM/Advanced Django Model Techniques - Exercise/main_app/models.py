420fd797e3ba7aaf5e9200bf5a5e491ee795787c
xaoccc <xaocccc@gmail.com>
2023-11-09 13:31:35 +0200

    Superhero Universe

--- Python ORM/Advanced Django Model Techniques - Exercise/main_app/models.py
+++ Python ORM/Advanced Django Model Techniques - Exercise/main_app/models.py
@@ -1,6 +1,7 @@
 from _decimal import Decimal
 from django.core.validators import MinValueValidator, MinLengthValidator
 from django.db import models
+from django.db.models import PositiveIntegerField
 from main_app.validators import validate_name, validate_phone
 
 
@@ -88,6 +89,44 @@ class DiscountedProduct(Product):
 
     def format_product_name(self):
         return f"Discounted Product: {self.name}"
+
+
+class RechargeEnergyMixin:
+    def recharge_energy(self, amount):
+        if self.energy + amount <= 100:
+            self.energy += amount
+        else:
+            self.energy = 100
+
+
+class Hero(models.Model, RechargeEnergyMixin):
+    name = models.CharField(max_length=100)
+    hero_title = models.CharField(max_length=100)
+    energy = models.PositiveIntegerField()
+
+
+class SpiderHero(Hero):
+    def swing_from_buildings(self):
+        self.energy -= 80
+        if self.energy <= 0:
+            return f"{self.name} as Spider Hero is out of web shooter fluid"
+        return f"{self.name} as Spider Hero swings from buildings using web shooters"
+
+    class Meta:
+        proxy = True
+
+class FlashHero(Hero):
+    def run_at_super_speed(self):
+        self.energy -= 65
+        if self.energy <= 0:
+            return f"{self.name} as Flash Hero needs to recharge the speed force"
+        return f"{self.name} as Flash Hero runs at lightning speed, saving the day"
+
+    class Meta:
+        proxy = True
+
+
+
 
 
 
