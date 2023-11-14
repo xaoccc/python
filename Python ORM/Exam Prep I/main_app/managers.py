a22546abdcc56cf36ee23d3add3a9a14c256fa67
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-11-14 21:24:58 +0200

    asd

--- Python ORM/Exam Prep I/main_app/managers.py
+++ Python ORM/Exam Prep I/main_app/managers.py
@@ -3,7 +3,4 @@ from django.db.models import Count
 
 class DirectorManager(models.Manager):
     def get_directors_by_movies_count(self):
-        result = []
-        for item in self.all().annotate(movies_num=Count("movie__director")).order_by("-movies_num", "full_name"):
-            result.append(item.full_name)
-        return result
+        return self.all().annotate(movies_num=Count("movie__director")).order_by("-movies_num", "full_name")
