fb992659d83033c8169d0b3fac313183bbe0641d
xaoccc <xaocccc@gmail.com>
2023-11-14 15:10:07 +0200

    skeleton setup

---
+++ Python ORM/Exam Prep I/caller.py
@@ -0,0 +1,9 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+# Create and run your queries within functions
