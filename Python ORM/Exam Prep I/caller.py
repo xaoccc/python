9c718e8ff0dd407b1a0039ee3c73dedc33e6e035
xaoccc <xaocccc@gmail.com>
2023-11-16 15:45:41 +0200

    prefetch_related detailed explanation

--- Python ORM/Exam Prep I/caller.py
+++ Python ORM/Exam Prep I/caller.py
@@ -71,11 +71,11 @@ def get_top_rated_awarded_movie():
     return f"Top rated awarded movie: {top_movie.title}, rating: {top_movie.rating:.1f}. Starring actor: {starring_actor}. Cast: {', '.join(cast)}."
 
 def increase_rating():
-    classic_movies = Movie.objects.filter(is_classic=True, rating__lt=9.9)
+    classic_movies = Movie.objects.filter(is_classic=True, rating__lte=9.9)
 
     if classic_movies.exists():
-        num_of_updated_movies = classic_movies.update(rating=F("rating") + 0.1)
-        return f"Rating increased for {num_of_updated_movies} movies."
+        classic_movies.update(rating=F("rating") + 0.1)
+        return f"Rating increased for {classic_movies.count()} movies."
 
     return "No ratings increased."
 
@@ -86,3 +86,54 @@ def increase_rating():
 # print(get_actors_by_movies_count())
 # print(get_top_rated_awarded_movie())
 # print(increase_rating())
+
+
+# access every RELATED object in the prefetch related table:
+all_movies = Movie.objects.all()
+
+# If we look at the first Movie object, we can see that, we can access directly the ForeignKey objects Actor and Director,
+# but we cannot access directly the ManyToMany objects Actors!
+first_movie_details = Movie.objects.first().__dict__
+print(first_movie_details)
+print(Movie.objects.first().starring_actor.full_name)
+
+all_movies_with_their_actors = Movie.objects.prefetch_related("actors")
+
+for movie in all_movies_with_their_actors:
+    # Here we can select all() actors,
+    current_movie_actors = movie.actors.all()
+    # or we can filter() them
+    # Using Filter we can:
+    # 1. See all movies an actor is participating, searcing by full name
+    current_movie_actors = movie.actors.filter(full_name="Stefan Danailov")
+
+    # or by id
+    current_movie_actors = movie.actors.filter(id=1)
+
+    # 2. See movies with youngest/oldest actors
+    current_movie_actors = movie.actors.filter(birth_date__gte="1970-01-01")
+
+    # 3. Search actors and movies by actor name or part of the name
+    current_movie_actors = movie.actors.filter(full_name__contains="test")
+
+    # and much, much more: order, update, delete, etc...
+    if current_movie_actors:
+        print(f"Movie {movie.title, movie.release_date} cast:")
+        # create, update, delete:
+        # current_movie_actors.update(full_name="test test")
+        # current_movie_actors.delete()
+        # current_movie_actors.create(id=1, full_name="Stefan Danailov", birth_date="1942-12-09", nationality="Bulgarian", is_awarded=True, last_updated="1998-05-05")
+        for actor in current_movie_actors:
+            print(actor.id, actor.full_name)
+
+
+# We can do the reverse search and find all movies from Actor model:
+all_actors_with_their_movies = Actor.objects.prefetch_related("movie_set")
+print(all_actors_with_their_movies)
+
+for actor in all_actors_with_their_movies:
+    current_actor_movies = actor.movie_set.all()
+    if current_actor_movies:
+        print(f"Actor {actor.full_name} is playing in the following movies:")
+        for movie in current_actor_movies:
+            print(f"{movie.title, movie.release_date}")
