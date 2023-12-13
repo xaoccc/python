31c858589ec4eaa7b51f7db9e441d51735bf02c9
xaoccc <xaocccc@gmail.com>
2023-12-13 12:03:10 +0200

    add json to xls

--- Projects/read_json_data.py
+++ Projects/read_json_data.py
@@ -1,20 +1,14 @@
 import json
 import openpyxl
-from datetime import datetime
 
-with open("CBT4A-outbound-response-new.json", "r") as file:
+with open("My_json_data.json", "r") as file:
     text = file.readlines()
 all_data = json.loads("".join(text))
 
-workbook = openpyxl.load_workbook('Ford all data.xlsx')
+workbook = openpyxl.load_workbook('My_file.xlsx')
 worksheet = workbook.active
 
-row = 0
-column = 0
+for row in all_data:
+    worksheet.append(tuple(row.values()))
 
-# row_one = tuple(all_data[0].keys())
-# worksheet.append(row_one)
-for vehicle_data in all_data:
-    worksheet.append(tuple(vehicle_data.values())[:-5])
-
-workbook.save('Ford all data.xlsx')
+workbook.save('My_file.xlsx')
