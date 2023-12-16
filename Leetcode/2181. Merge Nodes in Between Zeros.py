edefdbac6338ca861b8d4fd241e50ec908d86fd1
xaoccc <114498517+xaoccc@users.noreply.github.com>
2023-12-16 11:32:02 +0200

    leetcode

--- Leetcode/2181. Merge Nodes in Between Zeros.py
+++ Leetcode/2181. Merge Nodes in Between Zeros.py
@@ -6,17 +6,51 @@
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         result = []
+        nodes = []
         new_listnode = ListNode()
+        previous = False
+
+        # while head.next:
+
+        #     if head.val != 0:
+        #         if previous == True:
+        #             result[-1] += head.val
+        #         else:
+        #             result.append(head.val)
+
+        #         if head.next.val != 0:
+        #             result[-1] += head.next.val
+        #             head = head.next.next
+
+        #         else:
+        #             head = head.next
+        #         previous = True
+
+        #     else:
+        #         previous = False
+        #         head = head.next
 
         while head.next:
-            result.append(head.val)
-            head = head.next
 
-        for node in result:
-            if node == 0:
-                continue
-            else:
-                new_listnode.val = node
+            if head.val != 0:
+
+                if previous:
+                    new_listnode.next = head
+                else:
+                    new_listnode.val = head.val
+
+                if head.next.val != 0:
+                    new_listnode.val += head.next.val
+                    head = head.next.next
+
+                else:
+                    head = head.next
+                previous = True
                 new_listnode.next = ListNode()
+
+            else:
+
+                previous = False
+                head = head.next
 
         return new_listnode
