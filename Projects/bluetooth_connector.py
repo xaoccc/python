871a76dbcf185a4376e23ab22a12cbcc73b48c23
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-05-01 19:31:17 +0300

    ajsa

--- Projects/bluetooth_connector.py
+++ Projects/bluetooth_connector.py
@@ -1,26 +1,3 @@
-# import socket
-#
-# adapter_addr = 'ac:74:b1:bd:5d:2f'
-# port = 7  # Normal port for rfcomm?
-# buf_size = 1024
-#
-# s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-# s.bind((adapter_addr, port))
-# s.listen(1)
-# try:
-#     print('Listening for connection...')
-#     client, address = s.accept()
-#     print(f'Connected to {address}')
-#
-#     while True:
-#         data = client.recv(buf_size)
-#         if data:
-#             print(data)
-# except Exception as e:
-#     print(f'Something went wrong: {e}')
-#     client.close()
-#     s.close()
-
 # pip install pybluez2
 import bluetooth
 
