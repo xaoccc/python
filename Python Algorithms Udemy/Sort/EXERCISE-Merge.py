16cd0853488f7e8abb7b75153019dc15af03e0d3
xaoccc <xaocccc@gmail.com>
2024-12-06 15:51:05 +0200

    solved

---
+++ Python Algorithms Udemy/Sort/EXERCISE-Merge.py
@@ -0,0 +1,21 @@
+
+## WRITE MERGE FUNCTION HERE ##
+#                             #
+#                             #
+#                             #
+#                             #
+############################### 
+
+
+
+
+# MERGE REQUIRES TWO SORTED LISTS:
+print(merge([1,2,7,8], [3,4,5,6]))
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6, 7, 8]
+ """
---
+++ Python Algorithms Udemy/Sort/EXERCISE-Merge_Sort.py
@@ -0,0 +1,50 @@
+def merge(array1, array2):
+    combined = []
+    i = 0
+    j = 0
+    while i < len(array1) and j < len(array2):
+        if array1[i] < array2[j]:
+            combined.append(array1[i])
+            i += 1
+        else:
+            combined.append(array2[j])
+            j += 1
+              
+    while i < len(array1):
+        combined.append(array1[i])
+        i += 1
+
+    while j < len(array2):
+        combined.append(array2[j])
+        j += 1
+
+    return combined
+
+
+## WRITE MERGE_SORT FUNCTION HERE ##
+#                                  #
+#                                  #
+#                                  #
+#                                  #
+####################################
+
+
+
+original_list = [3,1,4,2]
+
+sorted_list = merge_sort(original_list)
+
+print('Original List:', original_list)
+
+print('\nSorted List:', sorted_list)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Original List: [3, 1, 4, 2]
+
+    Sorted List: [1, 2, 3, 4]
+
+ """
