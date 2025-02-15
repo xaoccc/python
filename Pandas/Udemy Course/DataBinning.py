6bfc4535f8ccec0640c4349af8544634187cb683
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-02-15 11:31:07 +0200

    pandas data binning

--- Pandas/Udemy Course/DataBinning.py
+++ Pandas/Udemy Course/DataBinning.py
@@ -23,7 +23,7 @@ geyser['qcut_dur_3'] = pd.qcut(geyser.duration, q=3, labels=['short', 'medium', 
 print(geyser)
 
 
-fig, ((h_dur, qc_dur2, qc_dur3), (h_dur2, cut2, cut3)) = plt.subplots(2, 3)
+fig, ((h_dur, qc_dur2, qc_dur3), (h_dur2, cut2, cut3), (h_dur3, sel_cut2, sel_cut3)) = plt.subplots(3, 3)
 sns.histplot(data=geyser, x='duration', bins=25, ax=h_dur, hue='kind', palette="coolwarm", edgecolor='white')
 sns.histplot(data=geyser, x='duration', bins=25, ax=qc_dur2, hue='qcut_dur_2', palette="coolwarm", edgecolor='white')
 sns.histplot(data=geyser, x='duration', bins=25, ax=qc_dur3, hue='qcut_dur_3', palette="coolwarm", edgecolor='white')
@@ -34,4 +34,14 @@ geyser['cut_dur_3'] = pd.cut(geyser.duration, bins=3, labels=['short', 'medium',
 sns.histplot(data=geyser, x='duration', bins=25, ax=h_dur2, hue='kind', palette="coolwarm", edgecolor='white')
 sns.histplot(data=geyser, x='duration', bins=25, ax=cut2, hue='cut_dur_2', palette="coolwarm", edgecolor='white')
 sns.histplot(data=geyser, x='duration', bins=25, ax=cut3, hue='cut_dur_3', palette="coolwarm", edgecolor='white')
+
+
+# Define custom binnings range
+geyser['sel_cut_dur_2'] = pd.cut(geyser.duration, bins=[0, 2, 100], labels=['short', 'long'])
+geyser['sel_cut_dur_3'] = pd.cut(geyser.duration, bins=[0, 2, 4, 100], labels=['short', 'medium', 'long'])
+
+sns.histplot(data=geyser, x='duration', bins=25, ax=h_dur3, hue='kind', palette="coolwarm", edgecolor='white')
+sns.histplot(data=geyser, x='duration', bins=25, ax=sel_cut2, hue='sel_cut_dur_2', palette="coolwarm", edgecolor='white')
+sns.histplot(data=geyser, x='duration', bins=25, ax=sel_cut3, hue='sel_cut_dur_3', palette="coolwarm", edgecolor='white')
+
 plt.show()
