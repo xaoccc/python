c966073987710cc59799e764d496086f8a0a734c
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-02-20 18:06:52 +0200

    custom content manager mimics an event loop

--- Python OOP/CustomContentManager.py
+++ Python OOP/CustomContentManager.py
@@ -1,17 +1,11 @@
 class MyContext:
     def __enter__(self):
-        for num in range(3):
-            print(num)
-        print("Entering the block")
+        print("Start event loop")
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        for num in range(7, 10):
-            print(num)
-        print("Exiting the block")
+        print("End event loop")
 
 
 with MyContext() as ctx:
-    for num in range(4, 7):
-        print(num)
-    print("Inside the block")
+    print("Processing message")
