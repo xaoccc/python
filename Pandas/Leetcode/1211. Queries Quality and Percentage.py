5e8d8423cf82b0cc5c786913ebf2d38294cfa5bf
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-03-02 10:39:33 +0200

    fixed pandas solution

--- Pandas/Leetcode/1211. Queries Quality and Percentage.py
+++ Pandas/Leetcode/1211. Queries Quality and Percentage.py
@@ -1,19 +1,32 @@
 import pandas as pd
 
 
+# def queries_stats(queries: pd.DataFrame) -> pd.DataFrame:
+#     queries.drop(columns=['result'], inplace=True)
+#     queries['quality'] = (queries['rating'] / queries['position'])
+#
+#     counts_total = queries['query_name'].value_counts()
+#
+#     low_ratings = queries.loc[queries['rating'] < 3, :]
+#     counts_low = low_ratings['query_name'].value_counts()
+#
+#     queries['poor_query_percentage'] = queries['query_name'].map((counts_low / counts_total) * 100).round(2)
+#
+#     queries = queries.groupby(by='query_name', as_index=False).mean()
+#     queries['quality'] = queries['quality'].round(2)
+#     queries['poor_query_percentage'].fillna(0, inplace=True)
+#
+#     return queries.drop(columns=['position', 'rating'])
+
+import pandas as pd
+# Function to reduce bias when used round(2)
+round2 = lambda x: round(x + 1e-9, 2)
+
 def queries_stats(queries: pd.DataFrame) -> pd.DataFrame:
-    queries.drop(columns=['result'], inplace=True)
-    queries['quality'] = (queries['rating'] / queries['position'])
+    queries['quality'] = queries.rating/queries.position
+    # queries.rating < 3 alone returns a bool, but in the math context (queries.rating < 3)*100 the bool is cast
+    # to int so values in the column will be either 100 or 0.
+    queries['poor_query_percentage'] = (queries.rating < 3)*100
 
-    counts_total = queries['query_name'].value_counts()
-
-    low_ratings = queries.loc[queries['rating'] < 3, :]
-    counts_low = low_ratings['query_name'].value_counts()
-
-    queries['poor_query_percentage'] = queries['query_name'].map((counts_low / counts_total) * 100).round(2)
-
-    queries = queries.groupby(by='query_name', as_index=False).mean()
-    queries['quality'] = queries['quality'].round(2)
-    queries['poor_query_percentage'].fillna(0, inplace=True)
-    print(queries.drop(columns=['position', 'rating']))
-    return queries.drop(columns=['position', 'rating'])
+    return queries.groupby(by='query_name')[['quality',
+          'poor_query_percentage']].mean().apply(round2).reset_index()
