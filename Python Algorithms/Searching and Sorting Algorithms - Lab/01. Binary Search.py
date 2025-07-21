021861a470a6e7992e84626911fecf632f40d549
xaoccc <xaocccc@gmail.com>
2025-07-21 14:16:59 +0300

    bug fix

--- Python Algorithms/Searching and Sorting Algorithms - Lab/01. Binary Search.py
+++ Python Algorithms/Searching and Sorting Algorithms - Lab/01. Binary Search.py
@@ -1,10 +1,20 @@
-# binary search
-def binary_search(elements, target):
+# Improved version of binary search
+# - Data type validation
+# - Empty list validation
+# - Fixed bug with finding first and last elements
+def index_binary_search(elements, target):
+
+    if not isinstance(elements, list):
+        return "Invalid elements data type!"
+    if not isinstance(target, int):
+        return "Invalid target data type!"
+    if not elements:
+        return -1
     left = 0
     right = len(elements) - 1
 
     while right >= left:
-        mid_index = (right - left) // 2
+        mid_index = left + (right - left) // 2
 
         if elements[mid_index] == target:
             return mid_index
@@ -15,16 +25,4 @@ def binary_search(elements, target):
     return -1
 
 
-elements = [int(i) for i in input().split()]
-target = int(input())
-
-print(binary_search(elements, target))
-
-# linear search:
-#
-# elements = input().split()
-# target = input()
-# if target in elements:
-#     print(elements.index(target))
-# else:
-#     print("-1")
+print(index_binary_search([1, 2, 3, 4, 5], 2))
